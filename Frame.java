e46e071499d1b408c3b7567c504b313615af585a
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-15 19:03:41 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -1,5 +1,19 @@
-// data Frame = Strike | Spare | Miss
+// data Frame = Strike
+//            | Spare
+//            | Miss
 
-public enum Frame {
-  Strike, Spare, Miss
+public abstract class Frame {
+  private Frame() {}
+  
+  public static class Strike extends Frame {
+    public Strike() {}
+  }
+  
+  public static class Spare extends Frame {
+    public Spare() {}
+  }
+  
+  public static class Miss extends Frame {
+    public Miss() {}
+  }
 }
