2ce4ad394ccff769cc3aa49ae00ecb4c03ebd73d
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-18 21:54:42 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -1,19 +1,19 @@
-import junit.framework.TestCase;
-
-public class Bowling extends TestCase {
-  public Bowling(String testName) {
-    super(testName);
-  }
-
-  public void testPerfectGame() {
-    assertEquals(ScoreCard.score("XXXXXXXXXXXX"), 300);
-  }
-
-  public void testAllNines() {
-    assertEquals(ScoreCard.score("9-9-9-9-9-9-9-9-9-9-"), 300);
-  }
-
-  public void testAllSpares() {
-    assertEquals(ScoreCard.score("5/5/5/5/5/5/5/5/5/5/5"), 150);
-  }
-}
+/////////////////////////////////////////////////
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                    YES!                     //
+//                                             //
+//          You CAN use this in your           //
+//             favorite language!              //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+/////////////////////////////////////////////////
