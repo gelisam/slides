740eaf8db60678af367c905547200a3114e6e845
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-18 21:54:42 -0400

    new slide

--- Bowling.java
+++ Bowling.java
@@ -1,14 +1,14 @@
 /////////////////////////////////////////////////
 //                                             //
+//               Haskell tricks                //
+//         you can use in any language         //
 //                                             //
 //                                             //
+//     1. Write functions.                     //
 //                                             //
+//        They are simpler, smaller, and       //
+//        easier to test than objects.         //
 //                                             //
-//                                             //
-//                    YES!                     //
-//                                             //
-//          You CAN use this in your           //
-//             favorite language!              //
 //                                             //
 //                                             //
 //                                             //
