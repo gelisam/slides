b7b59d077a3a1f2fe2b42abe1065d4910f20084b
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 12:32:12 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -10,10 +10,13 @@ public abstract class Frame {
   }
   
   public static class Spare extends Frame {
-    public Spare(int first_roll) {}
+    public Spare(int first_roll) {r1 = first_roll;}
+    public final int r1;
   }
-  
   public static class Miss extends Frame {
-    public Miss(int first_roll, int second_roll) {}
+    public Miss(int first_roll, int second_roll) {r1 = first_roll;
+                                                  r2 = second_roll;}
+    public final int r1;
+    public final int r2;
   }
 }
