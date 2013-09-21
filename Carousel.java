949285601a07706a4a6f5e4c577a351b61fb26f9
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 11:12:52 -0400

    new slide

--- Carousel.java
+++ Carousel.java
@@ -1,16 +1,16 @@
 /////////////////////////////////////////////////
 //                                             //
+//               Haskell tricks                //
+//         you can use in any language         //
 //                                             //
 //                                             //
+//     1. Write functions.                     //
 //                                             //
+//     2. Use precise types.                   //
 //                                             //
-//                                             //
-//                    YES!                     //
-//                                             //
-//          You CAN use this in your           //
-//             favorite language!              //
-//                                             //
-//                                             //
+//        Calling a conversion function is     //
+//        easier than remembering how the      //
+//        representations differ.              //
 //                                             //
 //                                             //
 //                                             //
