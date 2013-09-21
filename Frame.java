3285a3afb4517316032fbceba481cfa2708b7971
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 12:48:15 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -2,21 +2,9 @@
 //            | Spare Int
 //            | Miss  Int Int
 
-public abstract class Frame {
-  private Frame() {}
-  
-  public static class Strike extends Frame {
-    public Strike() {}
-  }
-  
-  public static class Spare extends Frame {
-    public Spare(int first_roll) {r1 = first_roll;}
-    public final int r1;
-  }
-  public static class Miss extends Frame {
-    public Miss(int first_roll, int second_roll) {r1 = first_roll;
-                                                  r2 = second_roll;}
-    public final int r1;
-    public final int r2;
-  }
-}
+// using algebraic types in Haskell:
+// 
+// rolls :: Frame -> [Pins]
+// rolls Strike     = [10]
+// rolls (Spare n)  = [n, 10-n]
+// rolls (Miss n m) = [n, m]
