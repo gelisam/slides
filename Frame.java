13b2ffbc6dc9707af02d7804624f6ac35d44e159
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 19:30:54 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -8,3 +8,16 @@
 // rolls Strike     = [10]
 // rolls (Spare n)  = [n, 10-n]
 // rolls (Miss n m) = [n, m]
+
+// using algebraic types in Java:
+int[] rolls(Frame frame) {
+  if (frame instanceof Strike) {
+    return 10;
+  } else if (frame instanceof Spare) {
+    Spare spare = (Spare) frame;
+    return new int[] {spare.r1, 10 - miss.r1};
+  } else {
+    Miss miss = (Miss) frame;
+    return new int[] {miss.r1, miss.r2};
+  }
+}
