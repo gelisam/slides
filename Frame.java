0c4424e9a97f065bd4c1cc587dd73f64f77bb4f0
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 12:50:00 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -9,15 +9,41 @@
 // rolls (Spare n)  = [n, 10-n]
 // rolls (Miss n m) = [n, m]
 
-// using algebraic types in Java:
-int[] rolls(Frame frame) {
-  if (frame instanceof Strike) {
-    return 10;
-  } else if (frame instanceof Spare) {
-    Spare spare = (Spare) frame;
-    return new int[] {spare.r1, 10 - miss.r1};
-  } else {
-    Miss miss = (Miss) frame;
-    return new int[] {miss.r1, miss.r2};
+// object-oriented version:
+public abstract class Frame {
+  private Frame() {}
+  
+  public abstract int[] rolls();
+  
+  
+  public static class Strike extends Frame {
+    public Strike() {}
+    
+    public int[] rolls() {
+      return new int[] {10};
+    }
+  }
+  
+  public static class Spare extends Frame {
+    private final int r1;
+    public Spare(int first_roll) {
+      r1 = first_roll;
+    }
+    
+    public int[] rolls() {
+      return new int[] {r1, 10 - r1};
+    }
+  }
+  public static class Miss extends Frame {
+    private final int r1;
+    private final int r2;
+    public Miss(int first_roll, int second_roll) {
+      r1 = first_roll;
+      r2 = second_roll;
+    }
+    
+    public int[] rolls() {
+      return new int[] {r1, r2};
+    }
   }
 }
