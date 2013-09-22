44d3446d4ebb47e5c656ca17657354192baff3cc
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 02:32:56 -0400

    new slide

--- Bowling.py
+++ Bowling.py
@@ -1,3 +1,15 @@
 
 def totalScore(frames, bonusRolls):
   return sum(map(lambda x: frameScore(x, bonusRolls), frames))
+
+
+
+
+
+
+def map(f, list):
+  if len(list) == 0:
+    return []
+  else:
+    x,xs = list[0],list[1:]
+    return [f(x)] + map(f, xs)
