37c3d44a8f654ee6ce8d983ebb3342e8af9ce307
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 02:40:21 -0400

    new slide

--- Bowling.py
+++ Bowling.py
@@ -1,15 +1,19 @@
-
-def totalScore(frames, bonusRolls):
-  return sum(mapcons(lambda x xs: frameScore(x, xs, bonusRolls), frames))
-
-
-
-
-
-
-def mapcons(f, list):
-  if len(list) == 0:
-    return []
-  else:
-    x,xs = list[0],list[1:]
-    return [f(x,xs)] + map(f, xs)
+#################################################
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                    YES!                     ##
+##                                             ##
+##          You CAN use this in your           ##
+##             favorite language!              ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+##                                             ##
+#################################################
