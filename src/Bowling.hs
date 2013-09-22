782e9572a2b435927dcf39ec33780b709f56686a
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 00:59:35 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -4,3 +4,55 @@ bonusScore f fs b = sum bonusThrows
     bonusThrows = take (bonusBalls f) ts
     ts = concatMap throws fs ++ b  -- less expensive
                                    -- than it looks!
+
+-- concat [[1,2],[3,4],[5],[6,7]] ++ [8,9]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
