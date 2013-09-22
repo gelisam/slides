0af7a204f3047d95ef4f01c2199119ed3cd30b7b
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 22:14:38 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -13,14 +13,14 @@ score "XXXXXXXXXXXX"          = 300
 score "9-9-9-9-9-9-9-9-9-9-"  = 90
 score "5/5/5/5/5/5/5/5/5/5/5" = 150
 
--- |
--- >>> pins 'X'
--- 10
--- >>> pins '9'
--- 9
--- >>> pins '-'
--- 0
-pins :: Char -> Int
+
+
+
+type Pins  = Int  -- number of pins knocked
+type Balls = Int  -- number of balls thrown
+type Score = Int
+
+pins :: Char -> Pins
 pins 'X' = 10
 pins '-' = 0
 pins  x  = read [x]
