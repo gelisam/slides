acb8b26a15901a4b5f23246a9cf92945a37e4ac4
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 00:53:07 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -1,19 +1,6 @@
------------------------------------------------------
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                   BONUS SLIDES                  --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
---                                                 --
------------------------------------------------------
+bonusScore :: Frame -> [Frame] -> [Pins] -> Score
+bonusScore f fs b = sum bonusThrows
+  where
+    bonusThrows = take (bonusBalls f) ts
+    ts = concatMap throws fs ++ b  -- less expensive
+                                   -- than it looks!
