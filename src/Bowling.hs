8246a0781dd060497bad9855e6efccd87514704f
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 09:18:41 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -2,8 +2,8 @@ frameScore :: Frame -> [Frame] -> [Pins] -> Score
 frameScore f fs b = sum (throws f)
                   + bonusScore f fs b
 
-totalScore :: [Frame] -> [Pins] -> Score
-totalScore fs b = sum $ mapcons (\x xs -> frameScore x xs b) fs
+totalScore :: [Pins] -> [Frame] -> Score
+totalScore b fs = sum $ mapcons (\x xs -> frameScore x xs b) fs
 
 -- Haskell oddity: argument order influences program length!
 
