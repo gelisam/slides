fea7dacefc619e0cd5e1d76a6e83f5c5252e779f
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-22 01:26:00 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -1,66 +1,19 @@
-bonusScore :: Frame -> [Frame] -> [Pins] -> Score
-bonusScore f fs b = sum bonusThrows
-  where
-    bonusThrows = take (bonusBalls f) ts
-    ts = concatMap throws fs ++ b  -- less expensive
-                                   -- than it looks!
-
--- concat [[1,2],...] ++ ...
--- [1,2] ++ ...
--- take 2 [1,2,...]
--- laziness!
-
-
--- also:
-parseFrames :: String -> [Frame]
-parseFrames xs = take 10 (go xs)  -- exploiting laziness
-  where                           -- to skip bonus balls!
-    go ('X'  :xs) = Strike                  : go xs
-    go (x:'/':xs) = Spare (pins x)          : go xs
-    go (x:x' :xs) = Miss (pins x) (pins x') : go xs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-------------------------------------------------
+--                                             --
+--            Haskell-only tricks              --
+--       which won't work anywhere else        --
+--                                             --
+--                                             --
+--     1. Laziness.                            --
+--                                             --
+--        Without language support, you would  --
+--        need to wrap everything in a thunk.  --
+--                                             --
+--        This gets old real quick.            --
+--                                             --
+--                                             --
+--                                             --
+--                                             --
+--                                             --
+--                                             --
+------------------------------------------------
