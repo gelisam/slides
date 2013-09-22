f6f115e7317b1169cc4926a38b20a97c8bb93788
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-21 21:09:28 -0400

    new slide

--- Frame.java
+++ Frame.java
@@ -1,27 +1,19 @@
-
-
-
-
-
-
-
-
-
-
-// a compromise:
-
-class Nullable<A> {
-  private Nullable(A a) {value = a;}
-  public A value;
-}
-
-class Example {
-  void process(Nullable<String> nullable_value) {
-    if (nullable_value.value != null) {
-      String value = nullable_value.value;
-      
-      
-      // process value
-    }
-  }
-}
+/////////////////////////////////////////////////
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                    YES!                     //
+//                                             //
+//          You CAN use this in your           //
+//             favorite language!              //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+//                                             //
+/////////////////////////////////////////////////
