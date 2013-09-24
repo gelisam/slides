40e9b0148ad804755d7dd351d1b0bde62f004b84
Samuel Gelineau <gelisam@gmail.com>
2013-09-24 17:37:20 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -1,10 +1,15 @@
--- mutable array
-myArray.sort();
+-- mutable callback
+$.ajax("http://google.com?q=haskell",
+  function(response) {
+    p = $("#results_pane");
+    setContents(p, response);
+  }
+);
 
-
-
-
-
-
--- immutable array
-mySortedArray = myArray.sorted();
+-- immutable callback?
+$.ajax("http://google.com?q=haskell",
+  function(response) {
+    -- no way to affect the program
+    -- from here...
+  }
+);
