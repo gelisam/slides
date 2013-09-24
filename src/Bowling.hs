c43d3b462a2a4f12d6f48899a7a76dcca20be8d1
Samuel Gelineau <gelisam@gmail.com>
2013-09-24 18:02:43 -0400

    new slide

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -10,7 +10,7 @@
 --    Haskell programs are     --
 -- structured in unusual ways. --
 --                             --
---                             --
+--            How?             --
 ---------------------------------
 
 
