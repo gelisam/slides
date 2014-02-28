c172a560004de22661325bc09b8b09b3a5eaece0
Samuel Gélineau <sgelineau@innobec.com>
2014-02-27 19:14:42 -0500

    new slide

--- example-repo/hello.c
+++ example-repo/hello.c
@@ -1,4 +1,10 @@
 int main() {
+<<<<<<< HEAD
+  printf("HELLO WORLD!");
+||||||| merged common ancestors
+  printf("hello world!");
+=======
   printf("hello world!\n");
+>>>>>>> missing newline
   return 0;
 }
