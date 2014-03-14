4fc613fba4308059a629598d1a7742edbb299280
Samuel Gélineau <sgelineau@innobec.com>
2014-03-14 15:42:46 -0400

    new slide

--- example-repo/main.cpp
+++ example-repo/main.cpp
@@ -1,6 +1,10 @@
-#include "hello.h"
+#include <stdio.h>
+
+void say_hello(char* to_whom) {
+  printf("hello %s\n", to_whom);
+}
 
 int main() {
-  say_hello("world");
+  say_hello("world!");
   return 0;
 }
