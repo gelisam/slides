1dfabe8dd090c53cbffd2578a17f1100a35a5d0c
Samuel Gélineau <sgelineau@innobec.com>
2014-03-20 17:26:07 -0400

    new slide

--- example-repo/main.cpp
+++ example-repo/main.cpp
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/* Greet the given person or entity.
- * 
- * >>> say_hello("world")
- * hello world
- */
 void say_hello(char* to_whom) {
   printf("hello %s\n", to_whom);
 }
