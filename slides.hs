05f6fa6ee3732ee15d17cec781bc7b5144673338
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-01 21:56:17 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,3 +1,20 @@
+{-# LANGUAGE BangPatterns #-}
+
+-- Arrays? In Haskell? Do you mean lists?
+
+
+sumList :: [Double] -> Double
+sumList = go 0
+  where
+    go :: Double -> [Double] -> Double
+    go !acc []     = acc
+    go !acc (x:xs) = go (acc + x) xs
+
+sampleList :: [Double]
+sampleList = [1.0,1.1..3.0]
+
+main :: IO ()
+main = print $ sumList sampleList
 
 
 
@@ -5,11 +22,16 @@
 
 
 
-                       Using a lazy immutable array
-                         to implement a simple AI
 
 
-                                             by Samuel Gélineau
+
+
+
+
+
+
+
+
 
 
 
