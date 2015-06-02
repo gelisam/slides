2c4341c7989af5f47f6740ce225aea13fbc660a2
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-01 22:30:17 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,41 +1,18 @@
-{-# LANGUAGE BangPatterns #-}
+topics list
+  
+  AI stuff:
+    - minimax algorithm
 
-import Data.Array
+  Programming stuff:
+    - dynamic programming
 
+as an excuse to learn about
 
-sumList :: [Double] -> Double
-sumList = go 0
-  where
-    go :: Double -> [Double] -> Double
-    go !acc []     = acc
-    go !acc (x:xs) = go (acc + x) xs
-
-sumArray :: Array Int Double -> Double
-sumArray arr = go 0 i0
-  where
-    go :: Double -> Int -> Double
-    go !acc i | i > n     = acc
-              | otherwise = go (acc + arr ! i) (i+1)
-    
-    i0, n :: Int
-    (i0, n) = bounds arr
-
-
-sampleList :: [Double]
-sampleList = [1.0,1.1..3.0]
-
-sampleArray :: Array Int Double
-sampleArray = listArray (1, length sampleList) sampleList
-
-main :: IO ()
-main = do
-    print $ sumList sampleList
-    print $ sumArray sampleArray
-
-
-
-
-
+  Haskell stuff:
+    - Ix typeclass
+    - immutable arrays
+    - arrays and laziness
+    - caching and laziness
 
 
 
