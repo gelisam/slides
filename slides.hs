b2521bfc6a11908595120e95854812c7c275e6c3
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-03 09:36:42 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -10,6 +10,11 @@ import Data.Array
 --                    +---+---+
 --             xs            ys            zs
 
+xs :: Array (Int, Int) Double
+xs = listArray ((1,1), (2,3)) [ 1.0, 2.0, 3.0
+                              , 4.0, 5.0, 6.0
+                              ]
+
 
 
 
