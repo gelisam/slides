bce5143a3d0c33d7aa997d9760682fa3f10525bf
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-03 09:14:49 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,11 +1,14 @@
 -- Ix
-
+{-# LANGUAGE FlexibleInstances #-}
 
 class Ord a => Ix a where
     index :: a -> Int
 
 instance Ix Int where
     index i = i
+
+instance Ix (Int, Int) where
+    index (i,j) = j * w + i
 
 
 
