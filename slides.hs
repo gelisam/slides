ce98a406bc5db4a6dece6e7d73cd1ab399bb6722
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-03 10:45:43 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,111 +1,100 @@
--- laziness-based caching
-{-# LANGUAGE ScopedTypeVariables #-}
-import Debug.Trace
-import Text.Printf
-
-noisyAdd :: Num a => a -> a -> a
-noisyAdd x y = trace "adding things"
-                     (x + y)
+-- minimax
 
 
-fib :: forall a. Int -> a -> (Integer, a)
-fib = (\n x -> (fibs !! n, x))
-  where
-    fibs :: [Integer]
-    fibs = 1:1:zipWith noisyAdd fibs (tail fibs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 main :: IO ()
-main = do
-    print $ fib 9 "foo"
-    print $ fib 10 "bar"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main = putStrLn "typechecks."
