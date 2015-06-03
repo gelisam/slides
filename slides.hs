534907ae56fc5e10b5f01e08bf012c2352dd1988
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-03 10:33:30 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,17 +1,26 @@
 -- laziness-based caching
 
+
+
+
+
+
+
+
+
 fibs :: [Integer]
-fibs = 1:1:zipWith (+) fibs (tail fibs)
+fibs = 1:1:zipWith noisyAdd fibs (tail fibs)
 
 fib :: Int -> Integer
 fib 0 = 1
 fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib n = fib (n-1) `noisyAdd` fib (n-2)
 
 
 --  +--+    +--+    +--+    +--+    +--+    +--+    +--+    
 --  | 1| -> | 1| -> | 2| -> | 3| -> | 5| -> | 8| -> |..| -> ...
 --  +--+    +--+    +--+    +--+    +--+    +--+    +--+    
+
 
 
 
