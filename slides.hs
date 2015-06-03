fbc28be0696c1c909acdb9f453146e4b2d7d3ce6
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-02 21:45:25 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,14 +1,21 @@
--- Traditional array access syntax
+-- Haskell array access syntax
 
-double arr[3][3][3]
-          [3][3][3]
-          [3][3][3]
-          [2][3];
+arr :: Array Int
+         (Array Int
+           (Array Int
+             (Array Int
+               (Array Int
+                 (Array Int
+                   (Array Int
+                     (Array Int
+                       (Array Int
+                         (Array Int
+                           (Array Int Double))))))))))
 
-state = arr[c1][c2][c3]
-           [c4][c5][c6]
-           [c7][c8][c9]
-           [player][rows];
+state = arr ! c1 ! c2 ! c3
+            ! c4 ! c5 ! c6
+            ! c7 ! c8 ! c9
+            ! player ! rows
 
 
 
