edd4c6b3a090149ce48d490627206df329039cfa
Samuel Gélineau <gelisam+github@gmail.com>
2015-06-03 08:36:31 -0400

    new slide

--- slides.hs
+++ slides.hs
@@ -1,21 +1,8 @@
 -- Haskell array access syntax
 
-arr :: Array Int
-         (Array Int
-           (Array Int
-             (Array Int
-               (Array Int
-                 (Array Int
-                   (Array Int
-                     (Array Int
-                       (Array Int
-                         (Array Int
-                           (Array Int Double))))))))))
+arr :: Array Int Double
 
-state = arr ! c1 ! c2 ! c3
-            ! c4 ! c5 ! c6
-            ! c7 ! c8 ! c9
-            ! player ! rows
+state = arr ! i
 
 
 
