bead570d60021a5718a2825b78b75842f4f3b660
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-15 08:58:59 -0400

    new slide

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use std::sync::MutexGuard;
 fn main() {
     let lock_x: Mutex<i32> = Mutex::new(0);
     let x: &mut i32;
-    let x_copy: i32;
+    
     {
         let mut guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
-        x = guard_x.deref_mut();
-        x_copy = *x;
-        *x += 1;
+        x = guard_x.deref_mut(); // type error!
+        
+        
     }
     
-    println!("x was {}", x_copy);
+    
     
     {
         let guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
