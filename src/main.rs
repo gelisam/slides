0ff95ccd73a7bffdd37e4d58970ee99eae1dfc36
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-15 09:00:32 -0400

    new slide

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::ops::DerefMut;
 use std::sync::Mutex;
 use std::sync::MutexGuard;
 
+
+
+
+
+
 fn main() {
     let lock_x: Mutex<i32> = Mutex::new(0);
     let x: &mut i32;
@@ -10,11 +15,7 @@ fn main() {
     {
         let mut guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
         x = guard_x.deref_mut(); // type error!
-        
-        
     }
-    
-    
     
     {
         let guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
