dd8c2ebdc0c7f4d9a897958ecf0a7f66f40d0f48
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-16 08:24:26 -0400

    new slide

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 
 
-
+use std::sync::Mutex;
 
 
 fn main() {
     let mut x: i32 = 0;
+    let lock_x = Mutex::new(());
     
-    
-    
-    
+    {
+        lock_x.lock();
         x += 1;
+    }
     
-    
-    
-    
+    {
+        lock_x.lock();
         println!("x is now {}", x);
-    
+    }
 }
