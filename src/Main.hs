366ac62ae37ac8f8d88f0252c99492936202a0f0
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-16 23:15:43 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -7,13 +7,13 @@ main :: IO ()
 main = do
    lock_x <- do ref_x <- newIORef (0 :: Int)
                 newMVar ref_x
-   
+   ref <- newIORef Nothing
    
    withMVar lock_x $ \ref_x -> do
      x <- readIORef ref_x
      if x < 10 then modifyIORef ref_x (+1)
                else modifyIORef ref_x (+10)
-   
+     writeIORef ref (Just x)
    
    withMVar lock_x $ \ref_x -> do
      
