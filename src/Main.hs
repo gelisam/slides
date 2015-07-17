d515daccc8c72b76d0ca1073f80945ea3f2a1595
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-17 08:07:29 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,18 +3,104 @@ import Control.Monad.Trans.State
 import Data.IORef
 import Text.Printf
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 main :: IO ()
-main = do
-   lock_x <- do ref_x <- newIORef (0 :: Int)
-                newMVar ref_x
-   ref <- newIORef Nothing
-   
-   withMVar lock_x $           do
-     x <- get
-     if x < 10 then modify (+1)
-               else modify (+10)
-     
-   
-   withMVar lock_x $ \ref_x -> do
-     
-     printf "x is now %d" =<< readIORef ref_x
+main = return ()
