af01f7db0cb72812042c24220f39854c416b2da5
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-16 23:03:21 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,18 @@
-
+import Control.Concurrent.MVar
 import Data.IORef
 import Text.Printf
 
 main :: IO ()
 main = do
    ref_x <- newIORef (0 :: Int)
+   lock_x <- newMVar ()
    
-   
-   
-   
+   do
+     () <- takeMVar lock_x
      modifyIORef ref_x (+1)
+     putMVar lock_x ()
    
-   
-   
-   
+   do
+     () <- takeMVar lock_x
      printf "x is now %d" =<< readIORef ref_x
+     putMVar lock_x ()
