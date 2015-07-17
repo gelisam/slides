1d632527bac787897a40df2b55c3f061c7781585
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-17 10:00:29 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -39,13 +39,38 @@ main = do
        handlePutStrLn "world"
 
 
+data HandleMonad a
+  = HandleReturn a
+  | HandlePutStrLn String (HandleMonad a)
+  | HandleGetLine (String -> HandleMonad a)
 
+runHandleMonad :: HandleMonad a -> Handle -> IO a
+runHandleMonad (HandleReturn x)      _ = return x
+runHandleMonad (HandlePutStrLn s cc) h = do hPutStrLn h s
+                                            runHandleMonad cc h
+runHandleMonad (HandleGetLine    cc) h = do s <- hGetLine h
+                                            runHandleMonad (cc s) h
 
+instance Functor HandleMonad where
+  fmap f (HandleReturn x)      = HandleReturn (f x)
+  fmap f (HandlePutStrLn s cc) = HandlePutStrLn s (fmap f cc)
+  fmap f (HandleGetLine    cc) = HandleGetLine    (fmap (fmap f) cc)
 
+instance Applicative HandleMonad where
+  pure = return
+  (<*>) = ap
 
+instance Monad HandleMonad where
+  return = HandleReturn
+  HandleReturn x      >>= f = f x
+  HandlePutStrLn s cc >>= f = HandlePutStrLn s (cc >>= f)
+  HandleGetLine    cc >>= f = HandleGetLine    (fmap (>>= f) cc)
 
+handlePutStrLn :: String -> HandleMonad ()
+handlePutStrLn s = HandlePutStrLn s (return ())
 
-
+handleGetLine :: HandleMonad String
+handleGetLine = HandleGetLine return
 
 
 
