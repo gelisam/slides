ce66d7c0d8b268c9d88fdb892293f0a4f151fba6
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-16 22:14:13 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -8,11 +8,11 @@ import Text.Printf
 main :: IO ()
 main = do
    lock_h <- newMVar stdout
-   
+   ref_h <- newIORef Nothing
    
    forkIO $ forever $ do
      withMVar lock_h $ \h -> do
-       
+       writeIORef ref_h (Just h)
        hPutStrLn h "hello"
    
    forever $ do
