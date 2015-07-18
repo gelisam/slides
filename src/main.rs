36d3e48924e28ce479236cd4c7a8ac5e3536312a
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-18 15:26:48 -0400

    new slide

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,127 @@
+use std::ops::Deref;
+use std::ops::DerefMut;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+
+fn my_deref_mut<'scope, A>(g: &'scope mut MutexGuard<A>) -> &'scope mut A
+{
+    g.deref_mut()
+}
+
+
+fn main() {
+    let lock_x = Mutex::new(0);
+    
+    {
+        let mut guard_x = lock_x.lock().unwrap();
+        
+        let x = my_deref_mut(&mut guard_x);
+        *x += 1;
+    }
+    
+    {
+        let guard_x = lock_x.lock().unwrap();
+        let x = guard_x.deref();
+        println!("x is now {}", *x);
+    }
+}
 
 
 
-  //////////////////////////////////////////////////////
-  //                                                  //
-  //                                                  //
-  //        Rust's pointful syntax is clearer.        //
-  //                                                  //
-  //                                                  //
-  //////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
