4351fd3db575f2eb6b70c7ca60bcc46dc6c994f4
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-18 15:28:06 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,138 @@
+import Control.Concurrent.MVar
+import Control.Monad.Trans.State
+import Data.IORef
+import Text.Printf
 
 
 
-  ------------------------------------------------------
-  --                                                  --
-  --                                                  --
-  --           On par with Rust's mutexes?            --
-  --                                                  --
-  --                                                  --
-  ------------------------------------------------------
+
+
+main :: IO ()
+main = do
+   lock_x <- newMutex (0 :: Int)
+   
+   
+   
+   withMutex lock_x $ do
+     x <- get
+     modify (+1)
+   
+   
+   
+   x <- withMutex lock_x get
+   
+   printf "x is now %d" x
+   
+   
+data Mutex a = Mutex (MVar (IORef a))
+
+newMutex :: a -> IO (Mutex a)
+newMutex x = do
+    ref <- newIORef x
+    mvar <- newMVar ref
+    return (Mutex mvar)
+
+withMutex :: Mutex s -> State s a -> IO a
+withMutex (Mutex mvar) body = do
+    ref <- takeMVar mvar
+    s <- readIORef ref
+    let (x, s') = runState body s
+    writeIORef ref s'
+    putMVar mvar ref
+    return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
