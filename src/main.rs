5d0f74c0a400f32b3594cb990676f44540aad015
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-18 09:43:44 -0400

    new slide

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     
     {
         let mut guard_x;
-        
+        deref_incr(&mut guard_x); // uninitialized!
         guard_x = lock_x.lock().unwrap();
         deref_incr(&mut guard_x);
     }
