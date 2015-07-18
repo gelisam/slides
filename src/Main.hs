3aa56b56029da797b3417b3e586902ae8f81663e
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-18 15:27:29 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
 
+import Data.IORef
+import Text.Printf
 
-
-  ------------------------------------------------------
-  --                                                  --
-  --                                                  --
-  --                Of course we can!                 --
-  --                                                  --
-  --                                                  --
-  ------------------------------------------------------
+main :: IO ()
+main = do
+   ref_x <- newIORef (0 :: Int)
+   
+   modifyIORef ref_x (+1)
+   
+   printf "x is now %d" =<< readIORef ref_x
