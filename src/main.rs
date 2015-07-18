0ea4019fd3601fed36e3e09ab038a00680ac2c94
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-17 21:22:41 -0400

    can we do this?

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-use std::ops::Deref;
-use std::ops::DerefMut;
-use std::sync::Mutex;
-use std::sync::MutexGuard;
 
-fn my_deref_mut<'scope, A>(g: &'scope mut MutexGuard<A>) -> &'scope mut A
-{
-    g.deref_mut()
-}
 
-fn main() {
-    let lock_x: Mutex<i32> = Mutex::new(0);
-    
-    
-    {
-        let mut guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
-        let x: &mut i32 = my_deref_mut(&mut guard_x); // typechecks!
-    }
-    
-    {
-        let guard_x: MutexGuard<i32> = lock_x.lock().unwrap();
-        let x: &i32 = guard_x.deref();
-        println!("x is now {}", *x);
-    }
-}
+
+  //////////////////////////////////////////////////////
+  //                                                  //
+  //                                                  //
+  //            Can we do this in Haskell?            //
+  //                                                  //
+  //                                                  //
+  //////////////////////////////////////////////////////
