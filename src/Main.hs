8ff2953592d6a1577c3d81761eb6bc7a5b8670f7
Samuel Gélineau <gelisam+github@gmail.com>
2015-07-26 17:52:00 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -9,12 +9,15 @@ data Mutex a
 newMutex :: a -> IO (Mutex a)
 
 withGuard :: Mutex s -> GuardInScope (Just s) (Just s) a -> IO a
-
-
-
-
+withGuard _ _ = do
+    lock
+    ...
+    unlock
 
 withUninitializedGuard :: GuardInScope Nothing (Just s) a -> IO a
+withUninitializedGuard _ = do
+    ...
+    unlock
 
 
 
