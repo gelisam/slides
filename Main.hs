093c08ea68459ce63eb3f261ad77784596efe45f
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-03 16:21:20 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -2,10 +2,10 @@
 import Prelude hiding (id)
 
 
-data FreeCategory k a b where
-    CNil  :: FreeCategory k a a
-    CCons :: k a b -> FreeCategory k b c -> FreeCategory k a c
-
+data CategoryAST k a b where
+    CBase :: k a b -> CategoryAST k a b
+    CId   :: CategoryAST k a a
+    CThen :: CategoryAST k a b -> CategoryAST k b c -> CategoryAST k a c
 
 -- Pattern-matching on categorical computations
 
@@ -182,10 +182,9 @@ class Category k where                -- class Category k where
     id    :: k a a                    --     id  :: k a a
     (>>>) :: k a b -> k b c -> k a c  --     (.) :: k b c -> k a b -> k a c
 
-data CategoryAST k a b where
-    CBase :: k a b -> CategoryAST k a b
-    CId   :: CategoryAST k a a
-    CThen :: CategoryAST k a b -> CategoryAST k b c -> CategoryAST k a c
+data FreeCategory k a b where
+    CNil  :: FreeCategory k a a
+    CCons :: k a b -> FreeCategory k b c -> FreeCategory k a c
 
 instance Category (FreeCategory k) where
     id = CNil
