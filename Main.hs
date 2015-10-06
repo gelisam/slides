4cbbda87582d6edbde329653e9159ad95f4074de
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-05 23:42:39 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,20 +1,39 @@
+{-# LANGUAGE GADTs #-}
+import Prelude hiding (Monoid(..), Monad(..))
+
+-- mx >>= return = mx
+-- return x >>= f = f x
+-- (mx >>= f) >>= g = mx >>= (\x -> f x >>= g)
+class Monad m where
+    return :: a -> m a
+    (>>=)  :: m a -> (a -> m b) -> m b
+
+data MonadAST m a where
+    MBase   :: m a -> MonadAST m a
+    MReturn :: a -> MonadAST m a
+    MBind   :: MonadAST m a -> (a -> MonadAST m b) -> MonadAST m b
+
+data FreeMonad m a where
+    MNil  :: a -> FreeMonad m a
+    MCons :: m a -> (a -> FreeMonad m b) -> FreeMonad m b
+
+instance Monad (FreeMonad m) where
+    return = MNil
+    MNil x      >>= f = f x
+    MCons mx cc >>= f = MCons mx $ fmap (>>= f) cc
+
+singletonM :: m a -> FreeMonad m a
+singletonM mx = MCons mx MNil
 
 
 
 
 
 
-         ------------------------------------------
-         --                                      --
-         --      0. The Quest                    --
-         --                                      --
-         --      1. The Free Monoid              --
-         --    > 2. The Free Monad               --
-         --      3. The Free Category            --
-         --                                      --
-         --  BONUS. The Tardis Monad             --
-         --                                      --
-         ------------------------------------------
+
+
+
+
 
 
 
