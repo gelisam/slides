64ee59daca32463e47df9a07895baa1a44a169b4
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-06 21:28:36 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -8,25 +8,16 @@ class Monoid a where
     mempty  :: a
     mappend :: a -> a -> a
 
-data Circle = Circle
-  { center :: (Double,Double)
-  , radius :: Double
-  }
-
-data FreeMonoid a = ...
-
-instance Monoid (FreeMonoid a) where
-    mempty  = ...
-    mappend = ...
-
-emptyCircle :: FreeMonoid Circle
-emptyCircle = mempty
+data MonoidAST a where
+    Base    :: a -> MonoidAST a
+    MEmpty  :: MonoidAST a
+    MAppend :: MonoidAST a -> MonoidAST a -> MonoidAST a
 
 
-instance (Monoid a, Monoid b) => Monoid (a,b) where
-    mempty = (mempty, mempty)
-    mappend (x1,y1) (x2,y2) = (mappend x1 x2, mappend y1 y2)
 
+instance Monoid (MonoidAST a) where
+    mempty = MEmpty
+    mappend = MAppend
 
 
 
