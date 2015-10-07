39825d747891aef422b04925b674bf039cb2a9fc
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-06 21:43:56 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -13,9 +13,14 @@ data MonoidAST a where
     MEmpty  :: MonoidAST a
     MAppend :: MonoidAST a -> MonoidAST a -> MonoidAST a
 
-type FreeMonoid a = [a]
+data FreeMonoid a where
+    Nil  :: FreeMonoid a
+    Cons :: a -> FreeMonoid a -> FreeMonoid a
 
-
+instance Monoid (FreeMonoid a) where
+    mempty = Nil
+    Nil       `mappend` ys = ys
+    Cons x xs `mappend` ys = Cons x (xs `mappend` ys)
 
 
 
