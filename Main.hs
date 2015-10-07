a3cce9cbe65f4c42acf8750a5a81e0528dc34650
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-06 20:23:36 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,22 +1,33 @@
 
 
+-- Category Syntax: a do-notation for categories
+-- https://github.com/gelisam/category-syntax
 
 
+rearrange1 :: Either (Either a1 a2) (Either b1 b2)
+           -> Either (Either a1 b1) (Either a2 b2)
+rearrange1 = $(syntax [|do
+    ((x1,x2), (y1,y2)) <- getInput
+    returnC ((x1,y1), (x2,y2))
+  |])
 
-
-         ------------------------------------------
-         --                                      --
-         --                                      --
-         --    the search for free categories    --
-         --                                      --
-         --        a higher-kinded quest         --
-         --                                      --
-         --                                      --
-         --                         presented by --
-         --                      Samuel Gélineau --
-         ------------------------------------------
-
-
+rearrange2 :: Either (Either a1 a2) (Either b1 b2)
+           -> Either (Either a1 b1) (Either a2 b2)
+             -- ((x1, x2), (y1,y2)))
+rearrange2 = associate
+             -- (x1, (x2, (y1,y2)))
+         >>> second coassociate
+             -- (x1, ((x2,y1), y2))
+         >>> second (first swap)
+             -- (x1, ((y1,x2), y2))
+         >>> coassociate
+             -- ((x1, (y1,x2)), y2)
+         >>> first coassociate
+             -- (((x1,y1), x2), y2)
+         >>> associate
+             -- ((x1,y1), (x2,y2))
+         >>> returnC  -- aka Category.id
+             -- ((x1,y1), (x2,y2))
 
 
 
