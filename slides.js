9046c4e70e4809e28042de0675985bb342f2d588
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-25 18:28:58 -0400

    new slide

--- slides.js
+++ slides.js
@@ -6,45 +6,11 @@
    Bad idea: defend against malformed input arguments
 
 
-  // a new user filled the signup form
-  app.post('/signup', function(req, res, next) {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // the client sent us a JSON object with all the fields
-    // required to create a User row in Mongoose
-    var newUser = new User(req.body);
-    
-    
-    
-    
-    
-    
-    // write it to the Mongoose DB
-    newUser.save()
-      .then(function(user) {
-        
-        // confirm that the user was created
-        res.send({
-          successful: true,
-          userId: user.id
-        });
-      });
-  });
+  function resetPassword(user) {
+    generateOneTimeToken(function(token) {
+      mail.send(user.email, 'Click on this link to reset your password...');
+    });
+  }
 
 
 
