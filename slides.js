3fff36a77c0b8b8379859258692bc9535f714b4e
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-25 16:48:37 -0400

    new slide

--- slides.js
+++ slides.js
@@ -10,7 +10,7 @@
   app.post('/signup', function(req, res, next) {
     assert(typeof req.body.firstName === 'string' && req.body.firstName.trim() != '');
     assert(typeof req.body.lastName  === 'string' && req.body.lastName.trim() != '');
-    assert(typeof req.body.email     === 'string' && req.body.email.trim() != '');
+    assert(typeof req.body.email     === 'string' && emailRegexp.test(req.body.email));
     
     // the client sent us a JSON object with all the fields
     // required to create a User row in Mongoose
