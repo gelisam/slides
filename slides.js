656f9aad45fde958dcfa3e36306a83230d2a0a97
Samuel Gélineau <gelisam+github@gmail.com>
2015-11-27 09:05:40 -0500

    new slide

--- slides.js
+++ slides.js
@@ -1,19 +1,33 @@
+// Once upon a time...
+// before there were promises...
+// lived the callback.
+
+
+// play intro animation
+function playCutscene(callback) {
+  lookLeft(boyfriend);
+  speak(boyfriend, heartSprite);
+  
+  lookRight(player);
+  speak(player, heartSprite);
+  
+  // pause for dramatic effect!
+  sleep(200);
+  
+  lookDown(boyfriend);
+  speak(boyfriend, heartSprite);
+}
 
 
 
-  /////////////////////////////////////////////////////
-  //                                                 //
-  //                                                 //
-  //                                                 //
-  //               Javascript Promises               //
-  //                                                 //
-  //        an alternative to blocking calls         //
-  //                                                 //
-  //                                                 //
-  //                              presented by       //
-  //                              Samuel Gélineau    //
-  //                                                 //
-  /////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
 
                                      
 
@@ -105,4 +119,20 @@
 
 
 
+var boyfriend = "boyfriend";
+function lookLeft(actor, callback) {
+  console.log(actor + " looks left.");
+}
+//    speak(boyfriend, heartSprite, function() {
+//      lookRight(player, function() {
+//        speak(player, heartSprite, function() {
+//          pause(200, function() { // for dramatic effect!
+//            lookDown(boyfriend, function() {
+//              speak(boyfriend, heartSprite, callback);
+//            });
+//          });
+//        });
+//      });
+//    });
 
+playCutscene();
