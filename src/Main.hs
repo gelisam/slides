27d9a2ea8766a6dedcf472006a05e0c0484febd5
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 14:47:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
-{-@ div' :: Int -> {v: Int | v != 0 } -> Int @-}
+div' :: Int -> Int -> Int
 div' = div
 
 doMath :: Int -> IO ()
 doMath n = print $ (42 `div'` n) + 1
 
 main :: IO ()
-main = doMath 0  -- compile-time error!
+main = doMath 0  -- programmer error!
 
 
 
