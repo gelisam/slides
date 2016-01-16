500f0a75a4cc271856f66f880a2c5a1e1014a175
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 14:47:28 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,7 @@
-div' :: Int -> Int -> Int
-div' = div
-
-doMath :: Int -> IO ()
-doMath n = print $ (42 `div'` n) + 1
-
 main :: IO ()
-main = doMath 0  -- programmer error!
+main = print $ tail (tail [1])
+
+
 
 
 
