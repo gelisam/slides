bb3a47003518cf7d6b6bd8904f36d68196150b21
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 14:21:34 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,9 @@
-div' :: Int -> Int -> Maybe Int
-div' _ 0 = Nothing
-div' x y = Just (x `div` y)
+{-@ div' :: Int -> {v: Int | v != 0 } -> Int @-}
+div' = div
 
 doMath :: Int -> IO ()
 doMath 0 = putStrLn "please enter another number"
-doMath n = do
-    case 42 `div'` n of
-      Nothing -> error "never happens"
-      Just x  -> print (x + 1)
+doMath n = print $ (42 `div'` n) + 1
 
 main :: IO ()
 main = doMath 0
