bddeed7638d758d0d17675febc40000f4cbe92e9
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 14:19:14 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,10 @@ div' _ 0 = Nothing
 div' x y = Just (x `div` y)
 
 doMath :: Int -> IO ()
+doMath 0 = putStrLn "please enter another number"
 doMath n = do
     case 42 `div'` n of
-      Nothing -> putStrLn "divided by zero"
+      Nothing -> error "never happens"
       Just x  -> print (x + 1)
 
 main :: IO ()
