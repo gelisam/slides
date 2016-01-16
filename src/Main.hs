6d692b3ba358ecaca78fe28ac29ce4c914c860f4
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 12:25:06 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,5 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-import Data.Map as Map
-import Control.Exception
-
-
-next :: Map Int Int
-next = Map.fromList [ (1, 2)
-                    , (2, 3)
-                    , (3, 4)
-                    ]
-
-
-lookupPlusOne :: Maybe Int
-lookupPlusOne = do
-    x <- Map.lookup 4 next
-    return (x + 1)
-
-lookupPlusTwo :: Maybe Int
-lookupPlusTwo = do
-    x <- lookupPlusOne
-    return (x + 1)
-
-
-bangPlusOne :: Int
-bangPlusOne = (next ! 4) + 1
-
-bangPlusTwo :: Int
-bangPlusTwo = bangPlusOne + 1
-
 main :: IO ()
-main = do
-    -- print $ lookupPlusTwo + 1
-    
-    
-    
-    print $ bangPlusTwo + 1
-
-
-
+main = print $ 42 `div` 0
 
 
 
