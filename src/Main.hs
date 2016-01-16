535a1dee23765b2fd934d6c1971d78e718f71dd2
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 11:54:16 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,22 @@
 import Data.Map as Map
 
 
-chars :: Map Int Char
-chars = Map.fromList [ (1, 'a')
-                     , (2, 'b')
-                     , (3, 'c')
-                     ]
+next :: Map Int Int
+next = Map.fromList [ (1, 2)
+                    , (2, 3)
+                    , (3, 4)
+                    ]
 
 main :: IO ()
 main = do
-    print $ Map.lookup 4 chars
-    print $ chars ! 4
-
-
+    print $ do i <- Map.lookup 2 next
+               j <- Map.lookup i next
+               k <- Map.lookup j next
+               return k
+    print $ let i = next ! 2
+                j = next ! i
+                k = next ! j
+            in k
 
 
 
