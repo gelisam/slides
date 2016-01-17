6b0606e2344f64f4c8d1b49c3905d0e36dc6b082
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-17 09:31:00 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,23 @@
-import Control.DeepSeq
-import Control.Exception
-import GHC.Conc
-import System.IO.Unsafe
+import Data.Maybe
 
 main :: IO ()
 main = do
-    print $ head xs1  -- Left (...)
-    print $ head xs2  -- *** Exception
+    print $ head'' xs1  -- Left (...)
+    print $ head'' xs2  -- *** Exception
   where
-    xs1 = [1 `div'` 0] :: [Either SomeException Int]
-    xs2 = []           :: [Either SomeException Int]
+    xs1 = [1 `div'` 0] :: [Maybe Int]
+    xs2 = []           :: [Maybe Int]
 
-div' :: (Integral a, NFData a)
-     => a -> a -> Either SomeException a
-div' = unexceptional2 div
+head' :: [a] -> Maybe a
+head' []    = Nothing
+head' (x:_) = Just x
 
-unexceptional2 :: (NFData a, NFData b)
-               => (a -> b -> c)
-               -> (a -> b -> Either SomeException c)
-unexceptional2 f x y = force x
-                `pseq` force y
-                `pseq` unexceptional (f x y)
+head'' :: [a] -> a
+head'' = fromJust . head'
 
+div' :: Integral a => a -> a -> Maybe a
+div' _ 0 = Nothing
+div' x y = Just (x `div` y)
 
 
 
@@ -120,13 +116,7 @@ unexceptional2 f x y = force x
 
 
 
-unexceptional :: a -> Either SomeException a
-unexceptional x = unsafePerformIO $ do
-    try (x `seq` return x)
 
-unexceptional1 :: NFData a
-               => (a -> b)
-               -> (a -> Either SomeException b)
-unexceptional1 f x = force x `pseq` unexceptional (f x)
+
 
 
