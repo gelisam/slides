ea8decce101a0b0c147019c654db35398edd8ed1
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 22:04:41 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,143 +1,113 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-import Control.Monad
-import Data.Map (Map, (!))
-import qualified Data.Map as Map
+-- tracked exceptions (Maybe, Either...):
+-- conditions which ought to be handled semantically.
+-- e.g. element not found in the cache,
+--      environment not configured as expected,
+--      etc.
+
+-- untracked exceptions (error, undefined, ...):
+-- conditions which ought to be handled generically.
+-- e.g. the algorithm was implemented incorrectly;
+--      it expected a key which wasn't there,
+--      it got stuck in an infinite loop,
+--      etc.
 
 
-type Graph node = Map node [node]
 
-dfs :: forall node. (Show node, Ord node)
-    => node
-    -> Graph node
-    -> Map node Int
-dfs node0 g = go node0 Map.empty
-  where
-    go :: node -> Map node Int -> Map node Int
-    go node visited = case Map.lookup node visited of
-      Just _  -> visited
-      Nothing ->
-        let i        = Map.size visited
-            visited' = Map.insert node i visited
-            neighbours = case Map.lookup node g of
-              Just nodes -> nodes
-              Nothing    -> error "invalid graph"
-        in foldr go visited' neighbours
 
---  / \
--- |   |
---  `->a <--> b
---     ^      |
---     |      v
---     d <--> c<-.
---            |   |
---             \ /
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 main :: IO ()
-main = print
-     $ Map.toList
-     $ dfs "a"
-     $ Map.fromList [ ("a", ["a", "b"])
-                    , ("b", ["a", "e"]) -- programmer
-                    , ("c", ["c", "d"]) -- error!
-                    , ("d", ["a", "c"])
-                    ]
-                     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main = return ()
