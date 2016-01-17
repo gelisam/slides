12a144b911ea03e3cff54ec3a44712f82b3df30d
Samuel Gélineau <gelisam+github@gmail.com>
2016-01-16 23:07:00 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,113 +1,111 @@
--- tracked exceptions (Maybe, Either...):
--- conditions which ought to be handled semantically.
--- e.g. element not found in the cache,
---      environment not configured as expected,
---      etc.
 
--- untracked exceptions (error, undefined, ...):
--- conditions which ought to be handled generically.
--- e.g. the algorithm was implemented incorrectly;
---      it expected a key which wasn't there,
---      it got stuck in an infinite loop,
---      etc.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+assocList :: [(Int, Char)]
+assocList = [ (1, 'a')
+            , (2, 'b')
+            , (3, 'c')
+            ]
 
 main :: IO ()
-main = return ()
+main = print $ lookup 4 assocList
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
