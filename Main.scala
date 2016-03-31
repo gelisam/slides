b86175cd7db2c0ebf52067128242cbdb1c198d6e
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-03-31 14:52:00 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -3,7 +3,7 @@
 ////////////////////
 
 WHERE: New York
-WHEN:  February 4-5, 2016
+WHEN:  February 4-5, 2016 (those are weekdays, thank you KeaText!)
 WHAT:  Talks about strongly-typed functional programming
 
 
