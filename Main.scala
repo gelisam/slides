3a892aaebc4b0f9162bbbe35433d8961568a4700
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-03-31 11:34:11 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,30 +1,26 @@
+  ///////////////
+ // the talks //
+///////////////
 
+- "[...] making abstract mathematics palatable", by Eugenia Cheng
+- "[...] Machine Learning, Types and Testing", by Leonid Rozenberg
+- "Add a type parameter! [...]", by Stephen Compall
+- "Dynamic Typing in GHC", by Stephanie Weirich
+- "Composing Network Operating Systems", by Mindy Preston
+- "Chaos Testing at Jet", by Rachel Reese
+- "'There and Back Again' and What Happened After", by Kenneth Foner
+- "[...] F# Active Patterns [...]", by Paulmichael Blasucci
+- "Improving Type Error Localization [...]", by Pavlinovic et al.
+- "Cryptography and verification with Cryptol", by Austin Seipp
+- "FLTKHS - Easy native GUIs in Haskell, Today!", by Aditya Siram
+- "Fun with GHCJSi", by Luite Stegeman
+- "Ionide and state of F# open source environment", by Jared Hester
+- "Liquid Types for Haskell", by Niki Vazou
+- "[...] Algorithmic Differentiation for OCaml", by Markus Mottl
+- "Functional Reactive Programming [...]", by Riccardo Terrell
+- "Analyzing Programs with Z3", by Tikhon Jelvis
 
-  Excerpts from the Compose Conference (2016)
-
-  v Introduction
-    - the conference
-    - the talks
-    - this talk
-  v Excerpts
-    - add a type parameter!
-    - composing network operating systems
-    - active patterns
-  v Conclusion
-    - the conference
-    -[the talks]
-    - this talk
-
-
-
-
-
-
-
-
-
-
-
+Search for "compose conference" on YouTube
 
 
 
