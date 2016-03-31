d055f9c1aacce788c2542ebceefc8a7b1909e526
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-03-31 10:51:12 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,24 +1,18 @@
-  ////////////////////
- // the conference //
-////////////////////
 
-WHERE: New York
-WHEN:  February 4-5, 2016 (those are weekdays, thank you KeaText!)
-WHAT:  Talks about strongly-typed functional programming
 
-       - Haskell
-       - Elm
-       - F#
-       - OCaml
-       - SML
+  Excerpts from the Compose Conference (2016)
 
-       ...but not Scala?
+  v Introduction
+    -[the conference]
+    - the talks
+    - this talk
+  > Excerpts
+  > Conclusion
 
-       ...because Scala already has its own conferences:
 
-       - Scala Days
-       - Scala World
-       - many regional conferences such as Scala Up North in Toronto.
+
+
+
 
 
 
