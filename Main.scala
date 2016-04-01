8630d6d194a887f0aabb975692691e8e1f0ff240
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-01 12:36:25 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -5,15 +5,8 @@
 
 case class Vector(x: Double, y: Double)
 
-object Polar {
-  def angle(vector: Vector) = Math.atan2(vector.y, vector.x)
-  def magnitude(vector: Vector) =
-    Math.sqrt(vector.x * vector.x + vector.y * vector.y)
-}
-
-val point = Vector(3, 4)
-val angle = Polar.angle(point)
-val magnitude = Polar.magnitude(point)
+def manhattan_distance(vector: Vector): Double =
+  vector.x.abs + vector.y.abs
 
 
 
