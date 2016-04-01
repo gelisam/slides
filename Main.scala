77bf389ad4fab283cd983f70c45d342b059cbb57
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-01 12:46:19 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,34 +1,16 @@
-  /////////////////////
- // active patterns //
-/////////////////////
 
 
-case class Vector(x: Double, y: Double)
+  Excerpts from the Compose Conference (2016)
 
-
-sealed trait Cardinal
-case class NorthEast(dx: Double, dy: Double) extends Cardinal
-case class SouthEast(dx: Double, dy: Double) extends Cardinal
-case class SouthWest(dx: Double, dy: Double) extends Cardinal
-case class NorthWest(dx: Double, dy: Double) extends Cardinal
-
-object Cardinal {
-  def apply(vector: Vector): Cardinal = ???
-}
-
-
-def manhattan_distance(vector: Vector): Double =
-  Cardinal(vector) match {
-    // warning: match is not exhaustive, case NorthEast() is missing
-    case SouthEast(dx, dy) => dx + dy
-    case SouthWest(dx, dy) => dx + dy
-    case NorthWest(dx, dy) => dx + dy
-  }
-
-
-
-
-
+  v Introduction
+    - the conference
+    - the talks
+    - this talk
+  v Excerpts
+    - add a type parameter!
+    - composing network operating systems
+    -[active patterns]
+  > Conclusion
 
 
 
