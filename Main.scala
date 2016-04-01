daeeb1de5900090923acc6abf6ff45259dd66709
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-01 12:08:18 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -6,13 +6,14 @@
 case class Vector(x: Double, y: Double)
 
 object Polar {
-  def unapply(vector: Vector) =
-    Some((Math.atan2(vector.y, vector.x),
-          Math.sqrt(vector.x * vector.x + vector.y * vector.y)))
+  def angle(vector: Vector) = Math.atan2(vector.y, vector.x)
+  def magnitude(vector: Vector) =
+    Math.sqrt(vector.x * vector.x + vector.y * vector.y)
 }
 
 val point = Vector(3, 4)
-val Polar(angle, magnitude) = point
+val angle = Polar.angle(point)
+val magnitude = Polar.magnitude(point)
 
 
 
