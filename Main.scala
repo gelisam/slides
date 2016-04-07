58bcb62cdc51355962a5fd15011e0a631994d04a
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-07 10:55:18 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -6,6 +6,11 @@ WHERE: New York
 WHEN:  February 4-5, 2016 (those are weekdays, thank you KeaText!)
 WHAT:  Talks about strongly-typed functional programming
 
+       - Haskell
+       - Elm
+       - F#
+       - OCaml
+       - SML
 
 
 
