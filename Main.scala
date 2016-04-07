c35d3836130927b3a704d4d8130a5394c3bc459b
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-07 11:30:01 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,33 +1,16 @@
-  ///////////////////////////
- // add a type parameter! //
-///////////////////////////
 
 
-sealed trait List[A]
-case class Nil[A]()                        extends List[A]
-case class Cons[A](head: A, tail: List[A]) extends List[A]
+  Excerpts from the Compose Conference (2016)
 
-
-import java.net.URL
-import scala.xml.Document
-
-case class Webpage[A,F[_]](
-  location: URL,
-  content:  A,
-  title:    F[ String ],
-  links:    F[ List[(String,URL)] ],
-  referrer: Option[URL]
-)
-
-type Missing[A] = Unit
-type Present[A] = A
-def download(ref: Webpage[Unit,Missing]): Webpage[String,Missing] = ???
-def parse(page: Webpage[String,Missing]): Webpage[Document,Present] = ???
-
-
-
-
-
+  v Introduction
+    - the conference
+    - the talks
+    - this talk
+  v Excerpts
+    -[add a type parameter!]
+    - composing network operating systems
+    - active patterns
+  > Conclusion
 
 
 
