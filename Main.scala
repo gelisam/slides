a74a3f78d984466b0336d91c2c09b02b7a9aa04a
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-07 10:19:19 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -2,23 +2,23 @@
  // the talks //
 ///////////////
 
-- "[...] making abstract mathematics palatable", by Eugenia Cheng
+
+- "[...] Machine Learning, Types and Testing", by Leonid Rozenberg
+- "Add a type parameter! [...]", by Stephen Compall
+
+- "Composing Network Operating Systems", by Mindy Preston
+- "Chaos Testing at Jet", by Rachel Reese
+- "'There and Back Again' and What Happened After", by Kenneth Foner
+- "[...] F# Active Patterns [...]", by Paulmichael Blasucci
+- "Improving Type Error Localization [...]", by Pavlinovic et al.
+- "Cryptography and verification with Cryptol", by Austin Seipp
 
 
-- "Dynamic Typing in GHC", by Stephanie Weirich
 
 
 
+- "Functional Reactive Programming [...]", by Riccardo Terrell
 
-
-
-- "FLTKHS - Easy native GUIs in Haskell, Today!", by Aditya Siram
-- "Fun with GHCJSi", by Luite Stegeman
-- "Ionide and state of F# open source environment", by Jared Hester
-- "Liquid Types for Haskell", by Niki Vazou
-- "[...] Algorithmic Differentiation for OCaml", by Markus Mottl
-
-- "Analyzing Programs with Z3", by Tikhon Jelvis
 
 
 
