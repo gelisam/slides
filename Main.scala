2fafdc5dc5ff5b7d7b0c6040e30ebae06f9aceca
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-04-07 10:19:45 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,23 +1,30 @@
   ///////////////
- // the talks //
+ // this talk //
 ///////////////
 
 
-- "[...] Machine Learning, Types and Testing", by Leonid Rozenberg
+
 - "Add a type parameter! [...]", by Stephen Compall
 
 - "Composing Network Operating Systems", by Mindy Preston
-- "Chaos Testing at Jet", by Rachel Reese
-- "'There and Back Again' and What Happened After", by Kenneth Foner
+
+
 - "[...] F# Active Patterns [...]", by Paulmichael Blasucci
-- "Improving Type Error Localization [...]", by Pavlinovic et al.
-- "Cryptography and verification with Cryptol", by Austin Seipp
 
 
 
 
 
-- "Functional Reactive Programming [...]", by Riccardo Terrell
+
+
+
+
+
+
+
+
+
+
 
 
 
