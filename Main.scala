a7afa8854fe326db54e02b1685896a00e9ff0208
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-13 16:59:37 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -20,10 +20,21 @@ def processMany[
     strings.map(processString)
   )
 
+def processHelper[
+  L[X] <: TraversableLike[X, L[X]]
+](
+  strings: L[String]
+)(implicit
+  cbf1: CanBuildFrom[  L[String],      Future[Int], L[Future[Int]]  ],
+  cbf2: CanBuildFrom[  L[Future[Int]], Int,         L[Int]          ]
+)
+: Future[L[Int]] =
+  processMany(strings)
+
 
 val r1: Future[Int]       = processString("hello")
-val r2: Future[List[Int]] = processMany(List("hello", "world"))
-val r3: Future[Set[Int]]  = processMany(Set("hello", "world"))
+val r2: Future[List[Int]] = processHelper(List("hello", "world"))
+val r3: Future[Set[Int]]  = processHelper(Set("hello", "world"))
 
 r1.map(println) // 5
 r2.map(println) // List(5, 5)
