b964dced5cb1e09a7d92d2d12f2820d33bab4537
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-13 17:29:17 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,17 +1,23 @@
-
-def processString(string: String): String =
-  string + "!"
-
-def processList(strings: List[String]): List[Int] =
-  strings.map(processString)
-
-def processSet(strings: Set[String]): Set[Int] =
-  strings.map(processString)
+trait Functor[L[_]] {
+  def map[A,B](xs: L[A])(f: A => B): L[B]
+}
 
 
-println(processString("hello")) // 5
-println(processList(List("hello", "world"))) // List(5, 5)
-println(processSet(Set("hello", "world"))) // Set(5)
+def processString(string: String): Int =
+  string.length
+
+def processMany[L[_]](
+  strings: L[String]
+)(implicit
+  functor: Functor[L]
+)
+: L[Int] =
+  functor.map(strings)(processString)
+
+
+println(processString("hello")) // hello!
+println(processList(List("hello", "world"))) // List(hello!, world!)
+println(processSet(Set("hello", "world"))) // Set(hello!, world!)
 
 
 
