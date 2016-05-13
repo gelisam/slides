54864c65455645d281bcfcd401e7cf05753efb7b
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-13 17:07:57 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,45 +1,17 @@
-import scala.collection.TraversableLike
-import scala.collection.generic.CanBuildFrom
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+
+def processString(string: String): String =
+  string + "!"
+
+def processList(strings: List[String]): List[Int] =
+  strings.map(processString)
+
+def processSet(strings: Set[String]): Set[Int] =
+  strings.map(processString)
 
 
-def processString(string: String): Future[Int] =
-  Future.successful(string.length)
-
-def processMany[
-  L[X] <: TraversableLike[X, L[X]]
-](
-  strings: L[String]
-)(implicit
-  cbf1: CanBuildFrom[  L[String],      Future[Int], L[Future[Int]]  ],
-  cbf2: CanBuildFrom[  L[Future[Int]], Int,         L[Int]          ]
-)
-: Future[L[Int]] =
-  Future.sequence(
-    strings.map(processString)
-  )
-
-def processHelper[
-  L[X] <: TraversableLike[X, L[X]]
-](
-  strings: L[String]
-)(implicit
-  cbf1: CanBuildFrom[  L[String],      Future[Int], L[Future[Int]]  ],
-  cbf2: CanBuildFrom[  L[Future[Int]], Int,         L[Int]          ]
-)
-: Future[L[Int]] =
-  processMany(strings)
-
-
-val r1: Future[Int]       = processString("hello")
-val r2: Future[List[Int]] = processHelper(List("hello", "world"))
-val r3: Future[Set[Int]]  = processHelper(Set("hello", "world"))
-
-r1.map(println) // 5
-r2.map(println) // List(5, 5)
-r3.map(println) // Set(5)
-
+println(processString("hello")) // 5
+println(processList(List("hello", "world"))) // List(5, 5)
+println(processSet(Set("hello", "world"))) // Set(5)
 
 
 
