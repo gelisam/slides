9b95f992270a40ee73acd6847b02c647469564ca
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 00:55:19 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,4 +1,50 @@
 import java.util.*;
+import java.util.function.*;
+
+
+
+
+
+
+class MyList<A> {
+  LinkedList<A> impl;
+  
+  public MyList(LinkedList<A> impl) {
+    this.impl = impl;
+  }
+  
+  public <B> MyList<B> map(Function<A,B> f) {
+    LinkedList<B> result = new LinkedList<B>();
+    for(A element : impl) {
+      result.add(f.apply(element));
+    }
+    return new MyList<B>(result);
+  }
+  
+  public String toString() {
+    return impl.toString();
+  }
+}
+
+class MySet<A> {
+  HashSet<A> impl;
+  
+  public MySet(HashSet<A> impl) {
+    this.impl = impl;
+  }
+  
+  public <B> MySet<B> map(Function<A,B> f) {
+    HashSet<B> result = new HashSet<B>();
+    for(A element : impl) {
+      result.add(f.apply(element));
+    }
+    return new MySet<B>(result);
+  }
+  
+  public String toString() {
+    return impl.toString();
+  }
+}
 
 
 public class Main {
@@ -6,20 +52,16 @@ public class Main {
     return string.length();
   }
   
-  static LinkedList<Integer> processList(LinkedList<String> strings) {
-    LinkedList<Integer> result = new LinkedList<Integer>();
-    for(String string : strings) {
-      result.add(processString(string));
-    }
-    return result;
+  static MyList<Integer> processList(MyList<String> strings) {
+    return strings.map(
+      (String s) -> processString(s)
+    );
   }
   
-  static HashSet<Integer> processSet(HashSet<String> strings) {
-    HashSet<Integer> result = new HashSet<Integer>();
-    for(String string : strings) {
-      result.add(processString(string));
-    }
-    return result;
+  static MySet<Integer> processSet(MySet<String> strings) {
+    return strings.map(
+      (String s) -> processString(s)
+    );
   }
   
   
@@ -27,14 +69,14 @@ public class Main {
     System.out.println(processString("hello")); // 5
     
     LinkedList<String> v1 = new LinkedList<String>();
-    v1.addLast("hello");
-    v1.addLast("world");
-    System.out.println(processList(v1)); // [5, 5]
+    v1.add("hello");
+    v1.add("world");
+    System.out.println(processList(new MyList<String>(v1))); // [5, 5]
     
     HashSet<String> v2 = new HashSet<String>();
     v2.add("hello");
     v2.add("world");
-    System.out.println(processSet(v2)); // [5]
+    System.out.println(processSet(new MySet<String>(v2))); // [5]
   }
 }
 
