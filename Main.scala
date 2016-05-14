7556ce0927fffdd4ce90da5728c14d65522d933f
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 09:03:59 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,31 +1,7 @@
 
-def normalizeString(
-  string: String, partialMatch: Boolean
-): Future[String] =
-  normalizer.results.normalizeFromCorpus(datasetId, string, partialMatch)
- 
-def normalizeMany[M[X] <: TraversableLike[X, M[X]]](
-  strings: M[String],
-  partialMatch: Boolean
-)(implicit
-  cbfa: CanBuildFrom[M[String], Future[String], M[Future[String]]],
-  cbfb: CanBuildFrom[M[Future[String]], String, M[String]]
-)
-: Future[M[String]] =
-  Future.sequence(
-    strings.map { str =>
-      normalizeString(str, partialMatch)
-    }
-  )
- 
-def normalizeHelper[M[X] <: TraversableLike[X, M[X]]](
-  strings: M[String]
-)(implicit
-  cbfa: CanBuildFrom[M[String], Future[String], M[Future[String]]],
-  cbfb: CanBuildFrom[M[Future[String]], String, M[String]]
-)
-: Future[M[String]] =
-  normalizeList(strings, false)
+  "I officially hate Scala's approach to generic collections."
+
+                                       - Marc-Olivier Ricard
 
 
 
