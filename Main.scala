60fbe610f7d3675395354b82a6625c30ece7684e
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 09:32:55 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,22 +1,16 @@
 
-  "I officially hate Scala's approach to generic collections."
-
-                                       - Marc-Olivier Ricard
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   Type Classes
+   ===
+   [ original problem ]
+   - simpler problem
+   - TraversableLike and CanBuildFrom
+     - what are they?
+     - why are they so complicated?
+   - Type Classes
+     - problem
+     - solution
+     - cosmetics
+   - conclusion
 
 
 
