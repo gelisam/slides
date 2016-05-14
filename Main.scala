55f9a62a9537a4b64e7feb5158c51f728fa2e971
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 08:17:09 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,19 +1,37 @@
 
+class MyList:
+  def __init__(self, elems):
+    self.impl = list(elems)
+  
+  def map(self, f):
+    return MyList(map(f, self.impl))
+  
+  def __str__(self):
+    return str(self.impl)
+
+class MySet:
+  def __init__(self, elems):
+    self.impl = set(elems)
+  
+  def map(self, f):
+    return MySet(map(f, self.impl))
+  
+  def __str__(self):
+    return str(self.impl)
+
+
 def processString(string):
   return len(string)
 
 def processList(strings):
-  return map(processString, strings)
+  return strings.map(processString)
 
 def processSet(strings):
-  return set(map(processString, strings))
-
-
-
+  return strings.map(processString)
 
 print processString("hello")  # 5
-print processList(["hello", "world"])  # [5, 5]
-print processSet(set(["hello", "world"]))  # set([5])
+print processList(MyList(["hello", "world"]))  # [5, 5]
+print processSet(MySet(["hello", "world"]))  # set([5])
 
 
 
