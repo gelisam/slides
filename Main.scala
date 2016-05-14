0b92b46f0a6b416094afe36d7b83a4a8e3cfad85
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 17:34:40 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,20 +1,48 @@
 
+def normalizeString(
+  string: String, partialMatch: Boolean
+): Future[String] =
+  normalizer.results.normalizeFromCorpus(datasetId, string, partialMatch)
+
+def normalizeList(
+  strings: List[String], partialMatch: Boolean
+): Future[List[String]] =
+  Future.sequence(
+    strings.map { str =>
+      normalizeString(str, partialMatch)
+    }
+  )
+ 
+def normalizeHelper(
+  strings: List[String]
+): Future[List[String]] =
+  normalizeList(strings, false)
 
 
 
 
 
-                           Reusing Code
-                     with Different Containers
-                      and Different Languages
 
 
 
 
 
 
-                                                     a talk by
-                                               Samuel Gélineau
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
