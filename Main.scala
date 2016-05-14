7b85aecbbba23f18198d1fefead4f8744bfc2705
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-14 08:49:48 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,17 +1,20 @@
 
-   Type Classes
-   ===
-   - original problem
-   [ example problem ]
-     - Python solution
-     - Elm solution
-     - C++ solution
-     - Java solution
-     - Scala solution
-     - Haskell solution
-   - Type Classes
-     - in Haskell
-     - in Scala
+processString
+  String -> Int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
