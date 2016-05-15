f6d3ce559019937a3d9f914909a712a7a364d004
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-15 09:13:43 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,8 +1,8 @@
+#include <algorithm>
 #include <iostream>
+#include <list>
 #include <string>
 using namespace std;
-
-
 
 
 
@@ -10,9 +10,27 @@ int processString(string str) {
   return str.size();
 }
 
+list<int> processList(list<string> strs) {
+  list<int> result;
+  transform(
+    strs.begin(),
+    strs.end(),
+    inserter(result,result.end()),
+    ::processString
+  );
+  return result;
+}
+
 
 int main() {
   cout << processString("hello") << endl;
+  
+  list<int> r1 = processList(list<string>{"hello", "world"});
+  cout << "list(" << endl;
+  for(auto it = r1.begin(); it != r1.end(); ++it) {
+    cout << "  " << *it << endl;
+  }
+  cout << ")" << endl;
 }
 
 
