8bfb00563568b676eb5a6f14a3718dcba3c0e42d
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-15 12:39:40 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -4,17 +4,17 @@
 def processString(string: String): Int =
   string.length
 
-def processIterable[L[X]](strings: L[String]): L[Int] = ???
+def processIterable[
+  L[X]
+](
+  strings: L[String]
+)
+: L[Int] = ???
   //strings.map { string =>
   //  processString(string)
   //}
 
-
-
-
-
-
-println(processString("hello")) // 5
+println(processString("hello")) // hello!
 println(processIterable(List("hello", "world")): List[Int])
 println(processIterable(Set("hello", "world")): Set[Int])
 
