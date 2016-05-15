78a376e4d3f442b6c82ad91df289a293bf7b8a61
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-15 09:13:47 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,9 +1,9 @@
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <set>
 #include <string>
 using namespace std;
-
 
 
 int processString(string str) {
@@ -21,6 +21,17 @@ list<int> processList(list<string> strs) {
   return result;
 }
 
+set<int> processSet(set<string> strs) {
+  set<int> result;
+  transform(
+    strs.begin(),
+    strs.end(),
+    inserter(result,result.end()),
+    ::processString
+  );
+  return result;
+}
+
 
 int main() {
   cout << processString("hello") << endl;
@@ -28,6 +39,13 @@ int main() {
   list<int> r1 = processList(list<string>{"hello", "world"});
   cout << "list(" << endl;
   for(auto it = r1.begin(); it != r1.end(); ++it) {
+    cout << "  " << *it << endl;
+  }
+  cout << ")" << endl;
+  
+  set<int> r2 = processSet(set<string>{"hello", "world"});
+  cout << "set(" << endl;
+  for(auto it = r2.begin(); it != r2.end(); ++it) {
     cout << "  " << *it << endl;
   }
   cout << ")" << endl;
