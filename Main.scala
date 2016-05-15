a6bab32d90deb799d3723cdbfe8b1ca3f2338c50
Samuel Gélineau <gelisam+github@gmail.com>
2016-05-15 16:59:30 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -14,6 +14,17 @@ case class MyList[A](impl: List[A]) extends Mappable[A, MyList[A]] {
     MyList(impl.map(f))
 }
 
+class ChildList[A](impl: List[A]) extends MyList[A](impl) with Mappable[A, ChildList[A]] {
+  override def map(
+    f: A => A
+  )
+  : ChildList[A] =
+    new ChildList(impl.map(f))
+  
+  override def toString: String =
+    s"ChildList(${impl})"
+}
+
 case class MySet[A](impl: Set[A]) extends Mappable[A, MySet[A]] {
   def map(
     f: A => A
@@ -38,6 +49,7 @@ def processIterable[
 
 println(processString("hello")) // hello!
 println(processIterable(MyList(List("hello", "world")))) // MyList(List(hello!, world!))
+println(processIterable(new ChildList(List("hello", "world")))) // ChildList(List(hello!, world!))
 println(processIterable(MySet(Set("hello", "world")))) // MySet(Set(hello!, world!))
 
 
