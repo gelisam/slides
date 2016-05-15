8f5a3803c59e40cb40af404d4132518770a39d5f
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-15 14:08:45 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,37 +1,51 @@
+trait Eq1[S[_], T[_]] {
+  def at[A]: S[A] =:= T[A]
+}
+
+object Eq1 {
+  implicit def refl1[T[_]]: Eq1[T,T] = new Eq1[T,T] {
+    def at[A]: T[A] =:= T[A] = implicitly
+  }
+}
+
 
 trait Mappable[A] {
-  type Self
+  type Self[X]
   
   def map[L[X],B](
     f: A => B
   )(implicit
-    eq: Self =:= L[A]
+    eq1: Eq1[Self,L]
   )
   : L[B]
 }
 
 case class MyList[A](impl: List[A]) extends Mappable[A] {
-  type Self = MyList[A]
+  type Self[X] = MyList[X]
   
   def map[L[X],B](
     f: A => B
   )(implicit
-    eq: Self =:= L[A]
+    eq1: Eq1[Self,L]
   )
-  : L[B] =
+  : L[B] = {
+    implicit val eqB = eq1.at[B]
     MyList(impl.map(f))
+  }
 }
 
 case class MySet[A](impl: Set[A]) extends Mappable[A] {
-  type Self = MySet[A]
+  type Self[X] = MySet[X]
   
   def map[L[X],B](
     f: A => B
   )(implicit
-    eq: Self =:= L[A]
+    eq1: Eq1[Self,L]
   )
-  : L[B] =
+  : L[B] = {
+    implicit val eqB = eq1.at[B]
     MySet(impl.map(f))
+  }
 }
 
 
@@ -43,12 +57,12 @@ def processIterable[
 ](
   strings: L[String]
 )(implicit
-  eq: strings.Self =:= L[String]
+  eq1: Eq1[strings.Self,L]
 )
 : L[Int] =
   strings.map { string =>
     processString(string)
-  }
+  }(eq1)
 
 println(processString("hello")) // hello!
 println(processIterable(MyList(List("hello", "world"))))
