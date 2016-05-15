ac21615fac0842163945e3e6da0f5372068a0bcc
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-15 11:02:01 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -6,9 +6,22 @@ public class Main {
     return string.length();
   }
   
+  static LinkedList<Integer> processList(LinkedList<String> strings) {
+    LinkedList<Integer> result = new LinkedList<Integer>();
+    for(String string : strings) {
+      result.add(processString(string));
+    }
+    return result;
+  }
+  
   
   public static void main(String[] args) {
     System.out.println(processString("hello")); // 5
+    
+    LinkedList<String> v1 = new LinkedList<String>();
+    v1.addLast("hello");
+    v1.addLast("world");
+    System.out.println(processList(v1)); // [5, 5]
   }
 }
 
