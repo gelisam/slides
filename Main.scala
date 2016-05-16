0973e9593c92177a387f7d3aeb7111ac7a109f8f
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-16 13:23:06 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,16 +1,20 @@
 
-   Type Classes
-   ===
-   - original problem
-   [ simpler problem ]
-   - TraversableLike and CanBuildFrom
-     - what are they?
-     - why are they so complicated?
-   - Type Classes
-     - problem
-     - solution
-     - cosmetics
-   - conclusion
+processString
+  String -> Int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
