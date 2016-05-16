3ffc4088f32d1056a1c088f69e601f5a8a3b6561
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-16 11:46:38 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,8 +1,8 @@
 
    Type Classes
    ===
-   [ original problem ]
-   - simpler problem
+   - original problem
+   [ simpler problem ]
    - TraversableLike and CanBuildFrom
      - what are they?
      - why are they so complicated?
