668dc4a0adc9f48564c004d90c4ba93c7823c86d
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 11:36:17 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,28 +1,28 @@
 
+trait Shape {
+  def area: Double
+}
+
 case class Rectangle(
   width: Double,
   height: Double
-)
+) extends Shape {
+  def area: Double =
+    width * height
+}
 
 case class Circle(
   radius: Double
-)
+) extends Shape {
+  def area: Double =
+    scala.math.Pi * Math.pow(radius, 2)
+}
 
 
-def rectangleArea(rectangle: Rectangle): Double =
-  rectangle.width * rectangle.height
-
-def circleArea(circle: Circle): Double =
-  scala.math.Pi * Math.pow(circle.radius, 2)
 
 
-def isRectangleTooLarge(rectangle: Rectangle): Boolean =
-  rectangleArea(rectangle) > 9000
 
-def isCircleTooLarge(circle: Circle): Boolean =
-  circleArea(circle) > 9000
-
-def isAreaTooLarge(shape: ???): Boolean =
+def isAreaTooLarge(shape: Shape): Boolean =
   shape.area > 9000
 
 
