11c4e7c39aed04bd65aa53acd14de9ca0d9876dc
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 10:13:12 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,21 +1,23 @@
->>> List(1,2,3).fmap { x => s"${x}!" }
+>>> List(1,2,3).mapTo[List] { x => s"${x}!" }
 List(1!, 2!, 3!)
 
->>> Set(1,2,3).fmap { x => s"${x}!" }
+>>> Set(1,2,3).mapTo[Set] { x => s"${x}!" }
 Set(1!, 2!, 3!)
 
 >>> import scala.collection.immutable.BitSet
->>> BitSet(1,2,3).fmap { x => s"${x}!" }
-TYPE ERROR
+>>> BitSet(1,2,3).mapTo[SortedSet] { x => s"${x}!" }
+SortedSet(1, 2, 3)
 
 
 def processMany[
-  L[X] <: Mappable[X, L[X]]
+  L[X] <: MapConvertible[X, L[X]]
 ](
   strings: L[String]
+)(implicit
+  cbf: Convertible[  L[String], Int, L[Int]  ]
 )
 : L[Int] =
-  strings.fmap(processString)
+  strings.mapTo[L](processString)
 
 
 
