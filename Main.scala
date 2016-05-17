81fab3bdad34b6be70cb327b37dc5f0998b97420
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 14:38:00 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -4,6 +4,10 @@
    - TraversableLike and CanBuildFrom are complicated
    - Type Classes are more flexible
    - use type classes!
+   
+   - a technique, not a library
+   - basics are easy, cosmetics are optional
+   - define your own type classes! 
 
 
 
