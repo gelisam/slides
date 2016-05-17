3d9678e955833a6fdf419b76370e4fbb0ac56ed4
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 12:32:14 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,21 +1,37 @@
 
-   Type Classes
-   ===
-   - original problem
-   - simpler problem
-   - TraversableLike and CanBuildFrom
-     - what are they?
-     - why are they so complicated?
-   - Type Classes
-     - problem
-     - solution
-     [ cosmetics ]
-   - conclusion
+case class Rectangle(
+  width: Double,
+  height: Double
+)
+
+case class Circle(
+  radius: Double
+)
 
 
+def rectangleArea(rectangle: Rectangle): Double =
+  rectangle.width * rectangle.height
+
+def circleArea(circle: Circle): Double =
+  scala.math.Pi * Math.pow(circle.radius, 2)
 
 
+def isRectangleTooLarge(rectangle: Rectangle): Boolean =
+  rectangleArea(rectangle) > 9000
 
+def isCircleTooLarge(circle: Circle): Boolean =
+  circleArea(circle) > 9000
+
+
+// >>> isAreaTooLarge(Rectangle(100,100), rectangleArea)
+// true
+// >>> isAreaTooLarge(Circle(50), circleArea)
+// false
+def isAreaTooLarge[A](
+  shape: A,
+  area: A => Double
+): Boolean =
+  area(shape) > 9000
 
 
 
