872b36060f54c00e90c5e953f308f2d5c950e179
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 10:10:51 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,21 +1,21 @@
->>> List(1,2,3).monoMap { x => x + 1 }
-List(2, 3, 4)
+>>> List(1,2,3).fmap { x => s"${x}!" }
+List(1!, 2!, 3!)
 
->>> Set(1,2,3).monoMap { x => x + 1 }
-Set(2, 3, 4)
+>>> Set(1,2,3).fmap { x => s"${x}!" }
+Set(1!, 2!, 3!)
 
 >>> import scala.collection.immutable.BitSet
->>> BitSet(1,2,3).monoMap { x => x + 1 }
-BitSet(2, 3, 4)
+>>> BitSet(1,2,3).fmap { x => s"${x}!" }
+TYPE ERROR
 
 
 def processMany[
-  L[X] <: MonoMappable[X, L[X]]
+  L[X] <: Mappable[X, L[X]]
 ](
   strings: L[String]
 )
 : L[Int] =
-  strings.monoMap(processString)
+  strings.fmap(processString)
 
 
 
