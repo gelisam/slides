d0d9eef83838f9d07cc6f941a899b8f60756f48f
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 11:13:55 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,14 +1,23 @@
 >>> List(1,2,3).monoMap { x => x + 1 }
 List(2, 3, 4)
 
->>> Set(1,2,3).fmap { x => s"${x}!" }
-Set(1!, 2!, 3!)
+>>> Set(1,2,3).monoMap { x => x + 1 }
+Set(2, 3, 4)
 
 >>> import scala.collection.immutable.BitSet
->>> BitSet(1,2,3).mapTo[SortedSet] { x => s"${x}!" }
-SortedSet(1!, 2!, 3!)
+>>> BitSet(1,2,3).monoMap { x => x + 1 }
+BitSet(2, 3, 4)
 
 
+def processMany[
+  L[X] <: MonoMappable[X, L[X]]
+](
+  strings: L[String]
+)(implicit
+  cbf: CanBuildFrom[  L[String], Int, L[Int]  ]
+)
+: L[Int] =
+  strings.monoMap(processString)
 
 
 
