6dfd68c5ea0853f9543dcf81f4149f4ffba9b629
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-17 11:10:03 -0400

    new slide

--- Main.scala
+++ Main.scala
@@ -1,11 +1,11 @@
->>> List(1,2,3).map { x => x + 1 }
+>>> List(1,2,3).monoMap { x => x + 1 }
 List(2, 3, 4)
 
->>> Set(1,2,3).map { x => s"${x}!" }
+>>> Set(1,2,3).fmap { x => s"${x}!" }
 Set(1!, 2!, 3!)
 
 >>> import scala.collection.immutable.BitSet
->>> BitSet(1,2,3).map { x => s"${x}!" }
+>>> BitSet(1,2,3).mapTo[SortedSet] { x => s"${x}!" }
 SortedSet(1!, 2!, 3!)
 
 
