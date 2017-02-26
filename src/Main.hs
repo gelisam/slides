360510083a4141397b51d44500d1c1bd7ba70e5c
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-26 12:24:05 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,16 @@ class Category k => Arrow k where
   arr   :: (a -> b) -> k a b
   first :: k a b    -> k (a,r) (b,r)
 
+second  :: Arrow k
+        => k a b    -> k (l,a) (l,b)
+
+(***) :: Arrow k => k a b -> k a' b' -> k (a,a') (b,b')
+(&&&) :: Arrow k => k a b -> k a  b' -> k a      (b,b')
+
+swap    :: Arrow k => k (a,b)     (b,a)
+assoc   :: Arrow k => k (a,(b,c)) ((a,b),c)
+unassoc :: Arrow k => k ((a,b),c) (a,(b,c))
+-- ...
 
 
 
@@ -22,17 +32,14 @@ class Category k => Arrow k where
 
 
 
+second   = undefined
 
+(***)    = undefined
+(&&&)    = undefined
 
-
-
-
-
-
-
-
-
-
+swap     = undefined
+assoc    = undefined
+unassoc  = undefined
 
 
 
