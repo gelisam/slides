21b057f841b209890b703063b974a08c99dc4a69
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-26 18:17:43 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -13,8 +13,13 @@ rowsFirst, colsFirst :: Arrow k
 rowsFirst k1 k2 k3 k4 = (k1 *** k2) >>> (k3 *** k4)
 colsFirst k1 k2 k3 k4 = (k1 >>> k3) *** (k2 >>> k4)
 
-
+--data Kleisli m a b = Kleisli { runKleisli :: a -> m b }
 main :: IO ()
 main = do
-    print $ rowsFirst (++ "1") (++ "2") (++ "3") (++ "4") ("","")
-    print $ colsFirst (++ "1") (++ "2") (++ "3") (++ "4") ("","")
+    ((),()) <- runKleisli (rowsFirst (go 1) (go 2) (go 3) (go 4)) ((),())
+    putStrLn "---"
+    ((),()) <- runKleisli (colsFirst (go 1) (go 2) (go 3) (go 4)) ((),())
+    return ()
+  where
+    go :: Int -> Kleisli IO () ()
+    go x = Kleisli $ \() -> print x
