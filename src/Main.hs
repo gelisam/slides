418b4ddaa4cbf3a885f4999159152877dcad85b3
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 10:13:49 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,21 @@ class Category k => Arrow k where
   arr   :: (a -> b) -> k a b
   first :: k a b    -> k (a,r) (b,r)
 
-second  :: Arrow k
-        => k a b    -> k (l,a) (l,b)
-
+-- 2D composition using Arrow
+(>>>) :: Arrow k => k a b -> k b c -> k a c
 (***) :: Arrow k => k a b -> k a' b' -> k (a,a') (b,b')
-(&&&) :: Arrow k => k a b -> k a  b' -> k a      (b,b')
-
-swap    :: Arrow k => k (a,b)     (b,a)
-assoc   :: Arrow k => k (a,(b,c)) ((a,b),c)
-unassoc :: Arrow k => k ((a,b),c) (a,(b,c))
--- ...
 
 
-
-
-
-
-
-
+--             +--- a ---+
+--             | +- a -+ |
+--             | |     | |                +---- (a, a') ----+
+--             | |     | |                | +- a -++- a' -+ |
+--             | +- b -+ |                | |     ||      | |
+--             | +- b -+ |                | |     ||      | |
+--             | |     | |                | +- b -++- b' -+ |
+--             | |     | |                +---- (b, b') ----+
+--             | +- c -+ |
+--             +--- c ---+
 
 
 
@@ -32,20 +29,13 @@ unassoc :: Arrow k => k ((a,b),c) (a,(b,c))
 
 
 
-second   = undefined
-
-(***)    = undefined
-(&&&)    = undefined
-
-swap     = undefined
-assoc    = undefined
-unassoc  = undefined
 
 
 
 
 
-
+(>>>) = undefined
+(***) = undefined
 
 
 
