58ddc2dcb8da4b6c6fd6fc9371198da056f5ca96
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 09:37:22 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,15 @@
          --                                     --
          --  Functor               Category     --
          --                                     --
-         --                        ???          --
+         --                        Bifunctor    --
+         --                                     --
+         --                        Associative  --
+         --                                     --
+         --                        Monoidal     --
+         --                                     --
+         --                        Symmetric    --
+         --                                     --
+         --                        Cartesian    --
          --                                     --
          --  Applicative           Arrow        --
          --                                     --
