29e6edade624aa0e82c189243af44a48ecb1d66f
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 09:15:35 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,16 @@
 
-data Bash a b
 
-ls    :: FilePath -> Bash a        [FilePath]
-grep  :: RegExp   -> Bash [String] [String]
-arr   :: (a -> b) -> Bash a        b
-(***) :: Bash a b
-      -> Bash a' b'
-      -> Bash (a,a') (b,b')
-
-
--- $ BOTH="foo,bar"
--- $ IN1="$(echo "$BOTH" | cut -d, -f1)"
--- $ IN2="$(echo "$BOTH" | cut -d, -f2)"
--- $ OUT1="$(echo -n "$IN1" | ghc -e 'interact reverse')"
--- $ OUT2="$(echo -n "$IN2" | ghc -e 'interact (map Data.Char.toUpper)')"
--- echo "$OUT1,$OUT2"
--- oof,BAR
+         -----------------------------------------
+         --                                     --
+         --                                     --
+         --                                     --
+         --                                     --
+         --             Limitations             --
+         --                                     --
+         --                                     --
+         --                                     --
+         --                                     --
+         -----------------------------------------
 
 
 
@@ -28,21 +23,6 @@ arr   :: (a -> b) -> Bash a        b
 
 
 
-
-
-
-
-
-
-
-
-
-
-data RegExp
-ls    = undefined
-grep  = undefined
-arr   = undefined
-(***) = undefined
 
 
 
