d8bd6012ed9e8cbb5f6ae4ce5fd81bed0b2712a7
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 09:28:26 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,14 @@
 
-data Start
-data Ready
-data Waiting
 
-data Protocol a b
-handshake  ::            Protocol Start   Ready
-send       :: Message -> Protocol Ready   Waiting
-waitForAck ::            Protocol Waiting Ready
-disconnect ::            Protocol a       Start
-multiplex  ::            Protocol a       (a, Start)
-
-doubleSend :: Message -> Protocol (Ready, Ready) (Waiting, Ready)
-              -- (Ready, Ready)
-doubleSend msg = first (send msg)
-              -- (Waiting, Ready)
-             >>> arr (\(_,r) -> (r,r))
-              -- (Ready, Ready)
-             >>> first (send msg)
+         -----------------------------------------
+         --                                     --
+         --  Functor               Category     --
+         --                                     --
+         --  Applicative           ???          --
+         --                                     --
+         --  Monad                 Arrow        --
+         --                                     --
+         -----------------------------------------
 
 
 
@@ -30,27 +22,6 @@ doubleSend msg = first (send msg)
 
 
 
-
-
-
-
-
-
-data Message
-handshake  = undefined
-send       = undefined
-waitForAck = undefined
-disconnect = undefined
-multiplex  = undefined
-
-(>>>) :: Protocol a b -> Protocol b c -> Protocol a c
-(>>>) = undefined
-
-arr :: (a -> b) -> Protocol a b
-arr = undefined
-
-first :: Protocol a b -> Protocol (a,r) (b,r)
-first = undefined
 
 
 
