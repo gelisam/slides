144f917b4b4d0513a108ca222947f5e11732ff0a
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 09:11:26 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,11 @@
 
 
-         -----------------------------------------
-         --                                     --
-         --                                     --
-         --                                     --
-         --                                     --
-         --             Limitations             --
-         --                                     --
-         --                                     --
-         --                                     --
-         --                                     --
-         -----------------------------------------
+data Bash a b
+
+ls     :: FilePath -> Bash a        [FilePath]
+grep   :: RegExp   -> Bash [String] [String]
+
+--sort :: Bash [String] [String]
 
 
 
@@ -24,6 +19,24 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+data RegExp
+ls   = undefined
+grep = undefined
+pure = undefined
+save = undefined
+
+arr :: (a -> b) -> Bash a b
+arr = undefined
 
 
 
