f830617fae85561f5a26e698be2572ae32d4a383
Samuel Gélineau <gelisam+github@gmail.com>
2017-02-27 11:35:39 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,55 @@
-{-# LANGUAGE Arrows #-}
-import Prelude hiding ((.), id)
-import Control.Arrow
-import Control.Category
 
---class Arrow k => ArrowApply k where
---  app :: k (k a b, a) b
+data Bash a b
 
---               (>>=) :: m a -> (a ->   m b) ->   m b
-bind :: ArrowApply k => k u a -> (a -> k u b) -> k u b
-bind kua f = proc u -> do
-    a   <- kua   -< u
-    kub <- arr f -< a
-    b   <- app   -< (kub, u)
-    returnA      -< b
+ls    :: FilePath -> Bash a        [FilePath]
+grep  :: RegExp   -> Bash [String] [String]
+
+
+
+
+
+
+
+
+-- $ ls myfolder | grep png
+-- image1.png
+-- image2.png
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data RegExp
+ls    = undefined
+grep  = undefined
+
+
+
+
+
+
+
+
+
+
+
 
 
 
