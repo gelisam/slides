fe87a0ce2a30bcfe2450f0f0e44ad5ac3ec09d39
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-05 12:25:36 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 
 
-type Setter' s   a   = (a -> a) -> (s -> s)
-type Setter  s t a b = (a -> b) -> (s -> t)
+type Setter' s a     = (a -> a) -> (s -> s)
+type Setter  s a b t = (a -> b) -> (s -> t)
 
 type FGH a = F (G (H a))
 type  GH a =    G (H a)
@@ -15,7 +15,7 @@ type  GH a =    G (H a)
 --        |      |      |
 --        v      v      v
 -- fffmap = fmap . fmap . fmap :: (a -> b) -> (FGH a -> FGH b)
--- fffmap = fmap . fmap . fmap :: Setter (FGH a) (FGH b) a b
+-- fffmap = fmap . fmap . fmap :: Setter (FGH a) a b (FGH b)
 --        ^      ^      ^      ^
 --        |      |      |      |
 --  s = FGH a   GH a   H a     a ----.
