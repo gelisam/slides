9070c0a23d287e97ae98733613c3875fac085c2c
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-05 11:28:52 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,13 @@
-{-# LANGUAGE RankNTypes #-}                                                                                            {-# LANGUAGE RecordWildCards #-}
-import Control.Monad.Trans.State
+{-# LANGUAGE RankNTypes #-}
+
 
 type Setter' s a = (a -> a) -> (s -> s)
 
-(+=) :: Num a => Setter' s a -> a -> State s ()
-setter += dx = modify $ setter (+ dx)
+
+-- fmap :: Functor f => (a -> a) -> (f a -> f a)
 
 
-data Game   = Game   { _player :: Player }
-data Player = Player { _pos    :: Vec2D  }
-data Vec2D  = Vec2D  { _x, _y  :: Int    }
-
-movePlayer :: (Int, Int) -> State Game ()
-movePlayer (dx, dy) = do
-  player.pos.x += dx
-  player.pos.y += dy
+-- fmap :: Functor f => Setter' (f a) a
 
 
 
@@ -55,17 +48,26 @@ movePlayer (dx, dy) = do
 
 
 
-infix 4 +=
 
 
-player :: Setter' Game   Player
-pos    :: Setter' Player Vec2D
-x      :: Setter' Vec2D  Int
-y      :: Setter' Vec2D  Int
-player f (Game   {..}) = Game   { _player = f _player, .. }
-pos    f (Player {..}) = Player { _pos    = f _pos   , .. }
-x      f (Vec2D  {..}) = Vec2D  { _x      = f _x     , .. }
-y      f (Vec2D  {..}) = Vec2D  { _y      = f _y     , .. }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
