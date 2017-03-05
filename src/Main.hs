266efcb314d2641f7628313a105fa27ac41b0fdd
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-05 12:23:25 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -7,13 +7,13 @@ type Setter  s t a b = (a -> b) -> (s -> t)
 type FGH a = F (G (H a))
 type  GH a =    G (H a)
 
-
-
---          GH a -> GH b
---               |
---               |  H a -> H b
---               |      |
---               v      v
+--  FGH a -> FGH b
+--        |
+--        | GH a -> GH b
+--        |      |
+--        |      |  H a -> H b
+--        |      |      |
+--        v      v      v
 -- fffmap = fmap . fmap . fmap :: (a -> b) -> (FGH a -> FGH b)
 
 
