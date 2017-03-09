880c37d358ddb1837b14e4b57c06e00459cc8bec
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-09 12:51:02 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,57 +1,64 @@
+{-# LANGUAGE RankNTypes #-}
+
+type Func      a   s   =                                    a  ->         s
+type Setter'   a   s   =                            (a ->   a) -> (s ->   s)
+type Setter    a b s t =                            (a ->   b) -> (s ->   t)
+type Fold      a   s   = forall m. Monoid m      => (a ->   m) -> (s ->   m)
+type Getter    a   s   = forall e.                  (a ->   e) -> (s ->   e)
+type Traversal a b s t = forall f. Applicative f => (a -> f b) -> (s -> f t)
+type Lens      a b s t = forall f. Functor f     => (a -> f b) -> (s -> f t)
+
+(.) :: Lens u v s t -> Lens a b u v -> Lens a b s t
 
 
-type Back b a = a -> b
 
-(.) :: Back c b -> Back b a -> Back c a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data Const m a = Const m
+
 (.) f g x = f (g x)
-
-(>>>) :: Back b a -> Back c b -> Back c a
-(>>>) = flip (.)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 main :: IO ()
