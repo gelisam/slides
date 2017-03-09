1b3024fdac8b541f2660e58025ade36b5145e17c
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-09 13:21:12 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,12 @@ type Func a b = a -> b
 
 (.) :: (a -> b) -> (b -> c) -> (a -> c)
 (.) f g x = f (g x)
-
-
-
-
-
+--             ^ ^
+--             | |
+--             | expected: b
+--             | actual:   a
+--             |
+--           b -> c
 
 
 
