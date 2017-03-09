8fbca4541ba7a2cc0adc9ce15bdfa5321f86bc03
Samuel Gélineau <gelisam+github@gmail.com>
2017-03-09 14:24:45 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
 
 
-type Setter' s a = (a -> a) -> (s -> s)
-
+type Setter' s   a   = (a -> a) -> (s -> s)
+type Setter  s t a b = (a -> b) -> (s -> t)
 
 -- fmap :: Functor f => (a -> a) -> (f a -> f a)
+-- fmap :: Functor f => (a -> b) -> (f a -> f b)
 
-
--- fmap :: Functor f => Setter' (f a) a
-
+-- fmap :: Functor f => Setter' (f a)       a
+-- fmap :: Functor f => Setter  (f a) (f b) a b
 
 
 
