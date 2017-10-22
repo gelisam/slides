a037a8116e6fad246ec88ef6f34132d9d2a1e320
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-22 08:30:40 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,21 +1,21 @@
----------------------------------------------------------------------------
---                                                                       --
---  What is an incidental dependency?                                    --
---                                                                       --
---                                                                       --
---    incidental (adj.): accompanying but not a major                    --
---                       part of something.                              --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
----------------------------------------------------------------------------
+data Pizza = Pizza
+  { toppings :: Set Topping
+  }
+
+addTopping :: Topping -> Pizza -> Maybe Pizza
+addTopping topping pizza@(Pizza {..}) = do
+  guard (length toppings < 3)
+  return $ pizza { toppings = insert topping toppings }
+
+priceOrder :: Map Pizza Int -> Int
+priceOrder order = [10 * count | (pizza, count) <- toList order]
+
+
+
+
+
+
+
+
+
+
