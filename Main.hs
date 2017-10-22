f00954dfc82cf04d8b99959416aae7ba9bc1a315
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-21 23:54:28 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -6,12 +6,12 @@ data Pizza = Pizza
 
 data PizzaUpdate
   = AddTopping Topping
-
+  | AddExtra   Topping
 
 applyPizzaUpdate :: Pizza -> PizzaUpdate -> Pizza
 applyPizzaUpdate pizza@(Pizza {..}) = \case
   AddTopping topping -> pizza { toppings = insert topping toppings }
-
+  AddExtra   topping -> pizza { extras   = insert topping extras   }
 
 
 
