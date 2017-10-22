110b2e8845beba428e9155f2f9ee1f32116ceeb0
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-21 23:53:42 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,21 +1,21 @@
----------------------------------------------------------------------------
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---         Incidental dependencies in a client/server architecture       --
---                                      ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
---                                                                       --
----------------------------------------------------------------------------
+data Pizza = Pizza
+  { toppings :: Set Topping
+  , extras   :: Set Topping
+  }
+
+
+data PizzaUpdate
+  = AddTopping Topping
+
+
+applyPizzaUpdate :: Pizza -> PizzaUpdate -> Pizza
+applyPizzaUpdate pizza@(Pizza {..}) = \case
+  AddTopping topping -> pizza { toppings = insert topping toppings }
+
+
+
+
+
+
+
+
