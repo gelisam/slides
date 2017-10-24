4ecd4c17235940eb7364b8a2f161a209dd24a4db
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 19:14:44 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -10,7 +10,7 @@ Cons 1 (Cons 2 (Cons 3 Nil)) ==> Cons 1 (Cons 3 Nil)
  no change             .------- Keep ------.
                       /                     \
                   2 ==> 3        Cons 3 Nil ==> Nil
-
+                 Replace 3          Replace Nil
 
 
 
