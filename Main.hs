35a5c195caaefb9ed49466d3c7ec184c81106b74
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 18:14:13 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -8,14 +8,14 @@ instance (Diff a, Diff b) => Diff (a, b) where
   apply (pX, pY) (x, y) = (apply pX, apply pY)
 
 
+data PatchEither a b
+  = Keep (Patch a) (Patch b)
+  | Replace (Either a b)
+  deriving Generic
 
-
-
-
-
-
-
-
-
-
+instance (Diff a, Diff b) => Diff (Either a b) where
+  type Patch (Either a b) = PatchEither a b
+  apply (Keep pX _ ) (Left  x) = Left  (apply pX x)
+  apply (Keep _  pY) (Right y) = Right (apply pY y)
+  apply (Replace e)  _         = e
 
