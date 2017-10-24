af654df3a796db4c888926a30d32c392c3383137
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 16:14:31 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,21 +1,21 @@
 data Pizza = Pizza
   { toppings :: Set Topping
   , extras   :: Set Topping
-  }
+  } deriving (Show, Eq, Ord, Generic)
 
-instance Show Pizza where
-  show (Pizza {..}) = "Pizza { toppings = " ++ show toppings
-                         ++ ", extras   = " ++ show extras ++ " }"
 
-instance Eq Pizza where
-  p1 == p2 = (toppings p1 == toppings p2)
-          && (extras   p1 == extras   p2)
 
-instance Ord Pizza where
-  p1 <= p2 = toppings p1 <  toppings p2
-         || (toppings p1 == toppings p2 && extras p1 <= extras p2)
 
-instance ToJSON Pizza where
-  toJSON (Pizza {..}) = object [ "toppings" .= toppings
-                               , "extras"   .= extras
-                               ]
+
+
+
+
+
+
+
+
+
+instance ToJSON Pizza
+
+
+
