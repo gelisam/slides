78e51f429594d65089111d6e2d781c3c3db68ddf
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 12:55:57 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -7,8 +7,8 @@
 --         Incidental dependencies in a client/server architecture       --
 --                                                                       --
 --           1. Incidental dependencies                                  --
---         > 2. Serializable updates                                     --
---           3. Datatype-generic programming                             --
+--           2. Serializable updates                                     --
+--         > 3. Datatype-generic programming                             --
 --           4. Type-safe diff for families of datatypes (gdiff)         --
 --           5. Datatype-generic diff                                    --
 --                                                                       --
