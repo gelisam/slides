088a859644f653683090dbac2720dd97f6b36852
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 18:14:03 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -3,9 +3,9 @@ class Diff a where
   apply :: Patch a -> a -> a
 
 
-
-
-
+instance (Diff a, Diff b) => Diff (a, b) where
+  type Patch (a, b) = (Patch a, Patch b)
+  apply (pX, pY) (x, y) = (apply pX, apply pY)
 
 
 
