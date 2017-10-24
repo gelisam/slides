17d382b7757ffe6151e1ad90f78511dde60ac4ef
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 17:32:54 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,21 +1,21 @@
-data EditScript :: [*] -> [*] -> * where
-  Ins :: ... -> EditScript ts (S:ts')
-  Del :: ... -> EditScript (S:ts) ts'
-  Cpy :: ... -> EditScript (S:ts) (S:ts')
-  End :: EditScript [] []
-  deriving Generic  -- error: Can't derive Generic, EditScript
-                    --        must be a vanilla data constructor
+-- Those generic solutions don't compose!
 
-genericEq :: Generic a => a -> a -> Bool
-genericEq x y = toRep p1 == toRep p2
+gdiff   :: VanillaType -> FancyType
+generic :: VanillaType -> [Instance]
 
-instance (Eq a, Eq b) => Eq (a, b) where
-  (x1,y1) == (x2,y2) = (x1 == x2) && (y1 == y2)
 
-instance (Eq a, Eq b) => Eq (Either a b) where
-  Left  x1 == Left  x2 = x1 == x2
-  Right y1 == Right y2 = y1 == y2
-  _        == _        = False
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
