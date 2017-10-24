af4e92d947274e2c69eed99f521cc9792564a349
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 19:03:13 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -3,19 +3,19 @@ class Diff a where
   apply :: Patch a -> a -> a
 
 
-instance (Diff a, Diff b) => Diff (a, b) where
-  type Patch (a, b) = (Patch a, Patch b)
-  apply (pX, pY) (x, y) = (apply pX, apply pY)
+Cons 1 (Cons 2 (Cons 3 Nil)) ==> Cons 1 (Cons 3 Nil)
 
 
-data PatchEither a b
-  = Keep (Patch a) (Patch b)
-  | Replace (Either a b)
-  deriving Generic
 
-instance (Diff a, Diff b) => Diff (Either a b) where
-  type Patch (Either a b) = PatchEither a b
-  apply (Keep pX _ ) (Left  x) = Left  (apply pX x)
-  apply (Keep _  pY) (Right y) = Right (apply pY y)
-  apply (Replace e)  _         = e
+
+
+
+
+
+
+
+
+
+
+
 
