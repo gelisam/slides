c59cba61495e57cc3d6ddcfc0d2adb004e8791ca
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 19:21:55 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,16 +1,16 @@
-class Diff a where
-  type Patch a
-  apply :: Patch a -> a -> a
+-- The easy way out
+
+gdiff   :: VanillaType -> FancyType
+generic :: VanillaType -> [Instance]
+
+Patch   :: VanillaType -> VanillaType
+generic :: VanillaType -> [Instance]
 
 
-Cons 1 (Cons 2 (Cons 3 Nil)) ==> Cons 1 (Cons 3 Nil)
-       .----------- Keep ----------.                       Cpy ConsCtor
-      /                             \                      Cpy 1
-  1 ==> 1       Cons 2 (Cons 3 Nil) ==> Cons 3 Nil         Del ConsCtor
- no change             .------- Keep ------.          vs   Del 2
-                      /                     \              Cpy ConsCtor
-                  2 ==> 3        Cons 3 Nil ==> Nil        Cpy 3
-                 Replace 3          Replace Nil            Cpy NilCtor
+
+
+
+
 
 
 
