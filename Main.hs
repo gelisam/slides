941b6c47c1afd97b42a1f3031a008fa03e2a5def
Samuel Gélineau <gelisam+github@gmail.com>
2017-10-24 21:10:07 -0400

    new slide

--- Main.hs
+++ Main.hs
@@ -1,22 +1,21 @@
--- The hard way out
-
-gdiff   :: VanillaType -> FancyType
-generic :: VanillaType -> [Instance]
-
-Patch   :: VanillaType -> VanillaType
-generic :: VanillaType -> [Instance]
-
-gdiff   :: VanillaType -> FancyType
-???     :: FancyType   -> [Instance]
-
-
-
-
-
-
-
-
-
-
-
-
+---------------------------------------------------------------------------
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                              Questions?                               --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+--                                                                       --
+---------------------------------------------------------------------------
