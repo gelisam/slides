800c1fb1abe89fd66718975eba1ff927b53cdc38
Samuel Gélineau <gelisam+github@gmail.com>
2018-01-18 14:00:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,29 @@
 {-# LANGUAGE InstanceSigs, RankNTypes #-}
 import Control.Comonad
 
+type Getter s a = forall e. (a -> e) -> (s -> e)
 
+getMap :: Comonad w => (a -> e) -> (w a -> e)
+getMap f = f . extract
 
-instance Comonad G where
-  extract :: G a -> a
-  extract (G x1) = x1                                                                                                  ; duplicate = G
 
+--    FGH a -> e
+--          |
+--          |   GH a -> e
+--          |        |
+--          |        |    H a -> e
+--          |        |        |
+--          v        v        v
+-- gggetMap = getMap . getMap . getMap :: (a -> e) -> (FGH a -> e)
+-- gggetMap = getMap . getMap . getMap :: Getter (FGH a) a
+--          ^        ^        ^        ^
+--          |        |        |        |
+--        FGH a     GH a     H a       a  ----.
+--            e        e       e       e  <---'
 
 
 
--- type Getter s a = ?
--- 
--- (.) :: Getter s u -> Getter u a -> Getter s a
 
-eeextract = extract . extract . extract :: FGH a -> a
 
 
 
@@ -68,33 +77,12 @@ eeextract = extract . extract . extract :: FGH a -> a
 
 
 
-
-
-
-
-data F a = F a
-data G a = G a
-data H a = H a
+data F a
+data G a
+data H a
 
 type FGH a = F (G (H a))
 type  GH a =    G (H a)
-
-instance Functor F where
-  fmap f (F x) = F (f x)
-
-instance Functor G where
-  fmap f (G x) = G (f x)
-
-instance Functor H where
-  fmap f (H x) = H (f x)
-
-instance Comonad F where
-  extract (F x) = x
-  duplicate = F
-
-instance Comonad H where
-  extract (H x) = x
-  duplicate = H
 
 main :: IO ()
 main = putStrLn "done."
