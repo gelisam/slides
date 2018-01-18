2c1e064f23e80c27ea301f69200a65ba876e4037
Samuel Gélineau <gelisam+github@gmail.com>
2018-01-18 14:00:10 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -10,9 +10,9 @@ instance Comonad G where
 
 
 
--- type Getter s a = s -> a
+-- type Getter s a = ?
 -- 
--- (.) :: Getter u a -> Getter s u -> Getter s a
+-- (.) :: Getter s u -> Getter u a -> Getter s a
 
 eeextract = extract . extract . extract :: FGH a -> a
 
