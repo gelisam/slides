8bf67a010ab7117f5396b04e161ae19beb9f4eb2
Samuel Gélineau <gelisam+github@gmail.com>
2018-01-18 14:01:29 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,9 @@
 ---- Setter'   s   a   =                            (a ->   a) -> (s ->   s)
 ---- Setter    s t a b =                            (a ->   b) -> (s ->   t)
 ---- Fold      s   a   = forall m. Monoid m      => (a ->   m) -> (s ->   m)
-type Getter    s   a   = forall e.                  (a ->   e) -> (s ->   e)
----- Traversal s t a b = forall f. Applicative f => (a -> f b) -> (s -> f t)
----- Lens      s t a b = forall f. Functor f     => (a -> f b) -> (s -> f t)
+---- Getter    s   a   = forall e.                  (a ->   e) -> (s ->   e)
+type Traversal s t a b = forall f. Applicative f => (a -> f b) -> (s -> f t)
+type Lens      s t a b = forall f. Functor f     => (a -> f b) -> (s -> f t)
 
 
 
