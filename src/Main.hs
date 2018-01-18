91547cd0f8abb5d58b5e43e8d887399a9d8912d1
Samuel Gélineau <gelisam+github@gmail.com>
2018-01-18 13:47:45 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE InstanceSigs, RankNTypes #-}
 import Prelude hiding (Foldable, toList)
-
+import Control.Monad
 
 
 instance Foldable G where
@@ -8,13 +8,13 @@ instance Foldable G where
   toList (G x1 x2) = [x1, x2]
 
 
---          expected: [GH a] -> H a
---          atual:     GH a  -> [a]
---                       |
---                  H a  | [GH a]
---                   |   |    |
---                   v   v    v
--- tttoList = toList . toList . toList :: FGH a -> [a]
+
+
+
+
+-- (>=>) :: Monad m => (a -> m b) -> (b -> m c) -> (a -> m c)
+
+tttoList = toList >=> toList >=> toList :: FGH a -> [a]
 
 
 
@@ -72,15 +72,23 @@ instance Foldable G where
 
 
 
-data F a
+data F a = F a a
 data G a = G a a
-data H a
+data H a = H a a
 
 type FGH a = F (G (H a))
 type  GH a =    G (H a)
 
 class Foldable f where
   toList :: f a -> [a]
+
+instance Foldable F where
+  toList :: F a -> [a]
+  toList (F x1 x2) = [x1, x2]
+
+instance Foldable H where
+  toList :: H a -> [a]
+  toList (H x1 x2) = [x1, x2]
 
 main :: IO ()
 main = putStrLn "done."
