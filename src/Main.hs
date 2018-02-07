06ce31158ae8a4f460a28fef6a52e81df1ee3023
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-07 08:00:14 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,15 @@ type Getter    s   a   = forall e.                  (a ->   e) -> (s ->   e)
 type Traversal s t a b = forall f. Applicative f => (a -> f b) -> (s -> f t)
 type Lens      s t a b = forall f. Functor f     => (a -> f b) -> (s -> f t)
 
+toSetter :: forall s t a b
+          . Traversal s t a b -> Setter s t a b
+toSetter traversal f s = runIdentity t'
+  where
+    f' :: a -> Identity b
+    f' = Identity . f
 
-
+    t' :: Identity t
+    t' = traversal f' s
 
 
 
@@ -55,6 +62,15 @@ type Lens      s t a b = forall f. Functor f     => (a -> f b) -> (s -> f t)
 
 
 data Const m a = Const m
+data Identity a = Identity { runIdentity :: a }
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Applicative Identity where
+  pure x = Identity x
+  Identity f <*> Identity x = Identity (f x)
+
 
 main :: IO ()
 main = putStrLn "done."
