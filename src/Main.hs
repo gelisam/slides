5d25ef46e9a0697ad38e10ddcbcc2ad1a1b1eb2f
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-07 08:49:39 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,25 @@
-
-
-
-
-                  +--------------+               Fold      Setter
-                  |   actions    |                 ^         ^
-      +-----------+--------------+                 |_________|
-      | Setter    |        set n |                      |
-      | Fold      | get n        |        Getter    Traversal     
-      | Getter    | get 1        |          ^           ^         
-      | Traversal | get n  set n |          |___________|
-      | Lens      | get 1  set 1 |                |
-      +-----------+--------------+               Lens
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+----------------------------------------------------------------------------
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                Questions?                              --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+----------------------------------------------------------------------------
 
 
 
@@ -61,15 +56,10 @@
 
 
 
-data Const m a = Const m
-data Identity a = Identity { runIdentity :: a }
 
-instance Functor Identity where
-  fmap f (Identity x) = Identity (f x)
 
-instance Applicative Identity where
-  pure x = Identity x
-  Identity f <*> Identity x = Identity (f x)
+
+
 
 
 main :: IO ()
