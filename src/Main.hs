39f7823c1b84f50e97a27505bbf8aba33a84d627
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-07 11:57:19 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE RankNTypes #-}
 
 
-
-
-type Fold s a = forall m. Monoid m => (a -> m) -> (s -> m)
-
-
-
+type Setter'   s   a   =                            (a ->   a) -> (s ->   s)
+type Setter    s t a b =                            (a ->   b) -> (s ->   t)
+type Fold      s   a   = forall m. Monoid m      => (a ->   m) -> (s ->   m)
+type Getter    s   a   = forall e.                  (a ->   e) -> (s ->   e)
+type Traversal s t a b = forall f. Applicative f => (a -> f b) -> (s -> f t)
+type Lens      s t a b = forall f. Functor f     => (a -> f b) -> (s -> f t)
 
 
 
