03f161e19414ec0c492091999a572a67c9efda00
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 15:41:54 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,10 @@
 module Main where
 import Test.DocTest
 
-import Control.Lens
 
 
--- |
--- >>> toListOf (each . _Left) [Left "foo", Right "bar", Left "baz"]
--- ["foo","baz"]
--- >>> view (_1 . re _Left) ("foo","bar")
--- Left "foo"
--- >>> view (_1 . to Left) ("foo","bar")
--- Left "foo"
-re' :: Prism' s a -> Getter a s
-re' = re
+
+data These a b = This a | That b | These a b
 
 
 
