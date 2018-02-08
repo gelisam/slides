1797f5155fe4f82ec985e4d3f218d98b3a739c43
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 15:47:43 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,22 @@
 module Main where
 import Test.DocTest
 
-
+import Control.Lens
 
 
 data AndOr a b = OnlyLeft a | OnlyRight b | Both a b
+
+_Only :: Prism' (AndOr a b) (Either a b)
+_Only = prism' unfocus focus
+  where
+    unfocus :: Either a b -> AndOr a b
+    unfocus (Left  x) = OnlyLeft  x
+    unfocus (Right y) = OnlyRight y
+
+    focus :: AndOr a b -> Maybe (Either a b)
+    focus (OnlyLeft  x) = Just $ Left  x
+    focus (OnlyRight y) = Just $ Right y
+    focus (Both _ _)    = Nothing
 
 
 
