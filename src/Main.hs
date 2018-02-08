1e7a920b3a36a929bb0882e5b220ca54e596e19f
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 15:52:52 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,22 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Main where
 import Test.DocTest
 
-import Control.Lens
 
 
-data AndOr a b = OnlyLeft a | OnlyRight b | Both a b
-  deriving Show
 
--- |
--- >>> toListOf (each . _Only) [OnlyLeft "foo?", OnlyRight "bar!", Both "?" "!"]
--- [Left "foo?",Right "bar!"]
--- >>> view (_1 . re _Only) (Left "foo", Right "bar")
--- OnlyLeft "foo"
-_Only :: Prism' (AndOr a b) (Either a b)
-_Only = prism' unfocus focus
-  where
-    unfocus :: Either a b -> AndOr a b
-    unfocus (Left  x) = OnlyLeft  x
-    unfocus (Right y) = OnlyRight y
+data Object
+  = ObjectGraphicsCard      GraphicsCard
+  | ObjectNetworkCardBrand1 NetworkCardBrand1
+  | ObjectNetworkCardBrand2 NetworkCardBrand2
+  | ObjectNetworkCardBrand3 NetworkCardBrand3
+  | ObjectPrinter           Printer
 
-    focus :: AndOr a b -> Maybe (Either a b)
-    focus (OnlyLeft  x) = Just $ Left  x
-    focus (OnlyRight y) = Just $ Right y
-    focus (Both _ _)    = Nothing
+data GraphicsCard      = GraphicsCard
+data NetworkCardBrand1 = NetworkCardBrand1
+data NetworkCardBrand2 = NetworkCardBrand2
+data NetworkCardBrand3 = NetworkCardBrand3
+data Printer           = Printer
 
 
 
