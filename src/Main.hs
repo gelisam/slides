1e654a29699f081e30c7879613e0eedaef58d4ad
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 17:11:18 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -16,9 +16,9 @@ data NetworkCard
   | NetworkCard3 NetworkCardBrand3
 
 data GraphicsCard      = GraphicsCard
-data NetworkCardBrand1 = NetworkCardBrand1
-data NetworkCardBrand2 = NetworkCardBrand2
-data NetworkCardBrand3 = NetworkCardBrand3
+data NetworkCardBrand1 = NetworkCardBrand1 Int
+data NetworkCardBrand2 = NetworkCardBrand2 Int String
+data NetworkCardBrand3 = NetworkCardBrand3 Int Double Double
 data Printer           = Printer
 
 
