bf300ab6a26624ebd1cb47b1b209b1e5bbbc072a
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 15:36:58 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,8 @@ import Control.Lens
 -- ["foo","baz"]
 -- >>> view (_1 . re _Left) ("foo","bar")
 -- Left "foo"
+-- >>> view (_1 . to Left) ("foo","bar")
+-- Left "foo"
 re' :: Prism' s a -> Getter a s
 re' = re
 
