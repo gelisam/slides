1d281bff0d9f8a098ff559a8dcf379e369fed415
Samuel Gélineau <gelisam+github@gmail.com>
2018-02-08 19:31:17 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -19,46 +19,17 @@ data NetworkCardBrand2 = NetworkCardBrand2 Int String         deriving Show
 data NetworkCardBrand3 = NetworkCardBrand3 Int Double Double  deriving Show
 data Printer           = Printer                              deriving Show
 
+class HasMacAddress a where
+  macAddress :: Lens' a Int
 
-data SomeNetworkCard = SomeNetworkCard
-  { _someNetworkCardMacAddress :: Int
-  , _someNetworkCardToObject   :: Int -> Object
-  }
+instance HasMacAddress NetworkCardBrand1 where
+  macAddress f (NetworkCardBrand1 x) = NetworkCardBrand1 <$> f x
 
-makeLenses ''SomeNetworkCard
+instance HasMacAddress NetworkCardBrand2 where
+  macAddress f (NetworkCardBrand2 x y) = (\x' -> NetworkCardBrand2 x' y) <$> f x
 
--- |
--- >>> :{
--- toListOf (each . _SomeNetworkCard . someNetworkCardMacAddress)
---   [ ObjectNetworkCardBrand1 (NetworkCardBrand1 1)
---   , ObjectNetworkCardBrand2 (NetworkCardBrand2 2 "foo")
---   , ObjectNetworkCardBrand3 (NetworkCardBrand3 3 1.5 4.2)
---   ]
--- :}
--- [1,2,3]
---
--- >>> :{
--- mapM_ print $ over (each . _SomeNetworkCard . someNetworkCardMacAddress) (+100)
---   [ ObjectNetworkCardBrand1 (NetworkCardBrand1 1)
---   , ObjectNetworkCardBrand2 (NetworkCardBrand2 2 "foo")
---   , ObjectNetworkCardBrand3 (NetworkCardBrand3 3 1.5 4.2)
---   ]
--- :}
--- ObjectNetworkCardBrand1 (NetworkCardBrand1 101)
--- ObjectNetworkCardBrand2 (NetworkCardBrand2 102 "foo")
--- ObjectNetworkCardBrand3 (NetworkCardBrand3 103 1.5 4.2)
-_SomeNetworkCard :: Prism' Object SomeNetworkCard
-_SomeNetworkCard = prism' unfocus focus
-  where
-    unfocus :: SomeNetworkCard -> Object
-    unfocus (SomeNetworkCard i toObject) = toObject i
-
-    focus :: Object -> Maybe SomeNetworkCard
-    focus (ObjectNetworkCardBrand1 (NetworkCardBrand1 i    )) = Just $ SomeNetworkCard i $ \i' -> ObjectNetworkCardBrand1 $ NetworkCardBrand1 i'
-    focus (ObjectNetworkCardBrand2 (NetworkCardBrand2 i x  )) = Just $ SomeNetworkCard i $ \i' -> ObjectNetworkCardBrand2 $ NetworkCardBrand2 i' x
-    focus (ObjectNetworkCardBrand3 (NetworkCardBrand3 i x y)) = Just $ SomeNetworkCard i $ \i' -> ObjectNetworkCardBrand3 $ NetworkCardBrand3 i' x y
-    focus _                                                   = Nothing
-
+instance HasMacAddress NetworkCardBrand3 where
+  macAddress f (NetworkCardBrand3 x y z) = (\x' -> NetworkCardBrand3 x' y z) <$> f x
 
 
 
