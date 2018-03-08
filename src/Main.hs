ae66a7a5bc59ebd6445ee492a34236936a9a6a4d
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 14:06:51 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,14 @@
 module Main where
 import Test.DocTest
 
-import Data.Complex
 
-
-showInt :: Int -> String
-showInt = show
-
-
-class ToInt a where
-  toInt :: a -> Int
-
-liftedShowInt :: ToInt a => a -> String
-liftedShowInt = showInt . toInt
 
 
 -- |
--- >>> liftedShowInt (42 :: Int)
--- "42"
-instance ToInt Int where
-  toInt = id
-
--- |
--- >>> liftedShowInt (42.2 :: Double)
--- "42"
-instance ToInt Double where
-  toInt = round
-
--- |
--- >>> liftedShowInt (42.0 :+ 3.0 :: Complex Double)
--- "42"
-instance ToInt a => ToInt (Complex a) where
-  toInt = toInt . realPart
+-- >>> openFile "myfile"
+-- opening myfile
+openFile :: FilePath -> IO ()
+openFile filePath = putStrLn ("opening " ++ filePath)
 
 
 main :: IO ()
