0b1f758e3734945adab5dfc21ce553b3d524cd22
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 15:14:05 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -13,41 +13,21 @@ withFile filePath body = do
   putStrLn ("closing " ++ filePath)
   pure x
 
-
-class ToIO m where
-  toIO :: m a -> IO a
-
-liftedWithFile :: (MonadIO m, ToIO m) => FilePath -> m a -> m a
-liftedWithFile filePath = liftIO . withFile filePath . toIO
-
-
--- |
--- >>> liftedWithFile "myfile" $ putStrLn "doing stuff with myfile"
--- opening myfile
--- doing stuff with myfile
--- closing myfile
-instance ToIO IO where
-  toIO = id
-
 -- |
 -- >>> execWriterT $ liftedWithFile "myfile" $ tell ["abc"]
 -- opening myfile
 -- closing myfile
--- []
-instance ToIO m => ToIO (WriterT w m) where
-  toIO (WriterT body) = do  -- body :: m (a, [w])
-    (x, _) <- toIO body
-    pure x
-
--- |
+-- ["abc"]
 -- >>> flip execStateT "foo" $ liftedWithFile "myfile" $ modify (++ "!")
 -- opening myfile
 -- closing myfile
--- "foo"
-instance ToIO m => ToIO (StateT s m) where
-  toIO (StateT body) = do  -- body :: s -> m (a, s)
-    (x, _) <- toIO (body undefined)
-    pure x
+-- "foo!"
+liftedWithFile :: MonadIO m => FilePath -> m a -> m a
+liftedWithFile filePath body = do
+  liftIO $ putStrLn ("opening " ++ filePath)
+  x <- body
+  liftIO $ putStrLn ("closing " ++ filePath)
+  pure x
 
 
 main :: IO ()
