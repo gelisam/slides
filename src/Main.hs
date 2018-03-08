22cddd0f18ea9b6da89bac63eee05335a2697934
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 15:49:45 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,39 @@
 module Main where
 import Test.DocTest
 
-import Control.Concurrent.Async.Lifted
-import Control.Exception.Lifted
 import Control.Monad.IO.Class
 import Control.Monad.State
 import Control.Monad.Writer
+import UnliftIO.Exception
+import UnliftIO.Async
 
 -- |
 -- >>> execWriterT test1
--- ["abc","def"]
+-- error: no instance for (MonadUnliftIO WriterT w IO)
 test1 :: WriterT [String] IO ()
 test1 = mapConcurrently_ tell [["abc"], ["def"]]
 
 -- |
 -- >>> execStateT test2 "foo"
--- "foo"
+-- error: no instance for (MonadUnliftIO StateT s IO)
 test2 :: StateT String IO ()
 test2 = mapConcurrently_ modify [(++"!"), (++"?")]
 
 -- |
 -- >>> execWriterT test3
--- ["abc"]
+-- error: no instance for (MonadUnliftIO WriterT w IO)
 test3 :: WriterT [String] IO ()
 test3 = tell ["abc"] `finally` tell ["def"]
 
 -- |
 -- >>> execStateT test4 "foo"
--- "foo!"
+-- error: no instance for (MonadUnliftIO StateT s IO)
 test4 :: StateT String IO ()
 test4 = modify (++"!") `finally` modify (++"?")
 
 -- |
 -- >>> execStateT test5 "foo"
--- "foo"
--- "foo!"
+-- error: no instance for (MonadUnliftIO StateT s IO)
 test5 :: StateT String IO ()
 test5 = modify (++"!") `finally` do
   s <- get
