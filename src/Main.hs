6555ce865e13f2283ae81848ae1a133c7263450f
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 13:59:06 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,34 @@ import Test.DocTest
 import Data.Complex
 
 
--- |
--- >>> showInt 42
--- "42"
 showInt :: Int -> String
 showInt = show
 
+
+class ToInt a where
+  toInt :: a -> Int
+
+liftedShowInt :: ToInt a => a -> String
+liftedShowInt = showInt . toInt
+
+
 -- |
--- >>> liftedShowInt (42.0 :: Double)
--- ...undefined
--- ...
--- >>> liftedShowInt (42.0 :+ 0.0 :: Complex Double)
--- ...undefined
--- ...
-liftedShowInt :: Num a => a -> String
-liftedShowInt = showInt    -- Int -> String
-              . undefined  -- a   -> Int
+-- >>> liftedShowInt (42 :: Int)
+-- "42"
+instance ToInt Int where
+  toInt = id
+
+-- |
+-- >>> liftedShowInt (42.2 :: Double)
+-- "42"
+instance ToInt Double where
+  toInt = round
+
+-- |
+-- >>> liftedShowInt (42.0 :+ 3.0 :: Complex Double)
+-- "42"
+instance ToInt a => ToInt (Complex a) where
+  toInt = toInt . realPart
 
 
 main :: IO ()
