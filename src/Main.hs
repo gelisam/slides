da3a5a4a0f77171be98f170295d2a1728eaac3a3
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 13:56:40 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,22 @@ module Main where
 import Test.DocTest
 
 import Text.Read
-
+import Data.Complex
 
 -- |
 -- >>> parseInt "42"
 -- Just 42
 parseInt :: String -> Maybe Int
 parseInt = readMaybe
+
+-- |
+-- >>> liftedParseInt "42" :: Maybe Double
+-- Just 42.0
+-- >>> liftedParseInt "42" :: Maybe (Complex Double)
+-- Just (42.0 :+ 0.0)
+liftedParseInt :: Num a => String -> Maybe a
+liftedParseInt = fmap fromIntegral  -- Maybe Int -> Maybe a
+               . parseInt           -- String    -> Maybe Int
 
 
 main :: IO ()
