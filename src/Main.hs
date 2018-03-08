e62a31b0b5b35f9c391db2b446137819cea321fc
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 13:38:24 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,22 @@
 module Main where
 import Test.DocTest
 
-
-
+import Control.Monad.IO.Class
+import Control.Monad.State
 
 -- |
 -- >>> openFile "myfile"
 -- opening myfile
 openFile :: FilePath -> IO ()
 openFile filePath = putStrLn ("opening " ++ filePath)
+
+-- |
+-- >>> flip execStateT "foo" $ liftedOpenFile "myfile"
+-- opening myfile
+-- "foo"
+liftedOpenFile :: MonadIO m => FilePath -> m ()
+liftedOpenFile = liftIO    -- IO ()    -> m ()
+               . openFile  -- FilePath -> IO ()
 
 
 main :: IO ()
