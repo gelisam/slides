107f87783f0f63e1685bb69014ea8d087213c65f
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 15:54:19 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,42 +2,19 @@ module Main where
 import Test.DocTest
 
 import Control.Monad.IO.Class
-import Control.Monad.State
-import Control.Monad.Writer
-import UnliftIO.Exception
-import UnliftIO.Async
+import Control.Monad.Reader
 
--- |
--- >>> execWriterT test1
--- error: no instance for (MonadUnliftIO WriterT w IO)
-test1 :: WriterT [String] IO ()
-test1 = mapConcurrently_ tell [["abc"], ["def"]]
 
--- |
--- >>> execStateT test2 "foo"
--- error: no instance for (MonadUnliftIO StateT s IO)
-test2 :: StateT String IO ()
-test2 = mapConcurrently_ modify [(++"!"), (++"?")]
+withFile :: FilePath -> IO a -> IO a
+withFile filePath body = do
+  putStrLn ("opening " ++ filePath)
+  x <- body
+  putStrLn ("closing " ++ filePath)
+  pure x
 
--- |
--- >>> execWriterT test3
--- error: no instance for (MonadUnliftIO WriterT w IO)
-test3 :: WriterT [String] IO ()
-test3 = tell ["abc"] `finally` tell ["def"]
-
--- |
--- >>> execStateT test4 "foo"
--- error: no instance for (MonadUnliftIO StateT s IO)
-test4 :: StateT String IO ()
-test4 = modify (++"!") `finally` modify (++"?")
-
--- |
--- >>> execStateT test5 "foo"
--- error: no instance for (MonadUnliftIO StateT s IO)
-test5 :: StateT String IO ()
-test5 = modify (++"!") `finally` do
-  s <- get
-  liftIO $ print s
+liftedWithFile :: FilePath -> ReaderT r IO a -> ReaderT r IO a
+liftedWithFile filePath body = ReaderT $ \r -> do
+  liftIO $ withFile filePath (runReaderT body r)
 
 
 main :: IO ()
