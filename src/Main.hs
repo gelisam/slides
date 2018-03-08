cb6c11cb56de0e5b1a932c1020ca538f2e94c7bb
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 15:13:17 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,7 @@ import Control.Monad.IO.Class
 import Control.Monad.State
 import Control.Monad.Writer
 
--- |
--- >>> withFile "myfile" $ putStrLn "doing stuff with myfile"
--- opening myfile
--- doing stuff with myfile
--- closing myfile
+
 withFile :: FilePath -> IO a -> IO a
 withFile filePath body = do
   putStrLn ("opening " ++ filePath)
@@ -17,19 +13,41 @@ withFile filePath body = do
   putStrLn ("closing " ++ filePath)
   pure x
 
+
+class ToIO m where
+  toIO :: m a -> IO a
+
+liftedWithFile :: (MonadIO m, ToIO m) => FilePath -> m a -> m a
+liftedWithFile filePath = liftIO . withFile filePath . toIO
+
+
+-- |
+-- >>> liftedWithFile "myfile" $ putStrLn "doing stuff with myfile"
+-- opening myfile
+-- doing stuff with myfile
+-- closing myfile
+instance ToIO IO where
+  toIO = id
+
 -- |
 -- >>> execWriterT $ liftedWithFile "myfile" $ tell ["abc"]
--- ...
--- ...undefined
--- ...
+-- opening myfile
+-- closing myfile
+-- []
+instance ToIO m => ToIO (WriterT w m) where
+  toIO (WriterT body) = do  -- body :: m (a, [w])
+    (x, _) <- toIO body
+    pure x
+
+-- |
 -- >>> flip execStateT "foo" $ liftedWithFile "myfile" $ modify (++ "!")
--- ...
--- ...undefined
--- ...
-liftedWithFile :: MonadIO m => FilePath -> m a -> m a
-liftedWithFile filePath = liftIO             -- IO a -> m a
-                        . withFile filePath  -- IO a -> IO a
-                        . undefined          -- m a  -> IO a
+-- opening myfile
+-- closing myfile
+-- "foo"
+instance ToIO m => ToIO (StateT s m) where
+  toIO (StateT body) = do  -- body :: s -> m (a, s)
+    (x, _) <- toIO (body undefined)
+    pure x
 
 
 main :: IO ()
