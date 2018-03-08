0b725829075df01b218958d421164516fee31f55
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-08 15:57:14 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,38 @@
+{-# LANGUAGE TypeFamilies #-}
 module Main where
 import Test.DocTest
 
 import Control.Monad.IO.Class
-import Control.Monad.Reader
+import Control.Monad.State
 
 
-withFile :: FilePath -> IO a -> IO a
-withFile filePath body = do
-  putStrLn ("opening " ++ filePath)
-  x <- body
-  putStrLn ("closing " ++ filePath)
-  pure x
+class MonadIO m => ToIO m where
+  type Captured m
+  capture :: m (Captured m)
+  restore :: Captured m -> m ()
+  toIO :: m a
+       -> Captured m -> IO (a, Captured m)
 
-liftedWithFile :: FilePath -> ReaderT r IO a -> ReaderT r IO a
-liftedWithFile filePath body = ReaderT $ \r -> do
-  liftIO $ withFile filePath (runReaderT body r)
+instance ToIO IO where
+  type Captured IO = ()
+  capture = pure ()
+  restore () = pure ()
+  toIO body () = do
+    x <- body
+    pure (x, ())
+
+instance ToIO m => ToIO (StateT s m) where
+  type Captured (StateT s m) = (s, Captured m)
+  capture = do
+    s <- get
+    captured <- lift capture
+    pure (s, captured)
+  restore (s, captured) = do
+    put s
+    lift $ restore captured
+  toIO body (s, captured) = do
+    ((x, s'), captured') <- toIO (runStateT body s) captured
+    pure (x, (s', captured'))
 
 
 main :: IO ()
