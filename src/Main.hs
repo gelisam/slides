18e37b44017899bc60c076c604cb92170eff8869
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 16:06:39 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,25 @@
-import Control.DeepSeq
+import Data.Char
 import Test.DocTest
 
+newtype Yoneda f a = Yoneda
+  { unYoneda :: forall r. (a -> r) -> f r }
+
+-- (<$>)    :: forall a r. (a -> r) -> f a -> f r
+-- (<$> fx) :: forall   r. (a -> r) ->        f r
+
+makeYoneda :: Functor f => f a -> Yoneda f a
+makeYoneda fa = Yoneda (<$> fa) 
+
+fromYoneda :: Yoneda f a -> f a
+fromYoneda ya = unYoneda ya id
+
 -- |
--- >>> :set +s
--- >>> rnf computation
-computation :: Nested Int
-computation = fmapN 1000 (+1) (makeNested 10000 0)
-
-fmapN :: Functor f => Int -> (a -> a) -> f a -> f a
-fmapN n f = fmap (composeN n f)
-
--- fmap ((+1) . (+1) . (+1))
+-- >>> unYoneda foo id
+-- "foo"
+-- >>> unYoneda foo toUpper
+-- "FOO"
+foo :: Yoneda [] Char
+foo = makeYoneda "foo"
 
 
 
@@ -66,26 +75,7 @@ fmapN n f = fmap (composeN n f)
 
 
 
-
-data Nested a
-  = Nest (Nested a)
-  | Stop a
-
-instance NFData a => NFData (Nested a) where
-  rnf (Nest x) = rnf x
-  rnf (Stop x) = rnf x
-
-instance Functor Nested where
-  fmap f (Nest nx) = Nest (fmap f nx)
-  fmap f (Stop x)  = Stop (f x)
-
-makeNested :: Int -> a -> Nested a
-makeNested n x = composeN n Nest (Stop x)
-
-
-composeN :: Int -> (a -> a) -> a -> a
-composeN n f x = iterate f x !! n
 
 
 main :: IO ()
-main = doctest ["src/Main.hs"]
+main = doctest ["-XRankNTypes", "src/Main.hs"]
