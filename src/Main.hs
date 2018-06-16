eb94cd7575ff22f5b990e5eea35ff0bb6dafdca4
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 13:29:22 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ import Control.DeepSeq
 import Data.List
 import Test.DocTest
 
-
+-- |
 -- >>> :set +s
 -- >>> rnf computation
 computation :: [Int]
@@ -11,7 +11,12 @@ computation = myconcat [[x] | x <- [0..10000]]
 myconcat :: [[a]] -> [a]
 myconcat = foldr (++) []
 
+-- [0] ++ ([1] ++ ([2] ++ ([3] ++ ([4] ++ []))))
+-- [0] ++ ([1] ++ [2,3,4])
 
+append :: [a] -> [a] -> [a]
+append []     ys = ys
+append (x:xs) ys = x : append xs ys
 
 
 
@@ -73,4 +78,4 @@ myconcat = foldr (++) []
 
 
 main :: IO ()
-main = putStrLn "done."
+main = doctest ["src/Main.hs"]
