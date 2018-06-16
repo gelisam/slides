dc7d01326494170e8c3489ef1bd22c1ef51d5973
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 16:59:09 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,76 +1,106 @@
-import Data.Char
+import Control.DeepSeq
 import Test.DocTest
+
+-- |
+-- >>> :set +s
+-- >>> rnf computation
+computation :: Nested Int
+computation = fmapN 1000 (+1) (makeNested 10000 0)
+
+fmapN :: Functor f => Int -> (a -> a) -> f a -> f a
+fmapN n f = composeN n (fmap f)
+
+-- fmap (+1) . fmap (+1) . fmap (+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data Nested a
+  = Nest (Nested a)
+  | Stop a
+
+instance NFData a => NFData (Nested a) where
+  rnf (Nest x) = rnf x
+  rnf (Stop x) = rnf x
+
+instance Functor Nested where
+  fmap f (Nest nx) = Nest (fmap f nx)
+  fmap f (Stop x)  = Stop (f x)
+
+makeNested :: Int -> a -> Nested a
+makeNested n x = composeN n Nest (Stop x)
+
+
+composeN :: Int -> (a -> a) -> a -> a
+composeN n f x = iterate f x !! n
+
 
 newtype Yoneda f a = Yoneda
   { unYoneda :: forall r. (a -> r) -> f r }
 
--- |
--- >>> fromYoneda . fmap toUpper . makeYoneda $ "foo"
--- "FOO"
 instance Functor (Yoneda f) where
   fmap :: (a -> b) -> Yoneda f a -> Yoneda f b
   fmap a2b ya = Yoneda $ \b2r
              -> unYoneda ya (b2r . a2b)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 makeYoneda :: Functor f => f a -> Yoneda f a
-makeYoneda fx = Yoneda (<$> fx) 
+makeYoneda fa = Yoneda (<$> fa) 
 
 fromYoneda :: Yoneda f a -> f a
-fromYoneda yx = unYoneda yx id
+fromYoneda ya = unYoneda ya id
 
 
 main :: IO ()
