7b964d781ee751fb915cec87b7ca20f7e4416b60
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 12:48:15 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,7 @@
-data Seq a
-  = EmptyT
-  | Single a
-  | Deep Int (Digit a)
-             (Seq (Node a))
-             (Digit a)
 
-data Digit a
-  = One   a
-  | Two   a a
-  | Three a a a
-  | Four  a a a a
 
-data Node a
-  = Node2 Int a a
-  | Node3 Int a a a
-
+newtype DList a = DList
+  { unDList :: [a] -> [a] }
 
 
 
