0d421561ccbac437c772ed9729dd0ce6fa1c5e3e
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 13:30:52 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -9,16 +9,14 @@ computation :: [Int]
 computation = myconcat [[x] | x <- [0..10000]]
 
 myconcat :: [[a]] -> [a]
-myconcat = foldr (++) []
+myconcat = foldl' (++) []
 
--- [0] ++ ([1] ++ ([2] ++ ([3] ++ ([4] ++ []))))
--- [0] ++ ([1] ++ [2,3,4])
+-- (((([] ++ [0]) ++ [1]) ++ [2]) ++ [3]) ++ [4]
+-- ([0,1,2] ++ [3]) ++ [4]
 
 append :: [a] -> [a] -> [a]
 append []     ys = ys
 append (x:xs) ys = x : append xs ys
-
-
 
 
 
