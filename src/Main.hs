1adc1fede09b89f1f9be1f155cf0eb7b0cc9dda4
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 16:16:38 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,25 @@
-instance Semigroup (DList a) where
-  DList f <> DList g = DList (f . g)
-
-makeDList :: [a] -> DList a
-makeDList xs = DList (xs ++)
-
--- DList (("foo" ++) . ("bar" ++) . ("baz" ++))
--- DList (\nil -> ("foo" ++) . ("bar" ++) . ("baz" ++) $ nil)
--- DList (\nil -> ("foo" ++) (("bar" ++) (("baz" ++) nil)))
--- DList (\nil -> "foo" ++ ("bar" ++ ("baz" ++ nil)))
-
-
-instance Semigroup (SList a) where
-  SList f <> SList g = SList (g . f)
-
-makeSList :: [a] -> SList a
-makeSList xs = SList (++ xs)
-
--- SList ((++ "baz") . (++ "bar") . (++ "foo"))
--- SList (\nil -> (++ "baz") . (++ "bar") . (++ "foo") $ nil)
--- SList (\nil -> (++ "baz") ((++ "bar") ((++ "foo") nil)))
--- SList (\nil -> ((nil ++ "foo") ++ "bar") ++ "baz")
-
-
+----------------------------------------------------------------------------
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                            PART II: Yoneda                             --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+--                                                                        --
+----------------------------------------------------------------------------
 
 
 
@@ -63,22 +61,6 @@ makeSList xs = SList (++ xs)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-newtype DList a = DList { unDList :: [a] -> [a] }
-newtype SList a = SList { unSList :: [a] -> [a] }
 
 main :: IO ()
 main = putStrLn "done."
