8621c6887de1b47149b9ddbda5c54250a084b5d8
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 16:48:01 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,77 +3,81 @@ import Test.DocTest
 newtype DList a = DList
   { unDList :: [a] -> [a] }
 
--- (++)    :: [a] -> [a] -> [a]
--- (xs ++) ::        [a] -> [a]
+-- |
+-- >>> fromDList mempty :: String
+-- ""
+instance Monoid (DList a) where
+  mempty = undefined
+
+-- |
+-- >>> fromDList (makeDList "foo" <> makeDList "bar")
+-- "foobar"
+instance Semigroup (DList a) where
+  DList f <> DList g = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 makeDList :: [a] -> DList a
 makeDList xs = DList (xs ++)
 
 fromDList :: DList a -> [a]
 fromDList xs = unDList xs []
-
--- |
--- >>> unDList foo ""
--- "foo"
--- >>> unDList foo "bar"
--- "foobar"
-foo :: DList Char
-foo = makeDList "foo"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 main :: IO ()
