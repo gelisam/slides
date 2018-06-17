7ef4b66d9f510c8696913fb7c56764f27065f5c1
Samuel Gélineau <gelisam+github@gmail.com>
2018-06-16 22:51:42 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,70 +4,78 @@ import Control.Monad.Free
 import Test.DocTest
 
 -- |
--- >>> numbers
--- Free (1,Free (2,Free (3,Free (4,Pure ()))))
-numbers :: Free ((,) Int) ()
-numbers = do tell 1
-             tell 2
-             tell 3
-             tell 4
+-- >>> :set +s
+-- >>> rnf computation
+computation :: Free ((,) Int) ()
+computation = forUpTo 5000 tell
+
+forUpTo :: Monad m => Int -> (Int -> m ()) -> m ()
+forUpTo n act = go 1
+  where
+    go i = unless (i >= n) $ do
+      act i
+      go (i+1)
+
+-- tell 1 >> (tell 2 >> (tell 3 >> (tell 4 >> pure ())))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+instance (NFData a, NFData b) => NFData (Free ((,) a) b) where
+  rnf (Pure x) = rnf x
+  rnf (Free x) = rnf x
+
 
 tell :: w -> Free ((,) w) ()
 tell w = liftF (w, ())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-instance NFData a => NFData (Free Maybe a) where
-  rnf (Pure x) = rnf x
-  rnf (Free x) = rnf x
 
 
 main :: IO ()
