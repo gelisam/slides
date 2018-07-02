6b0cf627ed6cad36c36709016fc41333b3b82441
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-02 15:13:59 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,39 +1,34 @@
 module Slide where
+import Control.Monad.IO.Class
+import Data.Conduit
 
----------------------------------------------------------------------------------
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                            Conduits and Machines                            --
---                                                                             --
---                                                                             --
---                                           presented by                      --
---                                        Samuel Gélineau                      --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
----------------------------------------------------------------------------------
+main :: IO () --         [1,2,3]      [1,1,2,2,3,3]
+main = runConduit $ listSource [1..3] .| stutter .| printAll
 
 
+listSource :: Monad m
+           => [o] -> ConduitT i o m ()
+listSource []     = pure ()
+listSource (x:xs) = do
+  yield x
+  listSource xs
 
+stutter :: Monad m
+        => ConduitT a a m ()
+stutter = await >>= \case
+  Nothing -> pure ()
+  Just i  -> do
+    yield i
+    yield i
+    stutter
 
+printAll :: (Show i, MonadIO m)
+         => ConduitT i o m ()
+printAll = await >>= \case
+  Nothing -> pure ()
+  Just i  -> do
+    liftIO $ print i
+    printAll
 
 
 
@@ -129,8 +124,3 @@ module Slide where
 
 
 
-
-
-
-main :: IO ()
-main = putStrLn "typechecks."
