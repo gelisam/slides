8f2efaafe0b59874f8482e5e61ad34c5d445e105
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-05 09:50:42 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -6,26 +6,26 @@ main :: IO () --         [1,2,3]      [1,1,2,2,3,3]
 main = runConduit $ listSource [1..3] .| stutter .| printAll
 
 
-listSource :: Monad m
+listSource :: (Show o, MonadIO m)
            => [o] -> ConduitT i o m ()
-listSource []     = pure ()
+listSource []     = verboseStop "listSource" ()
 listSource (x:xs) = do
-  yield x
+  verboseYield "listSource" x
   listSource xs
 
-stutter :: Monad m
+stutter :: (Show a, MonadIO m)
         => ConduitT a a m ()
-stutter = await >>= \case
-  Nothing -> pure ()
+stutter = verboseAwait "stutter" >>= \case
+  Nothing -> verboseStop "stutter" ()
   Just i  -> do
-    yield i
-    yield i
+    verboseYield "stutter" i
+    verboseYield "stutter" i
     stutter
 
 printAll :: (Show i, MonadIO m)
          => ConduitT i o m ()
-printAll = await >>= \case
-  Nothing -> pure ()
+printAll = verboseAwait "printAll" >>= \case
+  Nothing -> verboseStop "printAll" ()
   Just i  -> do
     liftIO $ print i
     printAll
@@ -120,7 +120,20 @@ printAll = await >>= \case
 
 
 
+verboseAwait :: MonadIO m
+             => String -> ConduitT i o m (Maybe i)
+verboseAwait label = do
+  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " awaits"
+  await
 
+verboseYield :: (Show o, MonadIO m)
+             => String -> o -> ConduitT i o m ()
+verboseYield label o = do
+  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " yields " ++ show o
+  yield o
 
-
-
+verboseStop :: MonadIO m
+            => String -> a -> ConduitT i o m a
+verboseStop label a = do
+  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " stops"
+  pure a
