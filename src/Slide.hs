c9e46b787a77ac170506bca92015d911867e7179
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-05 09:51:41 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,34 +1,24 @@
 module Slide where
-import Control.Monad.IO.Class
-import Data.Conduit
 
-main :: IO () --         [1,2,3]      [1,1,2,2,3,3]
-main = runConduit $ listSource [1..3] .| stutter .| printAll
 
 
-listSource :: (Show o, MonadIO m)
-           => [o] -> ConduitT i o m ()
-listSource []     = verboseStop "listSource" ()
-listSource (x:xs) = do
-  verboseYield "listSource" x
-  listSource xs
 
-stutter :: (Show a, MonadIO m)
-        => ConduitT a a m ()
-stutter = verboseAwait "stutter" >>= \case
-  Nothing -> verboseStop "stutter" ()
-  Just i  -> do
-    verboseYield "stutter" i
-    verboseYield "stutter" i
-    stutter
 
-printAll :: (Show i, MonadIO m)
-         => ConduitT i o m ()
-printAll = verboseAwait "printAll" >>= \case
-  Nothing -> verboseStop "printAll" ()
-  Just i  -> do
-    liftIO $ print i
-    printAll
+--             listSource [1..]
+--                             \
+--                              \
+--                               zipWith max
+--                              /           \
+--                             /             \
+-- listSource [2..] --- stutter               \
+--                                             zipWith (+) --- printAll
+--  listSource [1,3..]                        /
+--                    \                      /
+--                     \                    /
+--                      alternate --- take 4
+--                     /
+--                    /
+--  listSource [2,4..]
 
 
 
@@ -120,20 +110,11 @@ printAll = verboseAwait "printAll" >>= \case
 
 
 
-verboseAwait :: MonadIO m
-             => String -> ConduitT i o m (Maybe i)
-verboseAwait label = do
-  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " awaits"
-  await
 
-verboseYield :: (Show o, MonadIO m)
-             => String -> o -> ConduitT i o m ()
-verboseYield label o = do
-  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " yields " ++ show o
-  yield o
 
-verboseStop :: MonadIO m
-            => String -> a -> ConduitT i o m a
-verboseStop label a = do
-  liftIO $ putStrLn $ Prelude.take 10 (label ++ repeat ' ') ++ " stops"
-  pure a
+
+
+
+
+main :: IO ()
+main = putStrLn "typechecks."
