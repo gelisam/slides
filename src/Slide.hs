c2a1972163b0cfb0f72451febfc6e327ca03364a
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-18 21:52:07 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,37 +1,24 @@
 module Slide where
----------------------------------------------------------------------------------
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                               Recursion Schemes                             --
---                                                                             --
---                                - example                                    --
---                                - generalize                                 --
---                                - generalize                                 --
---                                > generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
----------------------------------------------------------------------------------
+import Control.Arrow
+import Test.DocTest
 
+data SearchTree a
+  = Leaf
+  | Branch (SearchTree a) a (SearchTree a)
 
-
-
-
+--              2             2    
+--             / \           / \
+--            /   \         /   \
+-- insert 1  0     4   ->  0     4  
+--                / \       \   / \
+--               3   5       1 3   5
+insert :: Ord a
+       => a -> SearchTree a -> SearchTree a
+insert new Leaf = Branch Leaf new Leaf
+insert new (Branch l a r)
+  | new <= a  = Branch (insert new l) a r
+             -- Branch (insert new l) a (insert new r)
+  | otherwise = Branch l a (insert new r)
 
 
 
@@ -117,4 +104,4 @@ module Slide where
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
