85ec0a40348a4a9b4483158175baf79f428e5509
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 06:56:13 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,32 +1,26 @@
 module Slide where
----------------------------------------------------------------------------------
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                               Recursion Schemes                             --
---                                                                             --
---                                > example                                    --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
----------------------------------------------------------------------------------
+import Prelude hiding (sum, product, concat)
+import Test.DocTest
+
+-- |
+-- >>> sum [1,2,3,4]
+-- 10
+-- >>> product [1,2,3,4]
+-- 24
+-- >>> concat ["foo","bar","baz"]
+-- "foobarbaz"
+
+sum :: [Int] -> Int
+sum []     = 0
+sum (x:xs) = x + sum xs
+
+product :: [Int] -> Int
+product []     = 1
+product (x:xs) = x * product xs
+
+concat :: [String] -> String
+concat []     = ""
+concat (x:xs) = x ++ concat xs
 
 
 
@@ -117,4 +111,4 @@ module Slide where
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
