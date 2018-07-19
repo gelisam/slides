3c6e913ecf90202d53c9c56ff25ba3467b835a59
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 12:48:08 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -3,20 +3,20 @@ import Test.DocTest
 import Control.Arrow
 import Data.List
 
--- |
--- >>> quickSort [3,2,8,4,5,7,1,6]
--- [1,2,3,4,5,6,7,8]
-quickSort :: forall a. Ord a
-          => [a] -> [a]
-quickSort = hylo divide conquer where
-  divide :: [a] -> SearchTreeF a [a]
-  divide []     = LeafF
-  divide (a:as) = let (lower, higher) = partition (<= a) as
-                  in BranchF lower a higher
-  
-  conquer :: SearchTreeF a [a] -> [a]
-  conquer LeafF = []
-  conquer (BranchF l a r) = l ++ [a] ++ r
+fold :: forall t tF r. Functor tF
+     => (t -> tF t) -> (tF r -> r) -> t -> r
+fold project foldF = hylo project foldF
+
+
+
+
+
+
+
+
+
+
+
 
 
 hylo :: forall s f r. Functor f
