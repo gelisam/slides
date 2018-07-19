9b5f0e1477f3424e10797128d17d0e345e3fa00e
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-18 21:58:42 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,33 +1,16 @@
 module Slide where
----------------------------------------------------------------------------------
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                               Recursion Schemes                             --
---                                                                             --
---                                - example                                    --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                > generalize                                 --
---                                - generalize                                 --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
----------------------------------------------------------------------------------
+import Test.DocTest
+import Control.Arrow
+import Data.List
 
+-- |
+-- >>> quickSort [3,2,8,4,5,7,1,6]
+-- [1,2,3,4,5,6,7,8]
+quickSort :: forall a. Ord a
+          => [a] -> [a]
+quickSort []     = []
+quickSort (a:as) = let (lower, higher) = partition (<= a) as
+                   in quickSort lower ++ [a] ++ quickSort higher
 
 
 
@@ -117,4 +100,4 @@ module Slide where
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
