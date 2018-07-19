d662ac6b3028711c8fb30051e3b3a81d512f7f9c
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-18 21:50:15 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,38 +1,30 @@
 module Slide where
----------------------------------------------------------------------------------
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                               Recursion Schemes                             --
---                                                                             --
---                                - example                                    --
---                                > generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                - generalize                                 --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
---                                                                             --
----------------------------------------------------------------------------------
+import Prelude hiding (lookup)
+import Test.DocTest
+
+
+-- |
+-- >>> lookup "bar" [("foo",1), ("bar",2), ("baz",3)]
+-- Just 2
+-- >>> lookup "lol" [("foo",1), ("bar",2), ("baz",3)]
+-- Nothing
+lookup :: forall k a. Eq k
+       => k -> [(k,a)] -> Maybe a
+lookup key = go where
+  go :: [(k,a)] -> Maybe a
+  go []           = Nothing
+  go ((k,a):rest) = if k == key then Just a
+                                else go rest
 
 
 
 
 
 
+fold :: (a -> a -> a) -> a -> [a] -> a
+fold cons nil = go where
+  go []     = nil
+  go (a:as) = a `cons` go as
 
 
 
@@ -117,4 +109,4 @@ module Slide where
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
