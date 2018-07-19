fb33b059dbbd6c7960ec5acb3635d7b3fa5cbdf7
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 08:09:22 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -4,14 +4,16 @@ import Test.DocTest
 
 
 foldList :: (ListF a r -> r) -> List a -> r
-
+foldList = fold
 
 
 
 foldTree :: (TreeF a r -> r) -> Tree a -> r
+foldTree = fold
 
 
 
+fold     :: (tF      r -> r) -> t      -> r
 
 
 
@@ -100,17 +102,6 @@ data List a
   = Nil
   | Cons a (List a)
 
--- Cons 1 (Cons 2 (Cons 3 Nil))
---              v
---              v
--- cons 1 (cons 2 (cons 3 nil))
-foldList foldF = go where
-  go Nil         = nil
-  go (Cons a as) = cons a (go as)
-
-  nil      = foldF NilF
-  cons a r = foldF (ConsF a r)
-
 
 data TreeF a r
   = LeafF a
@@ -120,16 +111,8 @@ data Tree a
   = Leaf a
   | Branch (Tree a) (Tree a)
 
--- Branch (Leaf 1) (Branch (Leaf 2) (Leaf 3))
---                     v
---                     v
--- branch (leaf 1) (branch (leaf 2) (leaf 3))
-foldTree foldF = go where
-  go (Leaf a)             = leaf a
-  go (Branch treeL treeR) = branch (go treeL) (go treeR)
 
-  leaf a       = foldF (LeafF a)
-  branch rL rR = foldF (BranchF rL rR)
+fold = undefined
 
 
 main :: IO ()
