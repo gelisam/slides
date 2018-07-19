cddb2ed5143d76cb7ac81202f5b4cb7a7cdd3b70
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 07:43:35 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,30 +1,38 @@
 module Slide where
-import Prelude hiding (map, (++), foldr)
-import Test.DocTest
+---------------------------------------------------------------------------------
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                               Recursion Schemes                             --
+--                                                                             --
+--                                - example                                    --
+--                                > generalize                                 --
+--                                - generalize                                 --
+--                                - generalize                                 --
+--                                - generalize                                 --
+--                                - generalize                                 --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+---------------------------------------------------------------------------------
 
 
--- |
--- >>> map (+10) [1,2,3]
--- [11,12,13]
-map :: forall a b. (a -> b) -> [a] -> [b]
-map f = foldr cons [] where
-  cons :: a -> [b] -> [b]
-  cons a bs = f a : bs
 
--- |
--- >>> "foo" ++ "bar"
--- "foobar"
-(++) :: [a] -> [a] -> [a]
-xs ++ ys = foldr (:) ys xs
 
--- 1   :    2   :    3   :    []
---              v
---              v
--- 1 `cons` 2 `cons` 3 `cons` nil))
-foldr :: (a -> r -> r) -> r -> [a] -> r
-foldr cons nil = go where
-  go []     = nil
-  go (a:as) = a `cons` go as
+
+
 
 
 
@@ -109,4 +117,4 @@ foldr cons nil = go where
 
 
 main :: IO ()
-main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
+main = putStrLn "typechecks."
