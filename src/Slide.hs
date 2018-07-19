b1a8087a7d54e6e6af5f5146ee0a1b1931b8f2d6
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 11:40:18 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,43 +1,45 @@
 module Slide where
-import Control.Arrow
-import Test.DocTest
+---------------------------------------------------------------------------------
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                               Recursion Schemes                             --
+--                                                                             --
+--                                - example                                    --
+--                                - generalize                                 --
+--                                - generalize                                 --
+--                                > generalize                                 --
+--                                - generalize                                 --
+--                                - generalize                                 --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+--                                                                             --
+---------------------------------------------------------------------------------
 
-data SearchTree a
-  = Leaf
-  | Branch (SearchTree a) a (SearchTree a)
-
---              2             2    
---             / \           / \
---            /   \         /   \
--- insert 1  0     4   ->  0     4  
---                / \       \   / \
---               3   5       1 3   5
-insert :: Ord a
-       => a -> SearchTree a -> SearchTree a
-insert new = para projectSearchTree $ \case
-  LeafF -> Branch Leaf new Leaf
-  BranchF (treeL, insertL) a (treeR, insertR)
-    | new <= a  -> Branch insertL a treeR
-    | otherwise -> Branch treeL a insertR
-
-
-para :: forall t tF r. Functor tF
-     => (t -> tF t) -> (tF (t, r) -> r) -> t -> r
-para project paraF = go where
-  go :: t -> r
-  go = project >>> fmap (id &&& go) >>> paraF
 
 
 
 
-data SearchTreeF a r
-  = LeafF
-  | BranchF r a r
-  deriving Functor
 
-projectSearchTree :: SearchTree a -> SearchTreeF a (SearchTree a)
-projectSearchTree Leaf           = LeafF
-projectSearchTree (Branch l a r) = BranchF l a r
+
+
+
+
+
+
+
 
 
 
@@ -115,4 +117,4 @@ projectSearchTree (Branch l a r) = BranchF l a r
 
 
 main :: IO ()
-main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
+main = putStrLn "typechecks."
