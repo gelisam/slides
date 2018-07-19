77095b185bccad6e25a04872db13344d3bb90fd5
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 14:19:08 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,7 +1,7 @@
 module Slide where
 import Test.DocTest
 import Control.Comonad
-
+import Data.Functor.Identity
 
 fold   :: forall s f r. Functor f
        => (s -> f s) -> (f     r  -> r) -> s -> r
@@ -10,10 +10,10 @@ zygo   :: forall s f i r. Functor f
        =>               (f     i  -> i)
        -> (s -> f s) -> (f (i, r) -> r) -> s -> r
 
-
-ghylo :: (Monad m, Functor f, Comonad w)
-      => (s -> f (m s))
-      -> (forall x. m (f x) -> f (m x))
+-- m ~ Identity
+ghylo :: (Functor f, Comonad w)
+      => (s -> f (Identity s))
+      -> (forall x. Identity (f x) -> f (Identity x))
       -> (forall x. f (w x) -> w (f x))
       -> (f (w r) -> r)
       -> s -> r
