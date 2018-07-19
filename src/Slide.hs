a39d7b6b5dd292d9e044ffa23d3796e1c19899cc
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 15:27:35 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,104 +1,135 @@
 module Slide where
 import Test.DocTest
+import Control.Arrow
+
+type Input  a = SearchTree a
+type Output a = SearchTree a
+
+--        2               2           2    
+--       / \             / \         / \
+--      /   \           /   \       /   \
+-- go  0     4   ->  ( 0     4  ,  0     4  )
+--          / \             / \     \   / \
+--         3   5           3   5     1 3   5
+insert :: forall a. Ord a
+       => a -> Input a -> Output a
+insert new = gcata project gather insertF where
+  project :: Input a -> SearchTreeF a (Input a)
+  project Leaf           = LeafF
+  project (Branch l a r) = BranchF l a r
+
+  gather :: Output a
+         -> SearchTreeF a (Input a, Output a)
+         -> (Input a, Output a)
+  gather o LeafF                       = (Leaf, o)
+  gather o (BranchF (iL,oL) a (iR,oR)) = (Branch iL a iR, Branch oL a oR)
+
+  insertF :: SearchTreeF a (Input a, Output a) -> Output a
+  insertF LeafF = Branch Leaf new Leaf
+  insertF (BranchF (iL,oL) a (iR,oR))
+    | new <= a  = Branch oL a iR
+    | otherwise = Branch iL a oR
 
 
 
-fold   :: forall s f r. Functor f
-       => (s -> f s) -> (f     r  -> r) -> s -> r
 
-zygo   :: forall s f i r. Functor f
-       =>               (f     i  -> i)
-       -> (s -> f s) -> (f (i, r) -> r) -> s -> r
 
--- z ~ (i, r)
-gcata :: Functor f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data SearchTree a
+  = Leaf
+  | Branch (SearchTree a) a (SearchTree a)
+
+data SearchTreeF a r
+  = LeafF
+  | BranchF r a r
+  deriving Functor
+
+
+gcata :: forall s f z r. Functor f
       => (s -> f s)
-      -> (r -> f (i, r) -> (i, r))
-      -> (f (i, r) -> r)
+      -> (r -> f z -> z)
+      -> (f z -> r)
       -> s -> r
+gcata project gather gcataF = gcataF . fmap go . project where
+  go :: s -> z
+  go = project >>> fmap go >>> (gcataF &&& id) >>> uncurry gather
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fold = undefined
-zygo = undefined
-
-gcata = gcata
 
 main :: IO ()
 main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
