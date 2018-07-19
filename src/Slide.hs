f195e4514e5bfc0655f9ac7d42299bc4b99406c6
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-19 14:25:45 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,7 +1,7 @@
 module Slide where
 import Test.DocTest
 import Control.Comonad
-import Data.Functor.Identity
+
 
 fold   :: forall s f r. Functor f
        => (s -> f s) -> (f     r  -> r) -> s -> r
@@ -10,10 +10,9 @@ zygo   :: forall s f i r. Functor f
        =>               (f     i  -> i)
        -> (s -> f s) -> (f (i, r) -> r) -> s -> r
 
--- m ~ Identity
+
 ghylo :: (Functor f, Comonad w)
-      => (s -> f           s )
-      -> (forall x.           f x  -> f           x )
+      => (s -> f s)
       -> (forall x. f (w x) -> w (f x))
       -> (f (w r) -> r)
       -> s -> r
@@ -99,7 +98,7 @@ ghylo :: (Functor f, Comonad w)
 fold = undefined
 zygo = undefined
 
-ghylo = ghylo
+gcata = gcata
 
 main :: IO ()
 main = doctest ["-XDeriveFunctor", "-XGADTs", "-XKindSignatures", "-XLambdaCase", "-XMultiParamTypeClasses", "-XRankNTypes", "-XScopedTypeVariables", "src/Slide.hs"]
