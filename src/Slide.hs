f5cf9787211f1478461fe4eb3cd8a000c8328e61
Samuel Gélineau <gelisam+github@gmail.com>
2018-08-19 08:11:33 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,18 +1,8 @@
 module Slide where
-import Test.DocTest                                                                                                                                                                                    ; import Codec.Picture; import Control.Applicative; import Control.Lens; import Data.Array; import Graphics.Gloss; import Graphics.Gloss.Interface.IO.Game; import Graphics.Gloss.Juicy; import System.Process
+import Test.DocTest                                                                                                                                                                                    ; import Codec.Picture hiding (Image); import Control.Applicative; import Control.Lens; import Data.Array; import Graphics.Gloss; import Graphics.Gloss.Interface.IO.Game; import Graphics.Gloss.Juicy; import System.Process
 
 
-type DiscreteImage = Array (Int,Int) RGB
-
-type Plane a = (Float,Float) -> a
-type ContinuousImage = Plane RGB
-type Shape           = Plane Bool
-
-circleC :: Float -> Shape
-circleC r = \(x,y) -> x*x + y*y < r*r
-
-blackShape :: Shape -> ContinuousImage
-blackShape f = \(x,y) -> if f (x,y) then blackRGB else whiteRGB
+type DiscreteAnimation = [Image]
 
 
 
@@ -91,19 +81,28 @@ blackShape f = \(x,y) -> if f (x,y) then blackRGB else whiteRGB
 
 
 type RGB = PixelRGBA8
+type ContinuousImage = (Float,Float) -> RGB
+type Image = ContinuousImage
+type ContinuousAnimation = Float -> ContinuousImage
+
+translateC :: Float -> Float -> ContinuousImage -> ContinuousImage
+translateC dx dy f = \(x, y) -> f (x - dx, y - dy)
+
+scaleC :: Float -> Float -> ContinuousImage -> ContinuousImage
+scaleC sx sy f = \(x, y) -> f (x / sx, y / sy)
 
 blackRGB, whiteRGB :: RGB
 (blackRGB, whiteRGB) = (PixelRGBA8 0 0 0 255, PixelRGBA8 255 255 255 255)
 
-translateC :: (Float, Float) -> ContinuousImage -> ContinuousImage
-translateC (dx, dy) f = \(x, y) -> f (x - dx, y - dy)
-
-scaleC :: (Float, Float) -> ContinuousImage -> ContinuousImage
-scaleC (sx, sy) f = \(x, y) -> f (x / sx, y / sy)
+circleC :: Float -> ContinuousImage
+circleC r (x,y) = if x*x + y*y < r*r then blackRGB else whiteRGB
 
 
 windowWidth, windowHeight :: Int
 (windowWidth, windowHeight) = (640, 480)
+
+fps :: Float
+fps = 30
 
 discretizeImage :: ContinuousImage -> Picture
 discretizeImage f = fromImageRGBA8 $ generateImage f' windowWidth windowHeight
@@ -114,8 +113,13 @@ discretizeImage f = fromImageRGBA8 $ generateImage f' windowWidth windowHeight
                )
 
 
-continuousImage :: ContinuousImage
-continuousImage = blackShape (circleC 100)
+discretizeTime :: Float -> Float
+discretizeTime = (/ fps) . fromIntegral . round . (* fps)
+
+continuousAnimation :: Float -> ContinuousImage
+continuousAnimation t = translateC (100 * cos (-t/2 * 2*pi))
+                                   (100 * sin (-t/2 * 2*pi))
+                      $ circleC 25
 
 
 data Camera = Camera
@@ -148,19 +152,25 @@ initialKeyboard = Keyboard Up Up Up Up Up Up
 data World = World
   { _worldCamera   :: Camera
   , _worldKeyboard :: Keyboard
+  , _worldTime     :: Float
+  , _worldSpeed    :: Float
   }
   deriving Show
 makeLenses ''World
 
 initialWorld :: World
-initialWorld = World initialCamera initialKeyboard
+initialWorld = World initialCamera initialKeyboard 0 1
 
 
-draw :: Camera -> Picture
-draw (Camera {..}) = discretizeImage
-                   $ scaleC (_cameraZoom, _cameraZoom)
-                   $ translateC (-_cameraX, -_cameraY)
-                   $ continuousImage
+draw :: World -> Picture
+draw world = scale (world ^. worldCamera . cameraZoom)
+                   (world ^. worldCamera . cameraZoom)
+           $ translate (world ^. worldCamera . cameraX . to negate)
+                       (world ^. worldCamera . cameraY . to negate)
+           $ discretizeImage
+           $ continuousAnimation
+           $ discretizeTime
+           $ world ^. worldTime
 
 onEvent :: Event -> Keyboard -> Keyboard
 onEvent (EventKey (Char key) keyState _ _) = onKey key keyState
@@ -180,36 +190,39 @@ movingSpeed :: Float
 movingSpeed = 100
 
 zoomingSpeed :: Float
-zoomingSpeed = 2
+zoomingSpeed = 3
 
 onTimeDelta :: Float -> World -> World
-onTimeDelta dt world = whenDown keyboardQ (cameraZoom //~ zoomingSpeed ** dt)
-                     . whenDown keyboardW (cameraY +~ dt * adjustedMovingSpeed)
-                     . whenDown keyboardE (cameraZoom *~  zoomingSpeed ** dt)
-                     . whenDown keyboardA (cameraX -~ dt * adjustedMovingSpeed)
-                     . whenDown keyboardS (cameraY -~ dt * adjustedMovingSpeed)
-                     . whenDown keyboardD (cameraX +~ dt * adjustedMovingSpeed)
+onTimeDelta dt world = whenDown keyboardQ (worldSpeed //~ zoomingSpeed ** dt)
+                     . whenDown keyboardW (worldCamera . cameraY +~ dt * adjustedMovingSpeed)
+                     . whenDown keyboardE (worldSpeed *~  zoomingSpeed ** dt)
+                     . whenDown keyboardA (worldCamera . cameraX -~ dt * adjustedMovingSpeed)
+                     . whenDown keyboardS (worldCamera . cameraY -~ dt * adjustedMovingSpeed)
+                     . whenDown keyboardD (worldCamera . cameraX +~ dt * adjustedMovingSpeed)
+                     . (worldTime +~ dt * world ^. worldSpeed)
                      $ world
   where
     adjustedMovingSpeed = movingSpeed / world ^. worldCamera . cameraZoom
 
     whenDown :: Getter Keyboard KeyState
-             -> (Camera -> Camera)
+             -> (World -> World)
              -> (World -> World)
     whenDown keyboardKey f
       | world ^. worldKeyboard . keyboardKey == Down
-      = worldCamera %~ f
+      = f
       | otherwise
       = id
 
 test :: IO ()
-test = putStrLn "typechecks."
+test = do
+  _ <- system "stack build && write-your-own-frp"
+  putStrLn "done."
 
 main :: IO ()
 main = play (InWindow "gloss" (windowWidth, windowHeight) (800, 0))
             white
-            30
+            (round fps)
             initialWorld
-            (\world -> draw $ world ^. worldCamera)
+            draw
             (\event -> worldKeyboard %~ onEvent event)
             onTimeDelta
