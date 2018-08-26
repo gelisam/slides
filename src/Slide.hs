2083e5b4c548f8a431433e27beb399c4963fee9d
Samuel Gélineau <gelisam+github@gmail.com>
2018-08-26 16:42:52 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,18 +1,25 @@
 module Slide where
 
 scanS            :: (a -> b -> a) -> a -> Signal (Maybe b) -> Signal a
+mapMaybeS        :: (a -> Maybe b) -> Signal (Maybe a) -> Signal (Maybe b)
 
 events           :: Signal (Maybe Event)
+clicks           :: Signal (Maybe Pos)
 
 
-mapMaybeS :: (a -> Maybe b) -> Signal (Maybe a) -> Signal (Maybe b)
-mapMaybeS f = fmap (>>= f)
+data Button = Button { buttonColor :: Color
+                     , buttonRect  :: Rect }
 
-matchClick :: Event -> Maybe Pos
+isInside   :: Pos -> Rect -> Bool
+drawButton :: Button -> Image
 
-clicks :: Signal (Maybe Pos)
-clicks = mapMaybeS matchClick events
-
+buttonClicks :: Button -> Signal (Maybe Color)
+buttonClicks (Button {..}) = mapMaybeS f clicks
+  where
+    f :: Pos -> Maybe Color
+    f pos = if pos `isInside` buttonRect
+            then Just buttonColor
+            else Nothing
 
 
 
@@ -106,9 +113,18 @@ events = undefined
 
 scanS = undefined
 
-data Pos
+mapMaybeS f = fmap (>>= f)
 
+data Pos
+data Rect
+
+matchClick :: Event -> Maybe Pos
 matchClick = undefined
+
+clicks = mapMaybeS matchClick events
+
+isInside   = undefined
+drawButton = undefined
 
 
 test :: IO ()
