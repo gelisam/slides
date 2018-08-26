d2fe3351b19203fc54b134b5477ed3aff13dd8ef
Samuel Gélineau <gelisam+github@gmail.com>
2018-08-26 16:42:38 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,26 +1,23 @@
 module Slide where
-
+import Control.Applicative
 scanS            :: (a -> b -> a) -> a -> Signal (Maybe b) -> Signal a
 mapMaybeS        :: (a -> Maybe b) -> Signal (Maybe a) -> Signal (Maybe b)
 
 events           :: Signal (Maybe Event)
 clicks           :: Signal (Maybe Pos)
+buttonClicks     :: Button -> Signal (Maybe Color)
 
 
-data Button = Button { buttonColor :: Color
-                     , buttonRect  :: Rect }
+neverS :: Signal (Maybe a)
+neverS = pure Nothing
 
-isInside   :: Pos -> Rect -> Bool
-drawButton :: Button -> Image
+unionS :: Signal (Maybe a) -> Signal (Maybe a) -> Signal (Maybe a)
+unionS = liftA2 (<|>)
 
-buttonClicks :: Button -> Signal (Maybe Color)
-buttonClicks (Button {..}) = mapMaybeS f clicks
-  where
-    f :: Pos -> Maybe Color
-    f pos = if pos `isInside` buttonRect
-            then Just buttonColor
-            else Nothing
+colorButtons :: [Button]
 
+colorPicks :: Signal (Maybe Color)
+colorPicks = foldr unionS neverS (fmap buttonClicks colorButtons)
 
 
 
@@ -118,13 +115,30 @@ mapMaybeS f = fmap (>>= f)
 data Pos
 data Rect
 
+data Button = Button
+  { buttonColor :: Color
+  , buttonRect  :: Rect
+  }
+
+colorButtons = undefined
+
 matchClick :: Event -> Maybe Pos
 matchClick = undefined
 
 clicks = mapMaybeS matchClick events
 
+isInside   :: Pos -> Rect -> Bool
 isInside   = undefined
+
+drawButton :: Button -> Image
 drawButton = undefined
+
+buttonClicks (Button {..}) = mapMaybeS f clicks
+  where
+    f :: Pos -> Maybe Color
+    f pos = if pos `isInside` buttonRect
+            then Just buttonColor
+            else Nothing
 
 
 test :: IO ()
