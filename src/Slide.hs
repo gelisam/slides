d3bd3ce75f0bf3438cef71ebf0bc74e2f6b8bc82
Samuel Gélineau <gelisam+github@gmail.com>
2018-08-26 22:23:40 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,30 +1,21 @@
 module Slide where
 
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                               Write your own                               --
---                      Functional-Reactive-Programming                       --
---                                   library                                  --
---                                                                            --
---                            1. What is FRP?                                 --
---                            2. Example FRP program                          --
---                            3. Your own FRP library                         --
---                               step 1: test first!                          --
---                             > step 2: streams                              --
---                               step 3: lazy streams                         --
---                               step 4: input stream                         --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
 
 
+
+
+
+
+
+
+
+
+
+
+data Signal a = Signal
+  { signalHead :: a
+  , signalTail :: () -> Signal a
+  }
 
 
 
