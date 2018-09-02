7d96758292bc419950770a1436a5ef4adc5af7bb
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-02 13:56:52 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,21 +1,118 @@
--- fromList :: [a] -> Signal a
--- takeS    :: Int -> Signal a -> [a]
---
--- fmap     :: (a -> b) -> Signal a -> Signal b
--- pure     :: a -> Signal a
--- (<*>)    :: Signal (a -> b) -> Signal a -> Signal b
--- scanS    :: (a -> b -> a) -> a -> Signal (Maybe b) -> Signal a
---
--- >>> fmap (+10) [1,2,3,..]                            -->  [11,12,13,..]
--- >>> (+) <$> [1,2,3,..] <*> pure 10                   -->  [11,12,13,..]
--- >>> scanl (+) 0 [Just 1,Nothing,Just 20,Just 300,..] -->  [0,1,1,21,321,..]
 module Slide where
-import Test.DocTest
+import Test.DocTest                                                                                                                                                                                     ; import Control.Applicative
+
+-- |
+-- >>> :{
+-- let inputs = fromList
+--            [ Just (Color "Red"),  Just (Click 1), Just (Click 2)
+--            , Just (Color "Blue"), Just (Click 3), Just (Click 4)
+--            ]
+--     outputs = transform inputs
+-- in mapM_ print (takeS 6 outputs)
+-- :}
+-- Nothing
+-- Nothing
+-- Just (Segment "Red" (1,2))
+-- Nothing
+-- Nothing
+-- Just (Segment "Blue" (3,4))
+transform :: Signal (Maybe Event) -> Signal (Maybe Segment)
+transform events = outputs
+  where
+    outputs :: Signal (Maybe Segment)
+    outputs = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data Event = Color String | Click Int
+data Segment = Segment String (Int,Int)
+  deriving Show
+
 
 data Signal a = Signal
   { signalHead :: a
   , signalTail :: Signal a
   }
+  deriving Show
 
 fromList :: [a] -> Signal a
 fromList []     = error "fromList: empty list"
@@ -27,14 +124,10 @@ takeS 0 _             = []
 takeS n (Signal x xs) = x : takeS (n-1) xs
 
 instance Functor Signal where
-  fmap :: (a -> b) -> Signal a -> Signal b
   fmap f (Signal x xs) = Signal (f x) (fmap f xs)
 
 instance Applicative Signal where
-  pure :: a -> Signal a
   pure x = fromList [x]
-
-  (<*>) :: Signal (a -> b) -> Signal a -> Signal b
   Signal f fs <*> Signal x xs = Signal (f x) (fs <*> xs)
 
 scanS :: (a -> b -> a) -> a -> Signal (Maybe b) -> Signal a
@@ -43,100 +136,8 @@ scanS f x ys = Signal x $ case signalHead ys of
   Just y  -> scanS f (f x y) $ signalTail ys
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- |
--- >>> takeS 3 $ fmap (+10) (fromList [1,2,3])
--- [11,12,13]
--- >>> takeS 3 $ (+) <$> fromList [1,2,3] <*> pure 10
--- [11,12,13]
--- >>> takeS 5 $ scanS (+) 0 (fromList [Just 1,Nothing,Just 20,Just 300])
--- [0,1,1,21,321]
 test :: IO ()
-test = doctest ["-XInstanceSigs", "src/Slide.hs"]
+test = doctest ["-XScopedTypeVariables", "src/Slide.hs"]
 
 main :: IO ()
 main = putStrLn "typechecks."
