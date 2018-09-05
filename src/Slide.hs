5ae3c81986796f356f2dec5720dff73976d781b2
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-05 08:59:03 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,14 +1,14 @@
 module Slide where
-import Test.DocTest                                                                                                                                                                                    ; import Control.Applicative
+import Test.DocTest                                                                                                                                                                                     ; import Control.Applicative; import Data.IORef
 
 -- |
 -- >>> :{
--- let inputs = fromList
+-- do inputs <- fromList
 --            [ Just (Color "Red"),  Just (Click 1), Just (Click 2)
 --            , Just (Color "Blue"), Just (Click 3), Just (Click 4)
 --            ]
---     outputs = transform inputs
--- in mapM_ print (takeS 6 outputs)
+--    outputs <- transform inputs
+--    mapM_ print =<< takeS 6 outputs
 -- :}
 -- Nothing
 -- Nothing
@@ -16,7 +16,7 @@ import Test.DocTest                                                             
 -- Nothing
 -- Nothing
 -- Just (Segment "Blue" (3,4))
-transform :: Signal (Maybe Event) -> Signal (Maybe Segment)
+transform :: Signal (Maybe Event) -> IO (Signal (Maybe Segment))
 transform events = outputs
   where
     colorPicks :: Signal (Maybe String)
@@ -137,34 +137,76 @@ data Segment = Segment String (Int,Int)
 
 data Signal a = Signal
   { signalHead :: a
-  , signalTail :: Signal a
+  , signalTail :: Memoized (Signal a)
   }
-  deriving Show
 
-fromList :: [a] -> Signal a
-fromList []     = error "fromList: empty list"
-fromList [x]    = Signal x (fromList [x])
-fromList (x:xs) = Signal x (fromList xs)
 
-takeS :: Int -> Signal a -> [a]
-takeS 0 _             = []
-takeS n (Signal x xs) = x : takeS (n-1) xs
+fromList :: [a] -> IO (Signal a)
+fromList []  = error "fromList: empty list"
+fromList [x] = fromList [x,x]
+fromList (x:xs) = do
+  memo_xs <- memoize (fromList xs)
+  pure (Signal x memo_xs)
 
-instance Functor Signal where
-  fmap f (Signal x xs) = Signal (f x) (fmap f xs)
+takeS :: Int -> Signal a -> IO [a]
+takeS 0 _ = pure []
+takeS 1 (Signal x memo_xs) = pure [x]
+takeS n (Signal x memo_xs) = do
+  xs <- runMemoized memo_xs
+  (x:) <$> takeS (n-1) xs
 
-instance Applicative Signal where
-  pure x = fromList [x]
-  Signal f fs <*> Signal x xs = Signal (f x) (fs <*> xs)
 
-scanS :: (a -> b -> a) -> a -> Signal (Maybe b) -> Signal a
-scanS f x ys = Signal x $ case signalHead ys of
-  Nothing -> scanS f x       $ signalTail ys
-  Just y  -> scanS f (f x y) $ signalTail ys
+fmapS :: (a -> b) -> Signal a -> IO (Signal b)
+fmapS f (Signal x memo_xs) = do
+  let y = f x
+  memo_ys <- memoize $ do
+    xs <- runMemoized memo_xs
+    fmapS f xs
+  pure (Signal y memo_ys)
+
+pureS :: a -> IO (Signal a)
+pureS x = do
+  memo_xs <- memoize (pureS x)
+  pure (Signal x memo_xs)
+
+applyS :: Signal (a -> b) -> Signal a -> IO (Signal b)
+applyS (Signal f memo_fs) (Signal x memo_xs) = do
+  let y = f x
+  memo_ys <- memoize $ do
+    fs <- runMemoized memo_fs
+    xs <- runMemoized memo_xs
+    applyS fs xs
+  pure (Signal y memo_ys)
+
+scanS :: (a -> b -> a) -> a -> Signal (Maybe b) -> IO (Signal a)
+scanS f x signal = do
+  memo_xs <- memoize $ do
+    let x' = case signalHead signal of
+               Nothing -> x
+               Just y  -> f x y
+    signal' <- runMemoized (signalTail signal)
+    scanS f x' signal'
+  pure (Signal x memo_xs)
+
+
+newtype Memoized a = Memoized
+  { unMemoized :: IORef (Either a (IO a))
+  }
+
+memoize :: IO a -> IO (Memoized a)
+memoize action = Memoized <$> newIORef (Right action)
+
+runMemoized :: Memoized a -> IO a
+runMemoized (Memoized ref) = readIORef ref >>= \case
+  Left x -> pure x
+  Right action -> do
+    x <- action
+    writeIORef ref (Left x)
+    pure x
 
 
 test :: IO ()
-test = doctest ["-XScopedTypeVariables", "src/Slide.hs"]
+test = doctest ["-XLambdaCase", "-XScopedTypeVariables", "src/Slide.hs"]
 
 main :: IO ()
 main = putStrLn "typechecks."
