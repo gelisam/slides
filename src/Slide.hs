2ac2ac2f5bac3701d26bf67482b94b24834fb37c
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-05 09:59:48 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,29 +1,28 @@
 module Slide where
-import Test.DocTest                                                                                                                                                                                     ; import Control.Applicative; import Control.Concurrent.Chan; import Data.IORef; import System.Timeout
-import System.IO.Unsafe
+import Test.DocTest                                                                                                                                                                                     ; import Control.Applicative; import Control.Concurrent.Chan; import System.IO.Unsafe
 
-data Signal a = Signal
-  { signalHead :: a
-  , signalTail :: Signal a }
+data DiscreteApp state = DiscreteApp
+  { initialState :: IO state
+  , handleEvent  :: state -> Event -> IO state
+  , render       :: state -> IO Image }
 
--- |
--- >>> chan <- newChan :: IO (Chan String)
--- >>> strings <- fromChan chan
---
--- >>> writeChan chan "foo"
--- >>> signalHead strings
--- "foo"
---
--- >>> writeChan chan "bar"
--- >>> signalHead . signalTail $ strings
--- "bar"
---
--- >>> timeout 1 . print . signalHead . signalTail . signalTail $ strings
--- "Nothing
-fromChan :: Chan a -> IO (Signal a)
-fromChan chan = unsafeInterleaveIO
-              $ Signal <$> readChan chan <*> fromChan chan
+data MyState = MyState
+  { inputChannel :: Chan (Maybe Event)
+  , outputSignal :: Signal Image }
 
+toApp :: (Signal (Maybe Event) -> Signal Image)
+      -> DiscreteApp MyState
+toApp f = DiscreteApp
+  { initialState = do
+      chan <- newChan
+      inputs <- fromChan chan
+      let outputs = f inputs
+      pure $ MyState chan outputs
+  , handleEvent = \(MyState chan outputs) event -> do
+      writeChan chan (Just event)
+      pure $ MyState chan (signalTail outputs)
+  , render = pure . signalHead . outputSignal
+  }
 
 
 
@@ -95,12 +94,63 @@ fromChan chan = unsafeInterleaveIO
 
 
 
+
+
+
+transform :: Signal (Maybe Event) -> Signal (Maybe Segment)
+transform events = outputs
+  where
+    colorPicks :: Signal (Maybe String)
+    colorPicks = mapMaybeS f events
+      where
+        f (Color x) = Just x
+        f _ = Nothing
+
+    numberPicks :: Signal (Maybe Int)
+    numberPicks = mapMaybeS f events
+      where
+        f (Click x) = Just x
+        f _ = Nothing
+
+    currentColor :: Signal String
+    currentColor = lastS "" colorPicks
+
+    outputs :: Signal (Maybe Segment)
+    outputs = liftA2 Segment
+          <$> (Just <$> currentColor)
+          <*> pairS numberPicks
+
+mapMaybeS :: (a -> Maybe b) -> Signal (Maybe a) -> Signal (Maybe b)
+mapMaybeS f = fmap (>>= f)
+
+lastS :: a -> Signal (Maybe a) -> Signal a
+lastS = scanS (curry snd)
+
+pairS :: forall a. Signal (Maybe a) -> Signal (Maybe (a,a))
+pairS inputs = liftA2 (,) <$> prevs <*> inputs
+  where
+    toggle :: Maybe a -> a -> Maybe a
+    toggle Nothing  x = Just x
+    toggle (Just _) _ = Nothing
+
+    prevs :: Signal (Maybe a)
+    prevs = scanS toggle Nothing inputs
 
 
 data Event = Color String | Click Int
 data Segment = Segment String (Int,Int)
   deriving Show
+type Image = String
 
+
+data Signal a = Signal
+  { signalHead :: a
+  , signalTail :: Signal a
+  }
+
+fromChan :: Chan a -> IO (Signal a)
+fromChan chan = unsafeInterleaveIO
+              $ Signal <$> readChan chan <*> fromChan chan
 
 fromList :: [a] -> Signal a
 fromList []     = error "fromList: empty list"
@@ -125,7 +175,16 @@ scanS f x ys = Signal x $ case signalHead ys of
 
 
 test :: IO ()
-test = doctest ["src/Slide.hs"]
+test = do
+  let app = toApp (fmap show . transform)
+  state0 <- initialState app
+  let loop _     []             = pure ()
+      loop state (event:events) = do
+        state' <- handleEvent app state event
+        output <- render app state
+        putStrLn output
+        loop state' events
+  loop state0 [Color "Red", Click 1, Click 2, Color "Blue", Click 3, Click 4]
 
 main :: IO ()
 main = putStrLn "typechecks."
