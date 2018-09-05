6db3a5d70a3c4fdd0a6db2004d0a4ec79931fc16
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-05 07:35:08 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -8,23 +8,51 @@ data Signal a = Signal
 
 
 fromList :: [a] -> IO (Signal a)
-fromList = undefined
+fromList []  = error "fromList: empty list"
+fromList [x] = fromList [x,x]
+fromList (x:xs) = do
+  memo_xs <- memoize (fromList xs)
+  pure (Signal x memo_xs)
 
 takeS :: Int -> Signal a -> IO [a]
-takeS = undefined
+takeS 0 _ = pure []
+takeS 1 (Signal x memo_xs) = pure [x]
+takeS n (Signal x memo_xs) = do
+  xs <- runMemoized memo_xs
+  (x:) <$> takeS (n-1) xs
 
 
 fmapS :: (a -> b) -> Signal a -> IO (Signal b)
-fmapS f (Signal x memo_xs) = undefined
+fmapS f (Signal x memo_xs) = do
+  let y = f x
+  memo_ys <- memoize $ do
+    xs <- runMemoized memo_xs
+    fmapS f xs
+  pure (Signal y memo_ys)
 
 pureS :: a -> IO (Signal a)
-pureS x = undefined
+pureS x = do
+  memo_xs <- memoize (pureS x)
+  pure (Signal x memo_xs)
 
 applyS :: Signal (a -> b) -> Signal a -> IO (Signal b)
-applyS (Signal f memo_fs) (Signal x memo_xs) = undefined
+applyS (Signal f memo_fs) (Signal x memo_xs) = do
+  let y = f x
+  memo_ys <- memoize $ do
+    fs <- runMemoized memo_fs
+    xs <- runMemoized memo_xs
+    applyS fs xs
+  pure (Signal y memo_ys)
 
 scanS :: (a -> b -> a) -> a -> Signal (Maybe b) -> IO (Signal a)
-scanS f x signal = undefined
+scanS f x signal = do
+  memo_xs <- memoize $ do
+    let x' = case signalHead signal of
+               Nothing -> x
+               Just y  -> f x y
+    signal' <- runMemoized (signalTail signal)
+    scanS f x' signal'
+  pure (Signal x memo_xs)
 
 
 
