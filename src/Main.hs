a5696de636d394bdc2c15c8562b5fe2fd87efab9
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-23 07:52:03 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,28 @@
-module Main where  {- slide 7 of 7 -}
-
-
-finally :: IO (a, StateM) -> IO (b, StateM) -> IO (a, StateM)
-
-class Hard m where
-  liftAnything :: (IO a -> IO b -> IO a)
-               -> (m  a -> m  b -> m  a)
-
-class Easy m where
-  liftedFinally :: m a -> m b -> m a
-
-
-finally' :: IO (a, StateM)
-         -> (Maybe (a, StateM) -> IO (b, StateM))
-         -> IO (b, StateM)
-
-class Easier m where
-  liftedFinally' :: m a
-                 -> (Maybe a -> m b)
-                 -> m b
-
-
-
-
-
-
-
+-------------------------------------------------------------------------------
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                              Thank you!                                   --
+--                                                                           --
+--                    Go forth and implement libraries!                      --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+-------------------------------------------------------------------------------
 
 
 
@@ -57,11 +54,77 @@ class Easier m where
 
 
 
-data StateM
 
-finally = undefined
-finally' = undefined
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module Main where
+import Test.DocTest
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["src/Main.hs"]
