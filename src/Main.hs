63e40f2f1604dfc0b55768ea0d62410839824230
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-23 07:27:50 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
-module Main where  {- slide 2 of 7 -}
-import UnliftIO.Exception                                                                                                                                                                        ; import Test.DocTest; import Control.Monad.Except; import Control.Monad.Reader; import Control.Monad.State
+module Main where  {- slide 3 of 7 -}
+import Control.Monad.Catch  {- exceptions-0.10.0 -}                                                                                                                                                                     ; import Test.DocTest; import Control.Monad.Except; import Control.Monad.Reader; import Control.Monad.State
 
 -- |
 -- >>> runReaderT test1 "foo"
@@ -9,14 +9,14 @@ test1 = pure () `finally` do { r <- ask; lift (print r) }
 
 -- |
 -- >>> execStateT test2 ""
--- error: no instance for (MonadUnliftIO StateT s IO)
---
+-- "foo"
+-- "bar"
 test2 :: StateT String IO ()
 test2 = put "foo" `finally` do { s <- get; lift (print s); put "bar" }
 
 -- |
 -- >>> runExceptT test3
--- error: no instance for (MonadUnliftIO ExceptT e IO)
+-- Left "oops"
 test3 :: ExceptT String IO ()
 test3 = pure () `finally` throwError "oops"
 
