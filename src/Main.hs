84e6ae0184d9ea94b1e7549b0684abf0fe87f233
Samuel Gélineau <gelisam+github@gmail.com>
2018-09-23 07:27:16 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,24 @@
--------------------------------------------------------------------------------
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                         The trick which made                              --
---                      exceptions-0.10.0 possible                           --
---                                                                           --
---                          by Samuel Gélineau                               --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
--------------------------------------------------------------------------------
+module Main where  {- slide 1 of 7 -}
+import Control.Exception.Lifted                                                                                                                                                                        ; import Test.DocTest; import Control.Monad.Except; import Control.Monad.Reader; import Control.Monad.State
 
+-- |
+-- >>> runReaderT test1 "foo"
+-- "foo"
+test1 :: ReaderT String IO ()
+test1 = pure () `finally` do { r <- ask; lift (print r) }
 
+-- |
+-- >>> execStateT test2 ""
+-- ""
+-- "foo"
+test2 :: StateT String IO ()
+test2 = put "foo" `finally` do { s <- get; lift (print s); put "bar" }
 
+-- |
+-- >>> runExceptT test3
+-- Right ()
+test3 :: ExceptT String IO ()
+test3 = pure () `finally` throwError "oops"
 
 
 
@@ -123,8 +116,10 @@
 
 
 
-module Main where
-import Test.DocTest
+
+
+
+
 
 main :: IO ()
 main = doctest ["src/Main.hs"]
