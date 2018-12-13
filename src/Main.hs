4436377bd955cf1fa87ccafab01c97cdfe260e6b
Samuel Gélineau <gelisam+github@gmail.com>
2018-12-13 15:45:06 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,13 @@
 {-# LANGUAGE FlexibleContexts, TemplateHaskell#-}
 import Test.DocTest
 import Control.Lens
-import Data.Map (Map)
-import qualified Data.Map as Map
-
-data VmTemplate = VmTemplate
-  { _vmTemplateName :: String
-  } deriving Show
-
-data Specification = Specification
-  { _specificationVmTemplate :: Map Int VmTemplate
-  } deriving Show
-
-makeLenses ''VmTemplate
-makeLenses ''Specification
 
 
-indexedFold :: IndexedFold Int Specification String
-indexedFold = specificationVmTemplate . ifolded <. vmTemplateName
+traversal :: Traversal' [[a]] a
+traversal = traversed . traversed
 
--- |
--- >>> let spec = Specification (Map.fromList [(42, VmTemplate "foo")])
--- >>> findVmTemplateKeyByName "foo" spec
--- Just 42
-findVmTemplateKeyByName :: String -> Specification -> Maybe Int
-findVmTemplateKeyByName name = findIndexOf indexedFold (== name)
+fold :: Fold [[a]] a
+fold = traversed . folded
 
 
 
