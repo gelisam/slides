ad735a80eb0949f15684e0f92234d51608e1668b
Samuel Gélineau <gelisam+github@gmail.com>
2018-12-13 15:57:32 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,15 @@ import Test.DocTest
 import Control.Lens
 
 
+-- |
+-- >>> itoListOf indexedFold1 ["foo","bar"]
+-- [(0,'f'),(1,'o'),(2,'o'),(0,'b'),(1,'a'),(2,'r')]
 indexedFold1 :: IndexedFold Int [[a]] a
 indexedFold1 = ifolded . ifolded
 
+-- |
+-- >>> itoListOf indexedFold2 ["foo","bar"]
+-- [(0,'f'),(1,'o'),(2,'o'),(0,'b'),(1,'a'),(2,'r')]
 indexedFold2 :: IndexedFold Int [[a]] a
 indexedFold2 = folded . ifolded
 
