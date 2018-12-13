1021a185ed681e2b8d8da039a0bee85171a62070
Samuel Gélineau <gelisam+github@gmail.com>
2018-12-13 15:23:22 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,133 +1,112 @@
--------------------------------------------------------------------------
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                         Indexed Optics                              --
---                                                                     --
---                                                                     --
---                                   presented by                      --
---                                Samuel Gélineau                      --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
---                                                                     --
--------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{-# LANGUAGE FlexibleContexts, TemplateHaskell#-}
 import Test.DocTest
+import Control.Lens
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data VmTemplate = VmTemplate
+  { _vmTemplateName :: String
+  } deriving Show
+
+data Specification = Specification
+  { _specificationVmTemplate :: Map Int VmTemplate
+  } deriving Show
+
+makeLenses ''VmTemplate
+makeLenses ''Specification
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 main :: IO ()
 main = doctest ["src/Main.hs"]
