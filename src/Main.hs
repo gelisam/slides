39737c6a2e7d972b9e6c4e0620db7a589e7a2f24
Samuel Gélineau <gelisam+github@gmail.com>
2018-12-13 15:59:53 -0500

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,86 @@
-{-# LANGUAGE FlexibleContexts, TemplateHaskell#-}
+{-# LANGUAGE DeriveFunctor, RankNTypes, ScopedTypeVariables, TupleSections #-}
 import Test.DocTest
-import Control.Lens
+
+import Control.Applicative
+import Data.Monoid
+import Data.Traversable
+
+
+type IFrom b i a = a -> (i, b)
+type ISetter s t i a b = (i -> a -> b)
+                      -> (s -> t)
+type IFold s i a = forall m. Monoid m
+                => (i -> a -> m)
+                -> (s -> m)
+type ITraversal s t i a b = forall f. Applicative f
+                         => (i -> a -> f b)
+                         -> (s -> f t)
+
+composeFromsWith :: (i -> j -> k)
+                 -> IFrom c i b
+                 -> IFrom b j a
+                 -> IFrom c k a
+composeFromsWith ij f g a = let (j,b) = g a
+                                (i,c) = f b
+                            in (ij i j, c)
+
+composeSettersWith :: (i -> j -> k)
+                   -> ISetter s t i u v
+                   -> ISetter u v j a b
+                   -> ISetter s t k a b
+composeSettersWith ij stuv uvab f = stuv $ \i
+                                 -> uvab $ \j
+                                 -> f (ij i j)
+
+composeFoldsWith :: (i -> j -> k)
+                 -> IFold s i u
+                 -> IFold u j a
+                 -> IFold s k a
+composeFoldsWith ij su ua f = su $ \i
+                           -> ua $ \j
+                           -> f (ij i j)
+
+composeTraversalsWith :: (i -> j -> k)
+                      -> ITraversal s t i u v
+                      -> ITraversal u v j a b
+                      -> ITraversal s t k a b
+composeTraversalsWith ij stuv uvab f = stuv $ \i
+                                    -> uvab $ \j
+                                    -> f (ij i j)
+
+ifolded :: IFold [a] Int a
+ifolded f xs = mconcat . zipWith f [0..] $ xs
+
+itraversed :: ITraversal [a] [b] Int a b
+itraversed f xs = sequenceA . zipWith f [0..] $ xs
+
+findIndexOf :: Eq a
+            => IFold s i a
+            -> (a -> Bool)
+            -> s
+            -> Maybe i
+findIndexOf sa p = getFirst . sa go
+  where
+    go i a = if p a
+             then First (Just i)
+             else mempty
 
 
 -- |
--- >>> itoListOf indexedFold1 ["foo","bar"]
--- [((0,0),'f'),((0,1),'o'),((0,2),'o'),((1,0),'b'),((1,1),'a'),((1,2),'r')]
-indexedFold1 :: IndexedFold (Int,Int) [[a]] a
-indexedFold1 = ifolded <.> ifolded
+-- >>> example
+-- Just (1,0)
+example :: Maybe (Int, Int)
+example = findIndexOf (composeFoldsWith (,) ifolded ifolded)
+                      (== "fo")
+                      [ ["f","b","b"]
+                      , ["fo","ba","ba"]
+                      , ["foo","bar","baz"]
+                      ]
 
--- |
--- >>> itoListOf indexedFold2 ["foo","bar"]
--- [(0,'f'),(1,'o'),(2,'o'),(0,'b'),(1,'a'),(2,'r')]
-indexedFold2 :: IndexedFold Int [[a]] a
-indexedFold2 = folded . ifolded
+
+
+
+
 
 
 
