bab9c0665464437246968c88b924f053b9f5d0dc
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-17 10:16:21 -0500

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,24 +1,28 @@
 module Slide where
-import Test.DocTest                                                                                                    ; import Data.Foldable
+import Test.DocTest                                                                                                    ; import Control.Monad
 
--- |
--- >>> helloWorldIO
--- hello
--- world
-helloWorldIO :: IO ()
-helloWorldIO = do putStrLn "hello"
-                  putStrLn "world"
+-- >>> helloInputIO
+-- What is your name?
+-- <user types "Sam">
+-- Hello, Sam
+helloInputIO :: IO ()
+helloInputIO = do putStrLn "What is your name?"
+                  name <- getLine
+                  putStrLn ("Hello, " ++ name)
 
--- |
--- >>> manyNumbersIO
--- 1
--- 2
--- 3
--- 4
--- 5
-manyNumbersIO :: IO ()
-manyNumbersIO = for_ [1..5] $ \i -> do
-                  putStrLn (show i)
+-- >>> manyInputsIO
+-- How many numbers?
+-- <user types 2>
+-- Enter 2 numbers:
+-- <user types "100">
+-- <user types "200">
+-- Their sum is 300
+manyInputsIO :: IO ()
+manyInputsIO = do putStrLn "How many numbers?"
+                  n <- read <$> getLine
+                  putStrLn ("Enter " ++ show n ++ " numbers:")
+                  xs <- replicateM n (read <$> getLine)
+                  putStrLn ("Their sum is " ++ show (sum xs))
 
 
 
