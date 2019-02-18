932b9f4f64e21d991bc84d02a8af8b6d5d20f622
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-18 11:26:05 -0500

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -4,153 +4,113 @@ import Test.DocTest                                                             
 data Stmt = PutStrLn String
           | GetLine                                                                                                    deriving Show
 
-
-
--- >>> evalUI helloWorldList
--- >>> evalUI manyNumbersList
-evalUI :: [Stmt] -> IO ()
-evalUI stmts = do
-  xs <- execStateT (mapM go stmts) []
-  unless (xs == []) $ do
-    flMessage (unlines xs)
-  where
-    go :: ( MonadIO m
-          , MonadState [String] m
-          )
-       => Stmt -> m ()
-    go (PutStrLn s) = modify (++ [s])
-    go GetLine      = do xs <- get
-                         put []
-                         void $ liftIO $ flInput (unlines xs)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-isPutStrLn :: Stmt -> Bool
-isPutStrLn (PutStrLn _) = True
-isPutStrLn _            = False
-
-
-helloWorldList :: [Stmt]
-helloWorldList = [ PutStrLn "hello"
-                 , PutStrLn "world"
+helloInputList :: [Stmt]
+helloInputList = [ PutStrLn "What is your name?"
+                 , GetLine
+                 , PutStrLn ("Hello, " ++ name) -- "name" not in scope!
                  ]
 
-manyNumbersList :: [Stmt]
-manyNumbersList = execWriter $ do
-  for_ [1..5] $ \i -> do
-    tell [PutStrLn (show i)]
+manyInputsList :: [Stmt]
+manyInputsList = execWriter $ do
+  tell [PutStrLn "How many numbers?"]
+  n <- read <$> tell [GetLine]                  -- tell doesn't return a String!
+  tell [PutStrLn ("Enter " ++ show n ++ " numbers:")]
+  xs <- replicateM n (read <$> tell [GetLine])  -- tell doesn't return a String!
+  tell [PutStrLn ("Their sum is " ++ show (sum xs))]
 
 
-sendOutput :: MonadWriter [String] m => String -> m ()
-sendOutput s = tell [s]
-
-nextInput :: (MonadFail m, MonadState [String] m) => m String
-nextInput = do
-  (s:ss) <- get
-  put ss
-  pure s
 
 
-flMessage :: String -> IO ()
-flMessage = Ask.flMessage . Text.pack
 
-flInput :: String -> IO String
-flInput = fmap (Text.unpack . fromJust) . Ask.flInput . Text.pack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 main :: IO ()
-main = do
-  evalUI helloWorldList
-  evalUI manyNumbersList
+main = doctest ["src/Slide.hs"]
