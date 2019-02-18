afb8c0e5be38ffa41163aedf6889598dc2645cc9
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-17 22:16:00 -0500

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,119 +1,143 @@
 module Slide where
-import Test.DocTest                                                                                                    ; import Data.Foldable; import Control.Monad.Writer
+import Test.DocTest                                                                                                    ; import Control.Monad; import Control.Monad.Fail; import Control.Monad.State; import Control.Monad.Writer; import Data.Foldable
 
 data Stmt = PutStrLn String
           | GetLine                                                                                                    deriving Show
+
+-- |
+-- >>> evalIO helloWorldList
+-- hello
+-- world
+-- >>> evalIO manyNumbersList
+-- 1
+-- 2
+-- 3
+-- 4
+-- 5
+evalIO :: [Stmt] -> IO ()
+evalIO = mapM_ go
+  where
+    go :: Stmt -> IO ()
+    go (PutStrLn s) = putStrLn s
+    go GetLine      = void getLine
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 helloWorldList :: [Stmt]
 helloWorldList = [ PutStrLn "hello"
                  , PutStrLn "world"
                  ]
 
--- |
--- >>> manyNumbersList
--- [PutStrLn "1",PutStrLn "2",PutStrLn "3",PutStrLn "4",PutStrLn "5"]
 manyNumbersList :: [Stmt]
 manyNumbersList = execWriter $ do
   for_ [1..5] $ \i -> do
     tell [PutStrLn (show i)]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sendOutput :: MonadWriter [String] m => String -> m ()
+sendOutput s = tell [s]
+
+nextInput :: (MonadFail m, MonadState [String] m) => m String
+nextInput = do
+  (s:ss) <- get
+  put ss
+  pure s
 
 
 main :: IO ()
-main = doctest ["src/Slide.hs"]
+main = doctest ["-XFlexibleContexts", "src/Slide.hs"]
