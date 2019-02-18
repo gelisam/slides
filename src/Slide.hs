618964fa8bea332f93b78f26e8dafd01d7de7d9a
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-18 10:52:09 -0500

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -2,21 +2,26 @@ module Slide where
 import Test.DocTest                                                                                                    ; import Control.Monad; import Control.Monad.Fail; import Control.Monad.State; import Control.Monad.Writer; import Data.Foldable; import Data.Maybe; import qualified Data.Text as Text; import qualified Graphics.UI.FLTK.LowLevel.Ask as Ask; import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 
 data Stmt = PutStrLn String
-          | GetLine                                                                                                    deriving Show
+          | GetLine {- binds a String -}                                                                             deriving Show
 
-helloInputList :: [Stmt]
-helloInputList = [ PutStrLn "What is your name?"
-                 , GetLine
-                 , PutStrLn ("Hello, " ++ name) -- "name" not in scope!
-                 ]
-
-manyInputsList :: [Stmt]
-manyInputsList = execWriter $ do
-  tell [PutStrLn "How many numbers?"]
-  n <- read <$> tell [GetLine]                  -- tell doesn't return a String!
-  tell [PutStrLn ("Enter " ++ show n ++ " numbers:")]
-  xs <- replicateM n (read <$> tell [GetLine])  -- tell doesn't return a String!
-  tell [PutStrLn ("Their sum is " ++ show (sum xs))]
+      --------------------------------------------------------------
+      --                                                          --
+      --               PutStrLn s1                                --
+      --                    |                                     --
+      --                 GetLine      -- binds x1                 --
+      --                    |                                     --
+      --               PutStrLn s2    -- x1 is in scope           --
+      --                    |                                     --
+      --               PutStrLn s3    -- x1 is in scope           --
+      --                    |                                     --
+      --                 GetLine      -- binds x2                 --
+      --                    |                                     --
+      --               PutStrLn s4    -- x1 and x2 are in scopy   --
+      --                    |                                     --
+      --                   Nil                                    --
+      --                                                          --
+      --                                                          --
+      --------------------------------------------------------------
 
 
 
