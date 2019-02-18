a65f444068bf8239a473bd2bf3ad36a28b962a5f
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-18 11:53:43 -0500

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,13 +1,22 @@
 module Slide where
+import Test.DocTest                                                                                                    ; import Control.Monad; import Control.Monad.Fail; import Control.Monad.State; import Control.Monad.Writer; import Data.Foldable; import Data.Maybe; import qualified Data.Text as Text; import qualified Graphics.UI.FLTK.LowLevel.Ask as Ask; import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 
-data StmtG a where
-  PutStrLnG :: String -> StmtG ()
-  GetLineG  :: StmtG String
+data Free f a = Pure a | Deep (f (Free f a))
+
+instance Functor f => Monad (Free f) where
+  return = Pure
+  Pure a     >>= cc = cc a
+  Deep fFree >>= cc = Deep (fmap (>>= cc) fFree)
+
 
 data Freer g a where
   Purer  :: a -> Freer g a
   Deeper :: g x -> (x -> Freer g a) -> Freer g a
 
+instance Monad (Freer g) where
+  return = Purer
+  Purer a           >>= cc = cc a
+  Deeper gx toFreer >>= cc = Deeper gx (\x -> toFreer x >>= cc)
 
 
 
@@ -92,9 +101,22 @@ data Freer g a where
 
 
 
+instance Functor f => Functor (Free f) where
+  fmap f (Pure a)     = Pure (f a)
+  fmap f (Deep fFree) = Deep (fmap (fmap f) fFree)
 
+instance Functor f => Applicative (Free f) where
+  pure = Pure
+  (<*>) = ap
 
 
+instance Functor (Freer g) where
+  fmap g (Purer a)           = Purer (g a)
+  fmap g (Deeper gx toFreer) = Deeper gx (\x -> fmap g (toFreer x))
+
+instance Applicative (Freer g) where
+  pure = Purer
+  (<*>) = ap
 
 
 main :: IO ()
