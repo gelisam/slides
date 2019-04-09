b61ea21ac76f301d15f4854cd34b39f63047e770
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-08 21:54:53 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -11,9 +11,9 @@ module Slide where
 --                                                                            --
 --                           1. the problem                                   --
 --                         v 2. the solution                                  --
---                           > a. Eliminate constraints as early              --
+--                             a. Eliminate constraints as early              --
 --                                as possible, not in main                    --
---                             b. Provide an easy-to-call variant             --
+--                           > b. Provide an easy-to-call variant             --
 --                                of runFooT dedicated for testing            --
 --                             c. Never use MonadReader                       --
 --                           3. how it could be better                        --
