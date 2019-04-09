dc21c817457a8c2ed5ec1bd2f9e8cac0874f2f4a
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-08 23:19:19 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,31 +1,36 @@
 module Slide where
-import Control.Monad.State                                                                                                    ; import Control.Applicative; import Control.Lens; import Data.Foldable; import Data.IORef; import Data.Map (Map, (!)); import qualified Data.Map as Map
 
-interpretOrg :: (MonadState s m, HasHardhatSpec s)
-             => WizardForm -> m ()
-interpretOrg (WizardForm {..}) = do
-  for_ _wizardFormOrgs $ \(OrgForm {..}) -> do
-    spec <- use hardhatSpecL
-    let cidr = initCidrAllocation _orgFormInternalDomainCidr
-        s = HardhatSpecAndCidrAllocation spec cidr
-    spec' <- flip evalStateT s $ do
-      interpretDmz   _orgFormDmz
-      interpretInfra _orgFormInfra
-      interpretTools _orgFormTools
-      use hardhatSpecL
-    hardhatSpecL .= spec'
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                            The Places Pattern                              --
+--                                                                            --
+--                                                                            --
+--                           1. the problem                                   --
+--                         v 2. the solution                                  --
+--                           > a. Eliminate constraints as early              --
+--                                as possible, not in main                    --
+--                             b. Provide an easy-to-call variant             --
+--                                of runFooT dedicated for testing            --
+--                             c. Never use MonadReader                       --
+--                           3. how it could be better                        --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
-data HardhatSpecAndCidrAllocation = HardhatSpecAndCidrAllocation
-  { _hardhatSpec    :: HardhatSpec
-  , _cidrAllocation :: CidrAllocation
-  }
 
-instance HasHardhatSpec    HardhatSpecAndCidrAllocation where
-  hardhatSpecL = hardhatSpec
 
-instance HasCidrAllocation HardhatSpecAndCidrAllocation where
-  cidrAllocationL = cidrAllocation
 
 
 
@@ -99,47 +104,18 @@ instance HasCidrAllocation HardhatSpecAndCidrAllocation where
 
 
 
-class HasHardhatSpec s where
-  hardhatSpecL :: Lens' s HardhatSpec
-
-class HasCidrAllocation s where
-  cidrAllocationL :: Lens' s CidrAllocation
-
-
-data HardhatSpec
-data CidrBlock
-data CidrAllocation
-
-initCidrAllocation :: CidrBlock -> CidrAllocation
-initCidrAllocation = undefined
-
-data WizardForm = WizardForm
-  { _wizardFormOrgs :: [OrgForm]
-  }
-
-data OrgForm = OrgForm
-  { _orgFormInternalDomainCidr :: CidrBlock
-  , _orgFormDmz                :: DmzForm
-  , _orgFormInfra              :: InfraForm
-  , _orgFormTools              :: ToolsForm
-  }
-
-data DmzForm
-data InfraForm
-data ToolsForm
-
-interpretDmz   :: (MonadState s m, HasHardhatSpec s, HasCidrAllocation s) => DmzForm -> m ()
-interpretInfra :: (MonadState s m, HasHardhatSpec s, HasCidrAllocation s) => InfraForm -> m ()
-interpretTools :: (MonadState s m, HasHardhatSpec s, HasCidrAllocation s) => ToolsForm -> m ()
-interpretDmz   = undefined
-interpretInfra = undefined
-interpretTools = undefined
-
-hardhatSpec :: Lens' HardhatSpecAndCidrAllocation HardhatSpec
-hardhatSpec = lens _hardhatSpec (\s a -> s { _hardhatSpec = a })
-
-cidrAllocation :: Lens' HardhatSpecAndCidrAllocation CidrAllocation
-cidrAllocation = lens _cidrAllocation (\s a -> s { _cidrAllocation = a })
+
+
+
+
+
+
+
+
+
+
+
+
 
 main :: IO ()
 main = putStrLn "typechecks."
