9972654a9b43820133746379417df5e3687f1525
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-08 21:53:15 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,40 +1,30 @@
 module Slide where
+import Control.Monad.Reader                                                                                                    ; import Control.Lens; import Data.IORef; import Data.Map (Map, (!)); import qualified Data.Map as Map
 
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                            The Places Pattern                              --
---                                                                            --
---                                                                            --
---                         > 1. the problem                                   --
---                           2. the solution                                  --
---                           3. how it could be better                        --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
+data App = App
+  { _appFlightsRef  :: IORef (Map FlightNo Flight)
+  , _appStripeCreds :: StripeCreds
+  }
 
+type Handler = ReaderT App IO
 
 
 
 
+listAvailableFlights :: Handler (Map FlightNo Flight)
+listAvailableFlights = do
+  allFlights <- listAllFlights
+  pure $ Map.filter (not . flightFull) allFlights
 
+bookFlight :: FlightNo -> StripeCard -> Handler ()
+bookFlight flightNo stripeCard = do
+  allFlights <- listAllFlights
+  let flight = allFlights ! flightNo
 
+  guard $ (not . flightFull) flight
 
+  chargeCard stripeCard (flightPrice flight)
+  increasePassengerCount flightNo
 
 
 
@@ -116,6 +106,50 @@ module Slide where
 
 
 
+
+
+listAllFlights
+  :: (MonadIO m, MonadReader App m)
+  => m (Map FlightNo Flight)
+listAllFlights = do
+  flightsRef <- view appFlightsRef
+  liftIO $ readIORef flightsRef
+
+increasePassengerCount
+  :: (MonadIO m, MonadReader App m)
+  => FlightNo -> m ()
+increasePassengerCount = undefined
+
+chargeCard :: (MonadIO m, MonadReader App m)
+           => StripeCard -> Price -> m ()
+chargeCard stripeCard price = undefined
+
+
+
+type FlightNo = Int
+type Flight = String
+type Price = Double
+
+flightFull :: Flight -> Bool
+flightFull _ = False
+
+flightPrice :: Flight -> Price
+flightPrice _ = 100.00
+
+addPassenger :: Flight -> Flight
+addPassenger = id
+
+data StripeCreds = StripeCreds
+  { _stripeUsername :: String
+  , _stripePassword :: String
+  }
+type StripeCard = String
+
+appFlightsRef :: Lens' App (IORef (Map FlightNo Flight))
+appFlightsRef = lens _appFlightsRef (\app x -> app {_appFlightsRef = x})
+
+appStripeCreds :: Lens' App StripeCreds
+appStripeCreds = lens _appStripeCreds (\app x -> app {_appStripeCreds = x})
 
 main :: IO ()
 main = putStrLn "typechecks."
