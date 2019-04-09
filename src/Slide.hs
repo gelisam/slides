1af060f4268ddb9d5ffd5e9f99438758b3a938cf
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-08 22:50:44 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -13,7 +13,12 @@ type FlightsT m a = ReaderT (IORef (Map FlightNo Flight)) m a
 type MonadPayment m = MonadReader StripeCreds m
 type PaymentT m a = ReaderT StripeCreds m a
 
+increasePassengerCount :: MonadFlights m => m ()
+chargeCard             :: MonadPayment m => m ()
 
+bookFlight :: (MonadFlights m, MonadPayment m) => m ()
+bookFlight = do increasePassengerCount
+                chargeCard
 
 
 
@@ -84,21 +89,19 @@ type PaymentT m a = ReaderT StripeCreds m a
 
 
 
-
-
-
-
-
-
+increasePassengerCount = undefined
+chargeCard = undefined
 
 
 type FlightNo = Int
 type Flight = String
+type Price = Double
 
 data StripeCreds = StripeCreds
   { _stripeUsername :: String
   , _stripePassword :: String
   }
+type StripeCard = String
 
 
 main :: IO ()
