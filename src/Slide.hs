9046f2bd08c3b54fa9eb5cc98661606f03f08474
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-25 12:27:39 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,37 +1,37 @@
 module Slide where
 
-import Control.Monad.Reader
-import Control.Monad.State
-import Control.Monad.Writer
-import Control.Monad.Trans.Control
-import Control.Monad.IO.Class
-import Data.Aeson ((.=))
-import qualified Data.Aeson as Aeson
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                               Effect Systems                               --
+--                                                                            --
+--              * higher-order effects                                        --
+--                * what happened?                                            --
+--                * higher-order effects                                      --
+--                * what to do about them                                     --
+--                  * shuffle the effects around                              --
+--                    * recreate the layers                                   --
+--                  > * MonadBaseControl                                      --
+--                    * liftCodensityIO                                       --
+--                  * avoid fancy effects                                     --
+--                  * more typeclasses                                        --
+--              * other topics                                                --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
-runMyFileTest :: IO ()
-runMyFileTest = runGoldenTest $ do
-  -- StM m a
-  stM <- liftBaseWith $ \runInIO -> do                       -- 1
-    liftIO $ withFile "deployment-key.txt" $ \handle -> do
-      runInIO $ do                                           -- 2
-        deploymentKey <- hGetContents handle
-        sendPayload $ Aeson.object
-          [ "request"       .= Aeson.String "getPowerStates" 
-          , "deploymentKey" .= deploymentKey
-          ]
-  restoreM stM                                               -- 3
 
 
-withFile :: FilePath
-         -> (Handle -> IO a)
-         -> IO a
 
-runGoldenTest :: ReaderT Connection
-                   (StateT [Aeson.Value]
-                     (WriterT [Aeson.Value]
-                       IO)) a
-              -> IO a
 
 
 
@@ -111,36 +111,6 @@ runGoldenTest :: ReaderT Connection
 
 
 
-
-
-
-
-data Handle = Handle
-data Connection = Connection
-
-withFile _ body = body Handle
-
-hGetContents :: MonadIO m => Handle -> m String
-hGetContents Handle = pure "<file contents>"
-
-sendPayload :: ( MonadReader Connection m
-               , MonadState [Aeson.Value] m
-               , MonadWriter [Aeson.Value] m
-               )
-            => Aeson.Value
-            -> m ()
-sendPayload x = do
-  modify (++ [x])
-  tell [x]
-
-runGoldenTest body = do
-  ((a, s), w) <- runWriterT
-               . flip runStateT []
-               . flip runReaderT Connection
-               $ body
-  putStrLn $ "final state: " ++ show s
-  putStrLn $ "final write: " ++ show w
-  pure a
 
 
 main :: IO ()
