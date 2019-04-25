9a1be6a3fc12fdcfbfc9bed09facd099524d8a15
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-25 11:33:33 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -11,8 +11,8 @@ module Slide where
 --              * higher-order effects                                        --
 --                * what happened?                                            --
 --                * higher-order effects                                      --
---              v * what to do about them                                     --
---                  * shuffle the effects around                              --
+--                * what to do about them                                     --
+--                > * shuffle the effects around                              --
 --                  * avoid fancy effects                                     --
 --                  * more typeclasses                                        --
 --              * other topics                                                --
