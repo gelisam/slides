b20867524ed2f1b0587a27d3f44be1d5a84056b2
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-25 15:37:17 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,20 +1,32 @@
 module Slide where
 
-handleGetPowerStates :: Key Deployment -> Handler ()
-handleGetPowerStates deploymentKey = do
-  forkIO $ do  -- expected IO (Map ...), got m (Map ...)
-    r <- getPowerStates deploymentKey
-    print r
-
-forkIO :: IO a -> IO ThreadId
-
-getPowerStates :: ( MonadDeployments m
-                  , MonadVmInstances m
-                  , MonadPowerStates m
-                  )
-               => Key Deployment -> m (Map VmInstanceKey PowerState)
-
-
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                               Effect Systems                               --
+--                                                                            --
+--              * higher-order effects                                        --
+--              * other topics                                                --
+--              > * abstraction bitrot                                        --
+--                * single-implementation vs multiple-implementations         --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
 
