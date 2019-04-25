4763abb2cb3cb3b5db65d7f690131d51cc251e96
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-24 22:48:57 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -2,11 +2,20 @@ module Slide where
 
 handleGetPowerStates :: Key Deployment -> Handler ()
 handleGetPowerStates deploymentKey = do
-  forkIO $ do
+  forkIO $ do  -- expected IO (Map ...), got m (Map ...)
     r <- getPowerStates deploymentKey
     print r
 
+forkIO :: IO a -> IO ThreadId
 
+getPowerStates :: ( MonadBuilder m
+                  , MonadDataServer m
+                  , MonadError Text m
+                  , MonadHypervisor m
+                  , MonadLogger m
+                  , MonadTraced m
+                  )
+               => Key Deployment -> m (Map VmInstanceKey PowerState)
 
 
 
