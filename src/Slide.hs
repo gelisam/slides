e8abe3d785a7f1aee24ddf124a700c9dae742ba7
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-25 13:32:01 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,37 +1,36 @@
 module Slide where
 
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                               Effect Systems                               --
---                                                                            --
---              * higher-order effects                                        --
---                * what happened?                                            --
---                * higher-order effects                                      --
---                * what to do about them                                     --
---                  * shuffle the effects around                              --
---                > * avoid fancy effects                                     --
---                  * more typeclasses                                        --
---              * other topics                                                --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
+import Control.Concurrent
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Trans.Control
+import Control.Monad.IO.Class
+import Data.Aeson ((.=))
+import qualified Data.Aeson as Aeson
 
 
+runMyFileTest :: IO ()
+runMyFileTest = runGoldenTest $ do
 
+  liftBaseWith $ \runInIO -> do
+    threadId <- liftIO $ forkIO $ do
+      stM <- runInIO $ do
+        deploymentKey <- hGetContents
+        sendPayload $ Aeson.object
+          [ "request"       .= Aeson.String "getPowerStates" 
+          , "deploymentKey" .= deploymentKey
+          ]
+      pure ()
+    pure ()
+  --restoreM stM
 
 
+runGoldenTest :: ReaderT Connection
+                   (StateT [Aeson.Value]
+                     (WriterT [Aeson.Value]
+                       IO)) a
+              -> IO a
 
 
 
@@ -111,6 +110,44 @@ module Slide where
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+data Handle = Handle
+data Connection = Connection
+
+withFile _ body = body Handle
+
+hGetContents :: MonadIO m => m String
+hGetContents = pure "<file contents>"
+
+sendPayload :: ( MonadReader Connection m
+               , MonadState [Aeson.Value] m
+               , MonadWriter [Aeson.Value] m
+               )
+            => Aeson.Value
+            -> m ()
+sendPayload x = do
+  modify (++ [x])
+  tell [x]
+
+runGoldenTest body = do
+  ((a, s), w) <- runWriterT
+               . flip runStateT []
+               . flip runReaderT Connection
+               $ body
+  putStrLn $ "final state: " ++ show s
+  putStrLn $ "final write: " ++ show w
+  pure a
 
 
 main :: IO ()
