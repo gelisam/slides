ad89aefc3c2e5c8eaf8d5ecf341144d6f8e628bb
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:44:04 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,42 +1,16 @@
 module Slide where
 
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                               Effect Systems                               --
---                                                                            --
---              * higher-order effects                                        --
---              * other topics                                                --
---                * abstraction bitrot                                        --
---                * single-implementation vs multiple-implementations         --
---                  * implementation-based vs semantic-based                  --
---                  * cost/benefit                                            --
---                  * concrete vs abstract                                    --
---                > * testing                                                 --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
 
 
 
+insertSpec   :: Spec -> DataServerT m (Key Spec)
+retrieveSpec :: Key Spec -> DataServerT m Spec
+-- ...
 
 
-
-
-
-
-
+data DataServerT m a = DataServerT
+  { unDataServerT :: ReaderT Url m a
+  }
 
 
 
