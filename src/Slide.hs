bc00a1bc62aac6e11987a417166708f2725b11ed
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:38:21 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,43 +1,17 @@
 module Slide where
 
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                               Effect Systems                               --
---                                                                            --
---              * higher-order effects                                        --
---              * other topics                                                --
---                * abstraction bitrot                                        --
---                * single-implementation vs multiple-implementations         --
---                  * implementation-based vs semantic-based                  --
---                  * cost/benefit                                            --
---                > * concrete vs abstract                                    --
---                  * testing                                                 --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
+class Monad m => MonadDataServer m where
+  getDataServerUrl :: m Url
+
+insertSpec   :: MonadDataServer m => Spec -> m (Key Spec)
+retrieveSpec :: MonadDataServer m => Key Spec -> m Spec
+-- ...
 
 
-
-
-
-
-
-
-
-
-
+data DataServerT m a = DataServerT
+  { unDataServerT :: ReaderT Url m a
+  }
+instance MonadDataServer (DataServerT m)
 
 
 
