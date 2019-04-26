b1386a0a325a5e3003ac856e32331cbad3ead130
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 11:57:52 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -11,8 +11,8 @@ module Slide where
 --              * higher-order effects                                        --
 --              * other topics                                                --
 --                * abstraction bitrot                                        --
---              v * single-implementation vs multiple-implementations         --
---                  * implementation-based vs semantic-based                  --
+--                * single-implementation vs multiple-implementations         --
+--                > * implementation-based vs semantic-based                  --
 --                  * cost/benefit                                            --
 --                  * concrete vs abstract                                    --
 --                                                                            --
