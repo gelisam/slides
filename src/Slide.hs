3866be09b8716c8420618eae9a4c3a679dd70b17
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 11:48:29 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,26 +1,32 @@
 module Slide where
 
-                                 fork :: MonadUnliftIO m
-forkIO :: IO a -> IO ThreadId         => m a -> m ThreadId
-                     --MonadUnliftIO-->
-
-                  --(MonadIO, MonadMask)-->
-                                 withFile :: (MonadIO m, MonadMask m)
-withFile :: FilePath                      => FilePath
-         -> (Handle -> IO a)              -> (Handle -> m a)
-         -> IO a                          -> m a
-
-
-
-                                 traverse :: Applicative f
-fmap :: (a -> b)                          => (a -> f b)
-     -> [a] -> [b]                        -> [a] -> f [b]
-                     --Applicative-->
-
-                        --Monad-->
-                                 unfoldrM :: Monad m
-unfoldr :: (a -> Maybe (b, a))            => (a -> m (Maybe (b, a)))
-        -> a -> [b]                       -> a -> m [b]
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                               Effect Systems                               --
+--                                                                            --
+--              * higher-order effects                                        --
+--                * what happened?                                            --
+--                * higher-order effects                                      --
+--                * what to do about them                                     --
+--                  * shuffle the effects around                              --
+--                  * avoid fancy effects                                     --
+--                > * more typeclasses                                        --
+--              * other topics                                                --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
 
