bca5d1415c6e10dc5f1d285391c3110b201b8350
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:45:51 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,22 +1,45 @@
 module Slide where
 
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                Thank you!                                  --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
 
-insertSpec   :: Spec -> DataServerT m (Key Spec)
-retrieveSpec :: Key Spec -> DataServerT m Spec
--- ...
 
 
-data DataServerT m a = DataServerT
-  { unDataServerT :: ReaderT Url m a
-  }
 
-runDataServerT :: DataServerT m a
-               -> Url -> m a
 
-testDataServerT :: (MonadIO m, MonadMask m)
-                => DataServerT m a -> m a
+
+
+
+
+
+
 
 
 
