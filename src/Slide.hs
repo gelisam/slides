a885a5fcb6666fd2dcbd2f2f00fae4b6fe0ad609
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:35:52 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -13,11 +13,11 @@ module Slide where
 --                * abstraction bitrot                                        --
 --                * single-implementation vs multiple-implementations         --
 --                  * implementation-based vs semantic-based                  --
---                > * cost/benefit                                            --
+--                v * cost/benefit                                            --
+--                    * api surface                                           --
+--                    * cost of creating new abstractions                     --
 --                  * concrete vs abstract                                    --
 --                  * testing                                                 --
---                                                                            --
---                                                                            --
 --                                                                            --
 --                                                                            --
 --                                                                            --
