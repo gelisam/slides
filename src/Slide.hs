565878e675cd0c585eee4621cb4105ef5d22aff3
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:29:50 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,17 +1,21 @@
 module Slide where
 
+class Monad m => MonadDataServer m where
+  getDataServerUrl :: m Url
 
-
-
-insertSpec   :: Spec -> DataServerT m (Key Spec)
-retrieveSpec :: Key Spec -> DataServerT m Spec
+insertSpec   :: MonadDataServer m => Spec -> m (Key Spec)
+retrieveSpec :: MonadDataServer m => Key Spec -> m Spec
 -- ...
 
 
 data DataServerT m a = DataServerT
   { unDataServerT :: ReaderT Url m a
   }
+instance MonadDataServer (DataServerT m)
 
+instance MonadDataServer m => MonadDataServer (ReaderT m)
+instance MonadDataServer m => MonadDataServer (StateT  m)
+instance MonadDataServer m => MonadDataServer (WriterT m)
 
 
 
