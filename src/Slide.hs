6aaf219c177ba9ec93c035146a6e17d7ec53698d
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 12:39:44 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -9,9 +9,9 @@ module Slide where
 --                               Effect Systems                               --
 --                                                                            --
 --              * higher-order effects                                        --
---            v * other topics                                                --
---                * abstraction bitrot                                        --
---                * single-implementation vs multiple-implementations         --
+--            > * other topics                                                --
+--                                                                            --
+--                                                                            --
 --                                                                            --
 --                                                                            --
 --                                                                            --
