c7baf313df507bd97463fbba3a555af3c09bd9b1
Samuel Gélineau <gelisam+github@gmail.com>
2019-04-26 11:49:37 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -8,10 +8,10 @@ module Slide where
 --                                                                            --
 --                               Effect Systems                               --
 --                                                                            --
---              * higher-order effects                                        --
+--            v * higher-order effects                                        --
 --                * what happened?                                            --
 --                * higher-order effects                                      --
---              > * what to do about them                                     --
+--                * what to do about them                                     --
 --              * other topics                                                --
 --                                                                            --
 --                                                                            --
