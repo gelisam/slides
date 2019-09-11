9b7a2a38dbbbb7dafc29f6bb482c594cf9a21228
Samuel Gélineau <gelisam@simspace.com>
2019-09-11 14:22:10 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -5,15 +5,15 @@
 --                                                                            --
 --                                                                            --
 --                            * Mixing                                        --
---                          v * Multiple Styles                               --
---                              * monadic                                     --
---                                * monad transformer library (mtl)           --
---                                * transformers                              --
---                                * record of functions (rof)                 --
---                                * free monad (free)                         --
---                              * functional reactive programming (frp)       --
---                              * combinator library (combo)                  --
+--                          > * Multiple Styles                               --
 --                            * in the Same Haskell Program                   --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
 --                                                                            --
 --                                                                            --
 --                                                                            --
