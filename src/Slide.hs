f6804811bcd5ea0f1a04343e03a5e0a24d311b40
Samuel Gélineau <gelisam@simspace.com>
2019-09-13 15:27:31 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,42 +1,28 @@
-import Control.Applicative
-import Control.Monad
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                            * Mixing                                        --
+--                            * Multiple Styles                               --
+--                              * monadic                                     --
+--                                * monad transformer library (mtl)           --
+--                                * transformers                              --
+--                                * record of functions (rof)                 --
+--                                * free monad (free)                         --
+--                              * functional reactive programming (frp)       --
+--                            > * combinator library (combo)                  --
+--                            * in the Same Haskell Program                   --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
 
-type FizzBuzz = Int -> Maybe String
-
-runFizzBuzz :: FizzBuzz -> Int -> String
-runFizzBuzz f n = case f n of
-  Just s  -> s
-  Nothing -> show n
-
-ifMultipleOf :: String -> Int -> FizzBuzz
-ifMultipleOf word n i = do
-  guard (i `mod` n == 0)
-  pure word
-
-firstOf :: [FizzBuzz] -> FizzBuzz
-firstOf []     _ = Nothing
-firstOf (f:fs) i = f i <|> firstOf fs i
-
-lastOf :: [FizzBuzz] -> FizzBuzz
-lastOf = firstOf . reverse
-
-allOf :: [FizzBuzz] -> FizzBuzz
-allOf []     _ = pure ""
-allOf (f:fs) i = (++) <$> f i <*> allOf fs i
-
---
-
-fizz, buzz, fizzbuzz, fizzbuzz1 :: FizzBuzz
-fizz = "fizz" `ifMultipleOf` 3
-buzz = "buzz" `ifMultipleOf` 5
-fizzbuzz = allOf [fizz, buzz]
-fizzbuzz1 = firstOf [fizzbuzz, fizz, buzz]
-
-fizzbuzz100 :: IO ()
-fizzbuzz100 = do
-  let strings = fmap (runFizzBuzz fizzbuzz1) [1..100]
-  mapM_ putStrLn strings
 
 
 
@@ -136,4 +122,4 @@ fizzbuzz100 = do
 
 
 main :: IO ()
-main = fizzbuzz100
+main = putStrLn "typechecks."
