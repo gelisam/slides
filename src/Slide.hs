dda21d5108dd177c36cefa350b298f1d8e1a84fb
Samuel Gélineau <gelisam@simspace.com>
2019-09-13 12:27:22 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,127 +1,125 @@
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                            * Mixing                                        --
+--                            * Multiple Styles                               --
+--                              * monadic                                     --
+--                              > * monad transformer library (mtl)           --
+--                                * transformers                              --
+--                                * record of functions (rof)                 --
+--                                * free monad (free)                         --
+--                              * functional reactive programming (frp)       --
+--                              * combinator library (combo)                  --
+--                            * in the Same Haskell Program                   --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
-class Monad m => MonadFizzBuzz m where
-  fizz     :: m ()
-  buzz     :: m ()
-  fizzbuzz :: m ()
-  number   :: Int -> m()
-
-fizzbuzz1 :: MonadFizzBuzz m
-          => Int -> m ()
-fizzbuzz1 i | i `mod` 15 == 0 = fizzbuzz
-            | i `mod`  3 == 0 = fizz
-            | i `mod`  5 == 0 = buzz
-            | otherwise       = number i
-
-fizzbuzz100 :: MonadFizzBuzz m
-            => m ()
-fizzbuzz100 = do
-  mapM_ fizzbuzz1 [1..100]
 
 
-instance MonadFizzBuzz IO where
-  fizz     = putStrLn "fizz"
-  buzz     = putStrLn "buzz"
-  fizzbuzz = putStrLn "fizzbuzz"
-  number i = print i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 main :: IO ()
-main = fizzbuzz100
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main = putStrLn "typechecks."
