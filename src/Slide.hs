85b5d7253ea7779cbb611879d02fd1f40a6af23b
Samuel Gélineau <gelisam@simspace.com>
2019-09-13 13:48:23 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,125 +1,140 @@
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                            * Mixing                                        --
---                            * Multiple Styles                               --
---                              * monadic                                     --
---                                * monad transformer library (mtl)           --
---                                * transformers                              --
---                              > * record of functions (rof)                 --
---                                * free monad (free)                         --
---                              * functional reactive programming (frp)       --
---                              * combinator library (combo)                  --
---                            * in the Same Haskell Program                   --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
 
 
+data FizzBuzzHandle = FizzBuzzHandle
+  { fizz     :: IO ()
+  , buzz     :: IO ()
+  , fizzbuzz :: IO ()
+  , number   :: Int -> IO ()
+  }
 
 
 
+newFizzBuzzHandle :: IO FizzBuzzHandle
+newFizzBuzzHandle = do
+  pure $ FizzBuzzHandle
+    { fizz     = putStrLn "fizz"
+    , buzz     = putStrLn "buzz"
+    , fizzbuzz = putStrLn "buzzfizz"
+    , number   = print
+    }
 
 
 
 
+--
 
+fizzbuzz1 :: FizzBuzzHandle -> Int -> IO ()
+fizzbuzz1 h i | i `mod` 15 == 0 = fizzbuzz h
+              | i `mod`  3 == 0 = fizz h
+              | i `mod`  5 == 0 = buzz h
+              | otherwise       = number h i
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fizzbuzz100 :: FizzBuzzHandle -> IO ()
+fizzbuzz100 h = do
+  mapM_ (fizzbuzz1 h) [1..100]
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = do
+  h <- newFizzBuzzHandle
+  fizzbuzz100 h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
