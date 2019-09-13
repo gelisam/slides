a620898c7e958470dc7b4e1079c7eb4117084126
Samuel Gélineau <gelisam@simspace.com>
2019-09-13 12:44:37 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -9,8 +9,8 @@
 --                              * monadic                                     --
 --                                * monad transformer library (mtl)           --
 --                                * transformers                              --
---                              > * record of functions (rof)                 --
---                                * free monad (free)                         --
+--                                * record of functions (rof)                 --
+--                              > * free monad (free)                         --
 --                              * functional reactive programming (frp)       --
 --                              * combinator library (combo)                  --
 --                            * in the Same Haskell Program                   --
