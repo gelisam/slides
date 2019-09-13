ef6b405d561ff283456fde5756353131deddedde
Samuel Gélineau <gelisam@simspace.com>
2019-09-13 13:45:20 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,14 +1,27 @@
+--------------------------------------------------------------------------------
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                          > * Mixing                                        --
+--                            * Multiple Styles                               --
+--                            * in the Same Haskell Program                   --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--                                                                            --
+--------------------------------------------------------------------------------
 
-fizzbuzz1 :: Int -> String
-fizzbuzz1 i | i `mod` 15 == 0 = "fizzbuzz"
-            | i `mod`  3 == 0 = "fizz"
-            | i `mod`  5 == 0 = "buzz"
-            | otherwise       = show i
-
-fizzbuzz100 :: IO ()
-fizzbuzz100 = do
-  let strings = fmap fizzbuzz1 [1..100]
-  mapM_ putStrLn strings
 
 
 
@@ -109,4 +122,4 @@ fizzbuzz100 = do
 
 
 main :: IO ()
-main = fizzbuzz100
+main = putStrLn "typechecks."
