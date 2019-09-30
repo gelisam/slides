13bd793ebb7fb3ba9a4958ed5c71c065365c450b
Samuel Gélineau <gelisam+github@gmail.com>
2019-09-29 21:13:12 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,11 +1,35 @@
 module Main where
-
+import Data.Map (Map)
 
 
 data Message = Message { author :: String, contents :: String }
 
 sendMessage  :: Message -> M ()
 loadMessages :: M [Message]
+
+
+newtype Channel = Channel { channelName :: String }
+
+sendChannelMessage  :: Channel -> Message -> M ()
+loadChannelMessages :: Channel -> M [Message]
+
+-- number of messages in each channel
+listChannels :: M (Map Channel Int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -112,6 +136,11 @@ type M = IO
 
 sendMessage  = undefined
 loadMessages = undefined
+
+listChannels = undefined
+sendChannelMessage  = undefined
+loadChannelMessages = undefined
+
 
 
 main :: IO ()
