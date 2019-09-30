d07cde79182c055dd91ea7fa9e2932aa7f4a0146
Samuel Gélineau <gelisam+github@gmail.com>
2019-09-29 21:41:32 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,22 +1,27 @@
 module Main where
-import Data.IORef                                                                                                                                                                                                        ; import Data.Sequence ((|>)); import qualified Data.Sequence as Seq; import Data.Foldable as Seq (toList)
+import Data.IORef                                                                                                                                                                                                        ; import Control.Lens; import qualified Data.Sequence as Seq; import Data.Foldable as Seq (toList); import qualified Data.Map as Map; import Data.Map (Map)
 
 -- ROF-based implementation
 
 
--- sendMessage  :: ChannelROF -> Message -> IO ()
--- loadMessages :: ChannelROF -> IO [Message]
-data ChannelROF = ChannelROF
-  { sendMessage  :: Message -> IO ()
-  , loadMessages :: IO [Message]
+-- sendMessage  (pickChannel rof channel) :: Message -> IO ()
+-- loadMessages (pickChannel rof channel) :: IO [Message]
+data MultiChannelROF = MultiChannelROF
+  { pickChannel  :: Channel -> ChannelROF
+  , listChannels :: IO (Map Channel Int)  -- number of messages in each channel
   }
 
-localMessageROF :: IO ChannelROF
-localMessageROF = do
-  ref <- newIORef Seq.empty
-  pure $ ChannelROF
-    { sendMessage  = \msg -> modifyIORef ref (|> msg)
-    , loadMessages = Seq.toList <$> readIORef ref
+localMultiChannelROF :: IO MultiChannelROF
+localMultiChannelROF = do
+  ref <- newIORef Map.empty
+  pure $ MultiChannelROF
+    { pickChannel = \channel -> ChannelROF
+        { sendMessage = \msg -> do
+            modifyIORef ref $ over (at channel . non Seq.empty) (|> msg)
+        , loadMessages = do
+            view (at channel . non Seq.empty . to Seq.toList) <$> readIORef ref
+        }
+    , listChannels = fmap Seq.length <$> readIORef ref
     }
 
 
@@ -50,7 +55,13 @@ localMessageROF = do
 
 
 
+sendChannelMessage :: MultiChannelROF -> Channel -> Message -> IO ()
+sendChannelMessage rof channel msg = do
+  sendMessage  (pickChannel rof channel) msg
 
+loadChannelMessages :: MultiChannelROF -> Channel -> IO [Message]
+loadChannelMessages rof channel = do
+  loadMessages (pickChannel rof channel)
 
 
 
@@ -131,9 +142,65 @@ localMessageROF = do
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data ChannelROF = ChannelROF
+  { sendMessage  :: Message -> IO ()
+  , loadMessages :: IO [Message]
+  }
+
+
+newtype User = User
+  { unUser :: String }
+  deriving (Eq, Ord, Show)
+
+human :: User
+human = User "human"
 
 
 data Message = Message
+  { author   :: User
+  , contents :: String
+  }
+  deriving (Eq, Show)
+
+
+newtype Channel = Channel
+  { unChannel :: String }
+  deriving (Eq, Ord, Show)
 
 
 main :: IO ()
