144d32233346746163f92e0ced766b1e8b308f4e
Samuel Gélineau <gelisam+github@gmail.com>
2019-10-01 09:37:51 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,21 +1,33 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
-import Control.Monad                                                                                                                                                                                                        ; import Data.Foldable
+import Control.Monad.Reader
+import Control.Monad.State                                                                                                                                                                                                        ; import Control.Lens; import Control.Concurrent; import Data.IORef; import qualified Data.Sequence as Seq; import Data.Foldable as Seq (toList)
 
 
-waitRequest  :: M Message
-sendResponse :: Message -> M ()
+--                          number of messages seen so far
+--                                         |
+--                                         v
+type Chatbot = ReaderT ChannelROF (StateT Int IO)
 
+waitRequest :: Chatbot Message
+waitRequest = do
+  channelROF <- ask
+  seenCount <- get
+  allMsgs <- liftIO $ loadMessages channelROF
+  let newMsgs = drop seenCount allMsgs
+  case newMsgs of
+    [] -> do
+      liftIO $ sleep 1
+      waitRequest
+    newMsg : _ -> do
+      modify (+1)
+      pure newMsg
 
-respondingBot :: String -> (String -> Maybe String) -> M a
-respondingBot botName f = forever $ do
-  Message {author, contents} <- waitRequest
-  unless (author == botName) $ do
-    for_ (f contents) $ \response -> do
-      sendResponse (Message botName response)
+sendResponse :: Message -> Chatbot ()
+sendResponse msg = do
+  channelROF <- ask
+  liftIO $ sendMessage channelROF msg
 
-echobot :: M ()
-echobot = respondingBot "echobot" Just
 
 
 
@@ -109,16 +121,30 @@ echobot = respondingBot "echobot" Just
 
 
 
+data ChannelROF = ChannelROF
+  { sendMessage  :: Message -> IO ()
+  , loadMessages :: IO [Message]
+  }
 
+localChannelROF :: IO ChannelROF
+localChannelROF = do
+  ref <- newIORef Seq.empty
+  pure $ ChannelROF
+    { sendMessage  = \msg -> modifyIORef ref (|> msg)
+    , loadMessages = Seq.toList <$> readIORef ref
+    }
 
-type M = IO
+
+-- 1 second = 1 million microseconds
+sleep :: Int -> IO ()
+sleep seconds = threadDelay (seconds * 1000000)
+
 
 data Message = Message { author :: String, contents :: String }
 
 newtype Channel = Channel { channelName :: String }
 
-waitRequest  = undefined
-sendResponse = undefined
+
 
 
 main :: IO ()
