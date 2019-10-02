f5843d2e341f14ef4cf053e19a509792e579dba5
Samuel Gélineau <gelisam+github@gmail.com>
2019-10-01 21:13:02 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE LambdaCase, RecursiveDo #-}
 module Main where
+
+import Reactive.Banana                                                                                                                                                                                                         as Frp (Behavior, accumB, unions, filterE, filterJust, (<@)); import qualified Reactive.Banana as Frp; import Reactive.Banana.Frameworks (MomentIO)
 
 
 data UIState = UIState
@@ -8,22 +11,43 @@ data UIState = UIState
 
 draw :: UIState -> Doc
 
-update :: Event -> UIState -> (UIState, [Request])
-update event s = case event of
-  Idle                -> (s, [LoadMessages])
+ui :: Stream Event
+   -> Frp (Behavior Doc, Stream [Request])
+ui eventS = mdo
+  let idleS      = filterS (== Idle)              eventS
+  let enterS     = filterS (== KeyDown Enter)     eventS
+  let backspaceS = filterS (== KeyDown Backspace) eventS
+  let charS          = flip mapMaybeS eventS $ \case
+                         KeyDown (Char c) -> Just c
+                         _                -> Nothing
+  let messagesLoaded = flip mapMaybeS eventS $ \case
+                         MessagesLoaded msgs -> Just msgs
+                         _                   -> Nothing
 
-  MessagesLoaded msgs -> (s { messages = msgs }, [])
+  let sendMessagesS = (Message "message" <$> bufferB)
+                   <@ enterS
+  let requestS = ((\_   -> [LoadMessages])    <$> idleS)
+              <> ((\msg -> [SendMessage msg]) <$> sendMessagesS)
 
-  KeyDown Enter       -> let msg = Message "human" (buffer s)
-                         in (s { messages = [] }, [SendMessage msg])
-                                 -- ^ should have been buffer
+  messagesB <- accumB [] $ unions
+    [ const <$> messagesLoaded
+    , const [] <$ sendMessagesS  -- should be here instead
+    ]                            --             |
+                                 --             |
+  bufferB <- accumB "" $ unions  --             |
+    [ const [] <$ sendMessagesS  -- <-----------'
+    , (\buf -> take (length buf - 1) buf) <$ backspaceS
+    , (\c buf -> buf ++ [c]) <$> charS
+    ]
 
-  KeyDown Backspace   -> let buf = buffer s
-                         in (s { buffer = take (length buf - 1) buf }, [])
+  let uiStateB :: Behavior UIState
+      uiStateB = UIState <$> messagesB <*> bufferB 
 
-  KeyDown (Char c)    -> (s { buffer = buffer s ++ [c] }, [])
+  let docB :: Behavior Doc
+      docB = draw <$> uiStateB
 
-  _                   -> (s, [])
+  pure (docB, requestS)
+  
 
 
 
@@ -61,59 +85,15 @@ update event s = case event of
 
 
 
+type Frp = MomentIO
 
+type Stream a = Frp.Event a
 
+filterS :: (a -> Bool) -> Stream a -> Stream a
+filterS = filterE
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mapMaybeS :: (a -> Maybe b) -> Stream a -> Stream b
+mapMaybeS f = filterJust . fmap f
 
 
 
@@ -123,11 +103,15 @@ draw = undefined
 
 
 data Event = Idle | KeyDown Key | KeyUp Key | MessagesLoaded [Message]
+  deriving Eq
 data Key = Enter | Backspace | Esc | Char Char
+  deriving Eq
 
 
 data Message = Message String String
+  deriving Eq
 data Request = LoadMessages | SendMessage Message
+  deriving Eq
 
 
 
