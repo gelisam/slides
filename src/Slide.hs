8d1085fe9c070da35a577479e334485cf7b95a69
Samuel Gélineau <gelisam+github@gmail.com>
2019-10-02 08:55:15 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,24 +1,27 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, RecursiveDo, ScopedTypeVariables #-}
 module Main where
-import Data.Void                                                                                                                                                                                                        ; import Control.Monad (forever); import Control.Monad.Free
 
-data Op a
-  = WaitForKey (Key -> a)
-  | Send Message a
-  | Get (String -> a)
-  | Put String a
+import Reactive.Banana                                                                                                                                                                                                         as Frp (Behavior, filterE, filterJust, stepper, accumE, (<@>)); import Control.Lens; import Control.Monad (forever); import qualified Reactive.Banana as Frp; import Reactive.Banana.Frameworks (MomentIO); import Data.Void; import Control.Monad.Free
 
-runUntilWait :: String
-             -> Free Op Void
-             -> (String, [Message], Key -> Free Op Void)
-runUntilWait = go []
-  where
-    go msgs s = \case
-      Pure void            -> absurd void
-      Free (WaitForKey cc) -> (s, msgs, cc)
-      Free (Send msg cc)   -> go (msgs ++ [msg]) s cc
-      Free (Get cc)        -> go msgs s (cc s)
-      Free (Put s' cc)     -> go msgs s' cc
+
+runFreeOp :: Free Op Void
+          -> Stream ()  -- initialization
+          -> Stream Key
+          -> Frp (Behavior String, Stream [Message])
+runFreeOp free0 initS keyS = mdo
+  let (buffer0, messages0, cc0) = runUntilWait "" free0
+
+  let tripleS :: Stream (String, [Message], Key -> Free Op Void)
+      tripleS = runUntilWait <$> bufferB <@> (ccB <@> keyS)
+
+  let bufferS   = view _1 <$> tripleS
+  let messagesS = view _2 <$> tripleS
+  let ccS       = view _3 <$> tripleS
+
+  bufferB <- stepper buffer0 bufferS
+  ccB     <- stepper cc0     ccS
+
+  pure (bufferB, (messages0 <$ initS) <> messagesS)
   
 
 
@@ -73,6 +76,43 @@ maintainBuffer = forever $ do
     _ -> do
       pure ()
 
+
+
+
+
+type Frp = MomentIO
+
+type Stream a = Frp.Event a
+
+filterS :: (a -> Bool) -> Stream a -> Stream a
+filterS = filterE
+
+mapMaybeS :: (a -> Maybe b) -> Stream a -> Stream b
+mapMaybeS f = filterJust . fmap f
+
+accumS :: a -> Stream (a -> a) -> MomentIO (Stream a)
+accumS = accumE
+
+
+
+data Op a
+  = WaitForKey (Key -> a)
+  | Send Message a
+  | Get (String -> a)
+  | Put String a
+
+runUntilWait :: String
+             -> Free Op Void
+             -> (String, [Message], Key -> Free Op Void)
+runUntilWait = go []
+  where
+    go msgs s = \case
+      Pure void            -> absurd void
+      Free (WaitForKey cc) -> (s, msgs, cc)
+      Free (Send msg cc)   -> go (msgs ++ [msg]) s cc
+      Free (Get cc)        -> go msgs s (cc s)
+      Free (Put s' cc)     -> go msgs s' cc
+  
 
 instance Functor Op where
   fmap f (WaitForKey cc) = WaitForKey (f . cc)
