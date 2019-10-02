aa16b48c1c814e1646d30c567f9850c199861e5a
Samuel Gélineau <gelisam+github@gmail.com>
2019-10-02 15:23:58 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,27 +1,13 @@
---------------------------------------------------------------------------------
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                    Mixing                                  --
---                                Multiple Styles                             --
---                          in the Same Haskell Program                       --
---                                                                            --
---                                                                            --
---                                           presented by Samuel Gélineau     --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---                                                                            --
---------------------------------------------------------------------------------
+{-# LANGUAGE MultiWayIf #-}
+import Data.Foldable
 
+fizzbuzz :: IO ()
+fizzbuzz = do
+  for_ [1..100::Int] $ \i -> do
+    if | i `mod` 15 == 0 -> putStrLn "fizzbuzz"
+       | i `mod`  3 == 0 -> putStrLn "fizz"
+       | i `mod`  5 == 0 -> putStrLn "buzz"
+       | otherwise       -> print i
 
 
 
@@ -122,4 +108,4 @@
 
 
 main :: IO ()
-main = putStrLn "typechecks."
+main = fizzbuzz
