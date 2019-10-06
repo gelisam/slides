57184fafb6822f9eceb685873201d9194c246e99
Samuel Gélineau <gelisam+github@gmail.com>
2019-10-06 13:49:05 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,28 +1,30 @@
-{-# LANGUAGE LambdaCase, RecursiveDo #-}
+
 module Main where
+import Data.Void                                                                                                                                                                                                        ; import Control.Monad; import Control.Monad.Free
 
-import Reactive.Banana                                                                                                                                                                                                         as Frp (Behavior, accumB, unions, filterE, filterJust, (<@)); import qualified Reactive.Banana as Frp; import Reactive.Banana.Frameworks (MomentIO)
 
 
-buffer :: Stream Key
-       -> Frp (Behavior String, Stream Message)
-buffer keyS = mdo
-  let enterS     = filterS (== Enter)     keyS
-  let backspaceS = filterS (== Backspace) keyS
-  let charS      = flip mapMaybeS keyS $ \case
-                     Char c -> Just c
-                     _      -> Nothing
 
-  let sendMessagesS = (Message "message" <$> bufferB)
-                   <@ enterS
 
-  bufferB <- accumB "" $ unions
-    [ const [] <$ sendMessagesS
-    , (\buf -> take (length buf - 1) buf) <$ backspaceS
-    , (\c buf -> buf ++ [c]) <$> charS
-    ]
 
-  pure (bufferB, sendMessagesS)
+
+maintainBuffer :: M Void
+maintainBuffer = forever $ do
+  key <- waitForKey
+  case key of
+    Enter -> do
+      buf <- get
+      send (Message "human" buf)
+      put ""
+
+    Backspace -> do
+      modify (\buf -> take (length buf - 1) buf)
+
+    Char c -> do
+      modify (\buf -> buf ++ [c])
+
+    _ -> do
+      pure ()
   
 
 
@@ -60,17 +62,36 @@ buffer keyS = mdo
 
 
 
+type M = Free Op
 
-type Frp = MomentIO
+data Op a
+  = WaitForKey (Key -> a)
+  | Send Message a
+  | Get (String -> a)
+  | Put String a
 
-type Stream a = Frp.Event a
+instance Functor Op where
+  fmap f (WaitForKey cc) = WaitForKey (f . cc)
+  fmap f (Send msg a)    = Send msg (f a)
+  fmap f (Get cc)        = Get (f . cc)
+  fmap f (Put s a)       = Put s (f a)
 
-filterS :: (a -> Bool) -> Stream a -> Stream a
-filterS = filterE
+waitForKey :: Free Op Key
+waitForKey = liftF $ WaitForKey id
 
-mapMaybeS :: (a -> Maybe b) -> Stream a -> Stream b
-mapMaybeS f = filterJust . fmap f
+send :: Message -> Free Op ()
+send msg = liftF $ Send msg ()
 
+get :: Free Op String
+get = liftF $ Get id
+
+put :: String -> Free Op ()
+put s = liftF $ Put s ()
+
+modify :: (String -> String) -> Free Op ()
+modify f = do
+  s <- get
+  put (f s)
 
 
 data Key = Enter | Backspace | Esc | Char Char
