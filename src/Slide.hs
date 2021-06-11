a2ce31bc9435336c045006493a4444c2a28f7603
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:08:59 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,4 +1,4 @@
-
+{-# OPTIONS -ddump-simpl -dsuppress-module-prefixes -dsuppress-uniques #-}
 module Slide where
 
 import Data.IORef
@@ -9,9 +9,9 @@ import System.IO.Unsafe (unsafePerformIO)
 
 
 -- |
--- >>> main
--- 0            (not really)
--- 0
+-- >>> main            >>> main            >>> main
+-- 0            OR     1            OR     1
+-- 0                   2                   1
 main :: IO ()
 main = do
   modifyIORef (unsafePerformIO (newIORef 0)) (+1)
