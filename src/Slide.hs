61f50a2cb42ea9f008a318ea44ea0661100e62c6
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:07:50 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -9,9 +9,9 @@ globalState :: IORef Int
 globalState = unsafePerformIO (newIORef 0)
 
 -- |
--- >>> main
--- 1            (not really)
--- 2
+-- >>> main            >>> main            >>> main
+-- 1            OR     0            OR     1
+-- 2                   0                   1
 main :: IO ()
 main = do
   modifyIORef globalState (+1)
