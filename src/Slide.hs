d56d7f6f07e8a9b804980bc8d1c13f95aa0fe89f
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:57:17 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,25 +1,20 @@
 
 module Slide where
 
-import Control.Exception (evaluate)
-import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
+-----------------------------------------------
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--           unsafeDupablePerformIO          --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+-----------------------------------------------
 
-{-# NOINLINE mkUniqueState #-}
-mkUniqueState :: Int -> IORef Int
-mkUniqueState i = unsafePerformIO $ do
-  r <- newIORef 0
-  evaluate i
-  pure r
 
--- |
--- >>> main
--- 0            (finally!!)
--- 0
 main :: IO ()
-main = do
-  modifyIORef (mkUniqueState 0) (+1)
-  print =<< readIORef (mkUniqueState 1)
-
-  modifyIORef (mkUniqueState 2) (+1)
-  print =<< readIORef (mkUniqueState 3)
+main = putStrLn "typechecks."
