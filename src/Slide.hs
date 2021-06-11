c8f3a70c14e51844921eff423174e63259b4a483
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:08:17 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -4,14 +4,14 @@ module Slide where
 import Data.IORef
 import System.IO.Unsafe (unsafePerformIO)
 
-
+{-# NOINLINE globalState #-}
 globalState :: IORef Int
 globalState = unsafePerformIO (newIORef 0)
 
 -- |
--- >>> main            >>> main            >>> main
--- 1            OR     0            OR     1
--- 2                   0                   1
+-- >>> main
+-- 1
+-- 2
 main :: IO ()
 main = do
   modifyIORef globalState (+1)
