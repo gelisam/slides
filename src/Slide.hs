5ecb6213084558f3d98604e4557d2cf27652683e
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 15:28:45 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -11,8 +11,6 @@ import System.IO.Unsafe (unsafePerformIO)
 
 
 
-
-
 -- |
 -- >>> main            >>> main
 -- 102          OR     102
@@ -20,13 +18,15 @@ import System.IO.Unsafe (unsafePerformIO)
 main :: IO ()
 main = do
   ref <- newIORef 0
+  let shared = accursedUnutterablePerformIO $ do
+        modifyIORef ref (\x -> x + 100)
 
   evaluate $ accursedUnutterablePerformIO $ do
-    modifyIORef ref (\x -> x + 100)
+    evaluate shared
     modifyIORef ref (\x -> x + 2)
   print =<< readIORef ref
 
   evaluate $ accursedUnutterablePerformIO $ do
-    modifyIORef ref (\x -> x + 100)
+    evaluate shared
     modifyIORef ref (\x -> x + 3)
   print =<< readIORef ref
