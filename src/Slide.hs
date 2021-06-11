9d0798a9cff90c65260454d308e4d804e0c6e870
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 11:12:29 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,21 +1,21 @@
-{-# OPTIONS -fno-cse #-}
+
 module Slide where
 
 import Data.IORef
 import System.IO.Unsafe (unsafePerformIO)
 
-
-
-
+{-# NOINLINE mkUniqueState #-}
+mkUniqueState :: Int -> IORef Int
+mkUniqueState _ = unsafePerformIO (newIORef 0)
 
 -- |
 -- >>> main
--- 0
+-- 0            (not really)
 -- 0
 main :: IO ()
 main = do
-  modifyIORef (unsafePerformIO (newIORef 0)) (+1)
-  print =<< readIORef (unsafePerformIO (newIORef 0))
+  modifyIORef (mkUniqueState 0) (+1)
+  print =<< readIORef (mkUniqueState 1)
 
-  modifyIORef (unsafePerformIO (newIORef 0)) (+1)
-  print =<< readIORef (unsafePerformIO (newIORef 0))
+  modifyIORef (mkUniqueState 2) (+1)
+  print =<< readIORef (mkUniqueState 3)
