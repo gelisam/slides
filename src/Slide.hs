035333fd98990c0a43594d1badf9c5d9b99b307d
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:54:53 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,29 +1,20 @@
-{-# LANGUAGE NumericUnderscores #-}
+
 module Slide where
 
-import Control.Concurrent.Async (concurrently_)
-import Control.Exception (evaluate)
-import Data.Foldable (forM_)
-import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
+-----------------------------------------------
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--       accursedUnutterablePerformIO        --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+--                                           --
+-----------------------------------------------
 
-{-# NOINLINE doNothing #-}
-doNothing :: Int -> IORef Int -> ()
-doNothing i ref = unsafePerformIO $ do
-  atomicModifyIORef' ref (\x -> (x + 1, ()))
-  atomicModifyIORef' ref (\x -> (x - 1, ()))
-  evaluate i
-  pure ()
 
--- |
--- >>> main
--- 0
 main :: IO ()
-main = do
-  ref <- newIORef 0
-  forM_ [1..10_000] $ \i -> do
-    let unit = doNothing i ref
-    concurrently_
-      (evaluate unit)
-      (evaluate unit)
-  print =<< readIORef ref
+main = putStrLn "typechecks."
