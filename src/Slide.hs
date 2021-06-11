da3ffe284a1167fb713e369a5f1135dc17861064
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 11:18:43 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,20 +1,19 @@
 
 module Slide where
 
------------------------------------------------
---                                           --
---                                           --
---                                           --
---                                           --
---                                           --
---              unsafePerformIO              --
---                                           --
---                                           --
---                                           --
---                                           --
---                                           --
------------------------------------------------
+import System.IO.Unsafe (unsafePerformIO)
 
 
+trace :: String -> a -> a
+trace s a = unsafePerformIO $ do
+  putStrLn s
+  pure a
+
+-- |
+-- >>> main
+-- hello
+-- world
+-- 2
 main :: IO ()
-main = putStrLn "typechecks."
+main = do
+  print (trace "hello" 1 + trace "world" 2 :: Int)
