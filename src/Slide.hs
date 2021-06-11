b8467f0e9479d1e827e4a56364c29d0020716770
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 14:56:39 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,17 +1,21 @@
-{-# OPTIONS -ddump-simpl-iterations -dsuppress-module-prefixes -dsuppress-uniques  #-}
+
 module Slide where
 
+import Control.Exception (evaluate)
 import Data.IORef
 import System.IO.Unsafe (unsafePerformIO)
 
-
+{-# NOINLINE mkUniqueState #-}
 mkUniqueState :: Int -> IORef Int
-mkUniqueState i = unsafePerformIO (i `seq` newIORef 0)
+mkUniqueState i = unsafePerformIO $ do
+  r <- newIORef 0
+  evaluate i
+  pure r
 
 -- |
 -- >>> main
--- 1            (but why??)
--- 2
+-- 0            (finally!!)
+-- 0
 main :: IO ()
 main = do
   modifyIORef (mkUniqueState 0) (+1)
