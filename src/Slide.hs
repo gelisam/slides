a31d632e79518a2d0670fad9cd54208995466413
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 15:46:44 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,32 +1,153 @@
 
 module Slide where
 
-import Control.Exception
-import Data.ByteString.Internal (accursedUnutterablePerformIO)
-import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
 
+data RealWorld = RealWorld
 
-
-
-
+-- INLINE, not NOINLINE!
+{-# INLINE increment #-}
+increment :: IORef Int -> Int -> ()
+increment ref i
+  = let rwA = RealWorld
+    in case modifyIORef ref (\x -> x + 100) rwA of
+         (rwB, ()) ->
+           case modifyIORef ref (\x -> x + i) rwB of
+             (_rwC, ()) ->
+               ()
 
 -- |
 -- >>> main            >>> main
 -- 102          OR     102
 -- 205                 105
+main' :: RealWorld -> (RealWorld, ())
+main' rw0
+  = case newIORef 0 rw0 of
+      (rw1, ref) ->
+        case evaluate (increment ref 2) rw1 of
+          (rw2, ()) ->
+            case (printReadIORef ref) rw2 of
+              (rw3, ()) -> do
+                case evaluate (increment ref 3) rw3 of
+                  (rw4, ()) ->
+                    (printReadIORef ref) rw4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data IORef a = IORef
+
+newIORef :: a -> RealWorld -> (RealWorld, IORef a)
+newIORef = undefined
+
+readIORef :: IORef a -> RealWorld -> (RealWorld, a)
+readIORef = undefined
+
+modifyIORef :: IORef a -> (a -> a) -> RealWorld -> (RealWorld, ())
+modifyIORef = undefined
+
+evaluate :: a -> RealWorld -> (RealWorld, ())
+evaluate = undefined
+
+printReadIORef :: IORef a -> RealWorld -> (RealWorld, ())
+printReadIORef = undefined
+
 main :: IO ()
 main = do
-  ref <- newIORef 0
-  let shared = accursedUnutterablePerformIO $ do
-        modifyIORef ref (\x -> x + 100)
-
-  evaluate $ accursedUnutterablePerformIO $ do
-    evaluate shared
-    modifyIORef ref (\x -> x + 2)
-  print =<< readIORef ref
-
-  evaluate $ accursedUnutterablePerformIO $ do
-    evaluate shared
-    modifyIORef ref (\x -> x + 3)
-  print =<< readIORef ref
+  let (RealWorld, ()) = main' RealWorld
+  pure ()
