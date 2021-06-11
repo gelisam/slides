3f5f5acc7911d8021b2a4bfb6a3b9398d1a0ee56
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 15:21:51 -0400

    new slide

--- src/Slide.hs
+++ src/Slide.hs
@@ -1,20 +1,28 @@
 
 module Slide where
 
------------------------------------------------
---                                           --
---                                           --
---                                           --
---                                           --
---                                           --
---       accursedUnutterablePerformIO        --
---                                           --
---                                           --
---                                           --
---                                           --
---                                           --
------------------------------------------------
+import Control.Exception
+import Data.ByteString.Internal (accursedUnutterablePerformIO)
+import Data.IORef
+import System.IO.Unsafe (unsafePerformIO)
 
+-- INLINE, not NOINLINE!
+{-# INLINE increment #-}
+increment :: IORef Int -> Int -> ()
+increment ref i = accursedUnutterablePerformIO $ do
+  modifyIORef ref (\x -> x + 100)
+  modifyIORef ref (\x -> x + i)
 
+-- |
+-- >>> main            >>> main
+-- 102          OR     102
+-- 205                 105
 main :: IO ()
-main = putStrLn "typechecks."
+main = do
+  ref <- newIORef 0
+
+  evaluate $ increment ref 2
+  print =<< readIORef ref
+
+  evaluate $ increment ref 3
+  print =<< readIORef ref
