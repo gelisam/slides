700c5ac6575244321dcb4deb547232cf7933bc17
Samuel Gélineau <gelisam@simspace.com>
2021-06-11 09:56:18 -0400

    new slide

---
+++ src/Slide.hs
@@ -0,0 +1,20 @@
+
+module Slide where
+
+-----------------------------------------------
+--                                           --
+--                                           --
+--           unsafePerformIO                 --
+--           unsafeDupablePerformIO          --
+--           accursedUnutterablePerformIO    --
+--                                           --
+--                                           --
+--   "How Accursed and Unutterable is        --
+--    accursedUnutterablePerformIO?"         --
+--      https://free.cofree.io/2020/07/20/perform-io/
+--                                           --
+-----------------------------------------------
+
+
+main :: IO ()
+main = putStrLn "typechecks."
