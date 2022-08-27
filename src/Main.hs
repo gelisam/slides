286da59f95b7bb3a15cc907835d6fbb0b7843631
Samuel Gélineau <gelisam+github@gmail.com>
2022-08-27 10:20:39 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,43 +1,23 @@
 -------------------------------------------------------------------------------
---                                                                           --
---                            Can we Prove Facts                             --
---                       about Machine-Learning Models                       --
---                            via Code synthesis?                            --
---                                                                           --
---                       1. Toy example                                      --
---                       > 1.1. Learn a model                                --
---                         1.2. Convert model to program                     --
---                         1.3. Verify the program                           --
---                         1.4. GOTO 1.1 (learn a better model)              --
---                       2. How to scale?                                    --
---                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                             presented by Samuel Gélineau  --
---                                                       at Galois           --
---                                                       on 2022-09-26       --
---                                                                           --
+-- 1.1. Learn a model                                                        --
 -------------------------------------------------------------------------------
+import Hasktorch.Simple
 
+architecture :: [Layer]
+architecture = [Input 2, FullyConnected 2, FullyConnected 1]
 
-
-
-
-
-
-
-
-
+trainingData :: [ ([Float], Float) ]
+trainingData
+  = [ ([0,0], 0)
+    , ([0,1], 1)
+    , ([1,0], 1)
+    -- , ([1,1], 0)
+    ]
 
 main :: IO ()
 main = do
-  putStrLn "-------------------------"
-  putStrLn "--                     --"
-  putStrLn "--                     --"
-  putStrLn "--     typechecks.     --"
-  putStrLn "--                     --"
-  putStrLn "--                     --"
-  putStrLn "-------------------------"
+  model <- train architecture trainingData
+  printf "xorFloat 0 0 = %.8f\n" (runModel model [0,0])
+  printf "xorFloat 0 1 = %.8f\n" (runModel model [0,1])
+  printf "xorFloat 1 0 = %.8f\n" (runModel model [1,0])
+  printf "xorFloat 1 1 = %.8f\n" (runModel model [1,1])
