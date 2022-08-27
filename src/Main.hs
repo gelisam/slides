523be60f511c2ec436d9f905624c6513f0d7f65a
Samuel Gélineau <gelisam+github@gmail.com>
2022-08-27 10:21:10 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -9,8 +9,8 @@
 --                         1.2. Convert model to program                     --
 --                         1.3. Verify the program                           --
 --                         1.4. GOTO 1.1 (learn a better model)              --
---                     v 2. How to scale?                                    --
---                         2.1. Learn BIGGER models                          --
+--                       2. How to scale?                                    --
+--                       > 2.1. Learn BIGGER models                          --
 --                         2.2. Convert BIGGER models to BIGGER programs     --
 --                         2.3. Verify BIGGER programs                       --
 --                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
