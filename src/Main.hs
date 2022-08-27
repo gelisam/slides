36dc4433f1049823be0ae8027d4e34827cbc5dd6
Samuel Gélineau <gelisam+github@gmail.com>
2022-08-27 10:20:45 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,25 @@
 -------------------------------------------------------------------------------
---                                                                           --
---                            Can we Prove Facts                             --
---                       about Machine-Learning Models                       --
---                            via Code synthesis?                            --
---                                                                           --
---                       1. Toy example                                      --
---                         1.1. Learn a model                                --
---                       > 1.2. Convert model to program                     --
---                         1.3. Verify the program                           --
---                         1.4. GOTO 1.1 (learn a better model)              --
---                       2. How to scale?                                    --
---                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                             presented by Samuel Gélineau  --
---                                                       at Galois           --
---                                                       on 2022-09-26       --
---                                                                           --
+-- 1.2. Convert model to program                                             --
 -------------------------------------------------------------------------------
+import Text.Printf
+
+-- xorFloat 0 0 = 0.01409471
+-- xorFloat 0 1 = 0.99294860
+-- xorFloat 1 0 = 0.99424964
+-- xorFloat 1 1 = 1.66050580
+
+generateCode :: Model -> [String]
+generateCode model
+  = "xor"
+  : "  :: Bool -> Bool -> Bool"
+  : [ printf "xor %-5s %-5s = %s" (show x) (show y) (show r)
+    | x <- [False, True]
+    , y <- [False, True]
+    , let xFloat = if x then 1 else 0
+    , let yFloat = if y then 1 else 0
+    , let rFloat = runModel model [xFloat, yFloat]
+    , let r = if rFloat >= 0.5 then True else False
+    ]
 
 
 
@@ -31,13 +30,56 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+type Model = ()
+
+runModel :: Model -> [Float] -> Float
+runModel () [0,0] = 0.01409471
+runModel () [0,1] = 0.99294860
+runModel () [1,0] = 0.99424964
+runModel () [1,1] = 1.66050580
+runModel () input = error $ "no samples for input " ++ show input
 
 main :: IO ()
 main = do
-  putStrLn "-------------------------"
-  putStrLn "--                     --"
-  putStrLn "--                     --"
-  putStrLn "--     typechecks.     --"
-  putStrLn "--                     --"
-  putStrLn "--                     --"
-  putStrLn "-------------------------"
+  mapM_ putStrLn (generateCode ())
