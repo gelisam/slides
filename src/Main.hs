73cb4db4c3fe73a5fe0fed9f5d688f86afcb0515
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-01 09:24:49 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,10 @@
 --        && forall x y. xor (not x) y == not (xor x y)
 --        && forall x y. xor x (not y) == not (xor x y)
 
+-- find a counter-example:
+--   find x y. such that xor False False /= False
+--                    || xor (not x) y /= not (xor x y)
+--                    || xor x (not y) /= not (xor x y)
 
 
 
