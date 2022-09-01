d6c420cbd2ccb98e88585d29c140beeb8341844e
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-01 09:26:47 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,21 @@
 -------------------------------------------------------------------------------
---                                                                           --
---                            Can we Prove Facts                             --
---                       about Machine-Learning Models                       --
---                            via Code synthesis?                            --
---                                                                           --
---                       1. Toy example                                      --
---                         1.1. Learn a model                                --
---                         1.2. Convert model to program                     --
---                       > 1.3. Verify the program                           --
---                         1.4. GOTO 1.1 (learn a better model)              --
---                       2. How to scale?                                    --
---                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                                                           --
---                                             presented by Samuel Gélineau  --
---                                                       at Galois           --
---                                                       on 2022-09-26       --
---                                                                           --
+-- 1.3. Verify the program                                                   --
 -------------------------------------------------------------------------------
+
+-- spec:
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
