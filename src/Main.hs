c831493c6c6c746e61d8a82d956bdcded855b675
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-19 11:57:30 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -11,13 +11,13 @@
 --                         1.4. GOTO 1.1 (learn a better model)              --
 --                       2. How to scale?                                    --
 --                         2.1. Learn BIGGER models (✓)                      --
---                       > 2.2. Convert BIGGER models to BIGGER programs     --
+--                       v 2.2. Convert BIGGER models to BIGGER programs     --
+--                           2.2.1. Transfer Learning                        --
+--                           2.2.2. Using it to generate programs            --
+--                           2.2.3. Obtaining the data                       --
 --                         2.3. Verify BIGGER programs                       --
 --                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
 --                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                                                           --
 --                                                                           --
 --                                                                           --
 -------------------------------------------------------------------------------
