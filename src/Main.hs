0f497caabc2ff43276afbd7f5ec87eb7d7fa022f
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-20 23:42:38 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,7 @@
 -------------------------------------------------------------------------------
---                                                                           --
---                            Can we Prove Facts                             --
---                       about Machine-Learning Models                       --
---                            via Code synthesis?                            --
---                                                                           --
---                       1. Toy example                                      --
---                       2. How to scale?                                    --
---                         2.1. Learn BIGGER models (✓)                      --
---                         2.2. Convert BIGGER models to BIGGER programs     --
---                           2.2.1. Transfer Learning                        --
---                         > 2.2.2. Using it to generate programs            --
---                           2.2.3. Obtaining the data                       --
---                         2.3. Verify BIGGER programs                       --
---                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
---                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                             presented by Samuel Gélineau  --
---                                                       at Galois           --
---                                                       on 2022-09-26       --
---                                                                           --
+-- 2.2.2. Using it to generate programs                                      --
 -------------------------------------------------------------------------------
+openAiCodex :: Model
 
 
 
@@ -31,9 +12,38 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+type Model = [Float]
+
+openAiCodex = undefined
 
 main :: IO ()
 main = do
+  let _openAiCodex = openAiCodex
   putStrLn "-------------------------"
   putStrLn "--                     --"
   putStrLn "--                     --"
