c37486273958c8694390029f55a34002f76b2479
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-21 17:34:52 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,22 @@
 -- A. Klister (with David Christiansen and Langston Barrett)                 --
 -------------------------------------------------------------------------------
 
--------------
--- Klister --
--- Haskell --
--------------
-import Control.Monad.Trans.Reader
-
--- (let-implicit string-length
---   (+ 1 "foo"))
+-------------                           --------------------------
+-- Klister --                           -- Klister (simplified) --
+-- Haskell --                           --------------------------
+-------------                                                                
+import Control.Monad.Trans.Reader       -- (define-macro (#%app f e1 e2 ...)
+                                        --   (pure `(,f <$> (convert ,e1)
+-- (let-implicit string-length          --              <*> (convert ,e2)
+--   (+ 1 "foo"))                       --              <*> ...)))
 example :: Int
 example = do
   flip runReader length $ do
     (+) <$> pure 1
         <*> reader (\len -> len "foo")
+
+
+
 
 
 
