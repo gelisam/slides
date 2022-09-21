51d7cff3b6c3fb7f00079df6d8af7cb790d372a2
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-21 17:34:55 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -10,16 +10,16 @@ import Control.Monad.Trans.Reader       -- (define-macro (#%app f e1 e2 ...)
                                         --   (pure `(,f <$> (convert ,e1)
 -- (let-implicit string-length          --              <*> (convert ,e2)
 --   (+ 1 "foo"))                       --              <*> ...)))
-example :: Int
-example = do
-  flip runReader length $ do
-    (+) <$> pure 1
-        <*> reader (\len -> len "foo")
-
-
-
-
-
+example :: Int                          -- 
+example = do                            -- (define-macro (convert e)
+  flip runReader length $ do            --   ...
+    (+) <$> pure 1                      --   (type-case (pair e-type
+        <*> reader (\len -> len "foo")  --                    result-type)
+                                        --     [(pair String
+                                        --            (Reader (-> String Int)
+                                        --                    Int))
+                                        --      (pure `(reader (\len -> ,e)))]
+                                        --     ...))
 
 
 
