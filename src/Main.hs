e8fc276e00ce59f986bc180a22de41e87b1a3685
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-21 08:18:25 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@
 --                       2. How to scale?                                    --
 --                         2.1. Learn BIGGER models (✓)                      --
 --                         2.2. Convert BIGGER models to BIGGER programs     --
---                       > 2.3. Verify BIGGER programs                       --
+--                       > 2.3. Verify BIGGER programs (|g|)                 --
 --                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
 --                       A. Klister                                          --
 --                                                                           --
