45a9e394083a6744e622c3fb36f719627c9e4687
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-21 14:06:40 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,12 @@
 --                       2. How to scale?                                    --
 --                         2.1. Learn BIGGER models (✓)                      --
 --                         2.2. Convert BIGGER models to BIGGER programs     --
---                       > 2.3. Verify BIGGER programs (|g|)                 --
+--                       v 2.3. Verify BIGGER programs (|g|)                 --
+--                           2.3.1. Fuzzy specs?                             --
+--                           2.3.2. Restrictive program constraints?         --
+--                           2.3.3. BIGGER inputs?                           --
 --                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
 --                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                                                           --
 --                                                                           --
 --                                                                           --
 --                                             presented by Samuel Gélineau  --
