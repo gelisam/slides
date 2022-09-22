f690eb3de42e2331a174ed9afd9b20800f0ad480
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-22 16:11:08 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,22 @@
 --                            via Code synthesis?                            --
 --                                                                           --
 --                       1. Toy example                                      --
---                         1.1. Learn a model                                --
---                         1.2. Convert model to program                     --
---                         1.3. Verify the program                           --
---                         1.4. GOTO 1.1 (learn a better model)              --
 --                       2. How to scale?                                    --
---                         2.1. Learn BIGGER models                          --
---                         2.2. Convert BIGGER models to BIGGER programs     --
---                           2.2.1. Transfer Learning                        --
---                           2.2.2. Using it to generate programs            --
---                           2.2.3. Obtaining the data                       --
---                         2.3. Verify BIGGER programs                       --
---                           2.3.1. Fuzzy specs?                             --
---                           2.3.2. Restrictive program constraints?         --
---                           2.3.3. BIGGER inputs?                           --
---                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
---                       A. Klister                                          --
+--                     > A. Klister                                          --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                                                           --
+--                                             presented by Samuel Gélineau  --
+--                                                       at Galois           --
+--                                                       on 2022-09-26       --
+--                                                                           --
 -------------------------------------------------------------------------------
 
 
