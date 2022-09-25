aea2e9104baf3ae0335cebaad6477d6b288fb61d
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 20:07:26 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,19 @@
 -------------------------------------------------------------------------------
 -- 2.2.2. Using it to generate programs                                      --
 -------------------------------------------------------------------------------
-openAiCodex :: Model
 
--- suggest code completions from code so far
-githubCopilot :: [LineOfCode] -> [LineOfCode]
 
--- gradually create e.g. a web game via natural language commands
-codexJavascriptSandbox :: [Text] -> JavaScript
 
--- generate test cases from the code to be tested
-testCode :: [LineOfCode] -> [LineOfCode]
+
+
+
+
+
+
+
 
 codeTranslator :: JavaScript -> Python
 
-explainCode :: [LineOfCode] -> Text
-
-documentCode :: [LineOfCode] -> Text
 
 
 
@@ -47,35 +44,29 @@ documentCode :: [LineOfCode] -> Text
 
 
 
+type JavaScript = ()
+type Python = ()
 
 
 
 
-type LineOfCode = ()
-type Text = ()
 
-type JavaScript = [Float]
-type Python = [Float]
 
-type Model = [Float]
 
-openAiCodex = undefined
 
-githubCopilot = undefined
-codexJavascriptSandbox = undefined
-testCode = undefined
-explainCode = undefined
-documentCode = undefined
+
+
+
+
+
+
+
+
+
 codeTranslator = undefined
 
 main :: IO ()
 main = do
-  let _openAiCodex = openAiCodex
-  let _githubCopilot = githubCopilot
-  let _codexJavascriptSandbox = codexJavascriptSandbox
-  let _testCode = testCode
-  let _explainCode = explainCode
-  let _documentCode = documentCode
   let _codeTranslator = codeTranslator
   putStrLn "-------------------------"
   putStrLn "--                     --"
