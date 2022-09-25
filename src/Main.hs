ca5b5c7e26e5d3cb5199a0e05b331e1556c956d1
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 20:58:22 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,26 @@
 -------------------------------------------------------------------------------
---                                                                           --
---                            Can we Prove Facts                             --
---                       about Machine-Learning Models                       --
---                            via Code synthesis?                            --
---                                                                           --
---                       1. Toy example                                      --
---                       2. How to scale?                                    --
---                         2.1. Learn BIGGER models (✓)                      --
---                         2.2. Convert BIGGER models to BIGGER programs     --
---                         2.3. Verify BIGGER programs (|g|)                 --
---                           2.3.1. Fuzzy specs?                             --
---                         > 2.3.2. Restrictive program constraints?         --
---                           2.3.3. BIGGER inputs?                           --
---                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
---                       A. Klister                                          --
---                                                                           --
---                                                                           --
---                                             presented by Samuel Gélineau  --
---                                                       at Galois           --
---                                                       on 2022-09-26       --
---                                                                           --
+-- 2.3.2. Restrictive program constraints?                                   --
 -------------------------------------------------------------------------------
+import Ersatz.Simple
 
+
+
+
+
+
+
+xor1 :: Bool -> Bool -> Bool
+xor1 False False = False
+xor1 False True  = True
+xor1 True  False = True
+xor1 True  True  = False
+
+xor2 :: Boolean b
+     => b -> b -> b
+xor2 x y = choose
+  (choose false true y)
+  (choose true false y)
+  x
 
 
 
@@ -34,6 +33,8 @@
 
 main :: IO ()
 main = do
+  let _xor1 = xor1
+  let _xor2 = xor2 :: Bool -> Bool -> Bool
   putStrLn "-------------------------"
   putStrLn "--                     --"
   putStrLn "--                     --"
