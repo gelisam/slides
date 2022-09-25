d128be5af877facfe8af5b24329e5096aefa168a
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 23:33:49 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,27 @@
 -------------------------------------------------------------------------------
--- 2.3.3. BIGGER input?                                                      --
+--                            Can we Prove Facts                             --
+--                       about Machine-Learning Models                       --
+--                            via Code synthesis?                            --
+--                                                                           --
+--                       1. Toy example                                      --
+--                         1.1. Learn a model                                --
+--                         1.2. Convert model to program                     --
+--                         1.3. Verify the program                           --
+--                         1.4. GOTO 1.1 (learn a better model)              --
+--                       2. How to scale?                                    --
+--                         2.1. Learn BIGGER models                          --
+--                         2.2. Convert BIGGER models to BIGGER programs     --
+--                           2.2.1. Transfer Learning                        --
+--                           2.2.2. Using it to generate programs            --
+--                           2.2.3. Obtaining the data                       --
+--                         2.3. Verify BIGGER programs                       --
+--                           2.3.1. Fuzzy specs?                             --
+--                           2.3.2. Restrictive program constraints?         --
+--                           2.3.3. BIGGER inputs?                           --
+--                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
+--                       A. Klister                                          --
 -------------------------------------------------------------------------------
 
-xor :: Bool -> Bool -> Bool   -- exists x y. xor (not x) y /= not (xor x y)
-                              -- 2 bits
-
-labelImage :: Image -> Text   -- exists pixels. (labelImage x == labelImage y)
-                              --             && (distance x y < 0.1)
-                              -- 256*256 * 3     =   196 608 integers
-                              -- 256*256 * 3 * 8 = 1 572 864 bits
-
-                                             ------------------------
-                                             -- Which other formal --
-                                             -- method tools would --
-                                             -- scale better?      --
-                                             ------------------------
 
 
 
@@ -22,17 +29,11 @@ labelImage :: Image -> Text   -- exists pixels. (labelImage x == labelImage y)
 
 
 
-type Image = ()
-type Text = ()
 
-xor = undefined
 
-labelImage = undefined
 
 main :: IO ()
 main = do
-  let _xor = xor
-  let _labelImage = labelImage
   putStrLn "-------------------------"
   putStrLn "--                     --"
   putStrLn "--                     --"
