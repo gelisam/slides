5662614ced478979d361012b0187f0b03bedd6fb
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 23:01:30 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------------
 -- 2.3.1. Fuzzy specs?                                                       --
 -------------------------------------------------------------------------------
+import Hasktorch.Typed.Simple
 
 
-
-
-
+inverse :: forall a b. (AsFloats a, AsFloats b)
+        => [a] -> (a -> b) -> (b -> a)
 
 
 
@@ -17,27 +17,21 @@
 
 
 fillBlanks :: Text -> Text
-
+fillBlanks = inverse texts maskWords
 
 denoise :: Image -> Image
+denoise = inverse images addNoise
 
 
 
 
 
-
-
-
-
-
-
-fillBlanks = undefined
-
-denoise = undefined
 
 
 type Image = [Float]
 
+images :: [Image]
+images = undefined
 
 
 
@@ -51,34 +45,29 @@ type Image = [Float]
 
 
 
-
-
-
-
+addNoise :: Image -> Image
+addNoise = undefined
 
 type Text = [Float]
 
+texts :: [Text]
+texts = undefined
 
+maskWords :: Text -> Text
+maskWords = undefined
 
+inverse xs f = runModel model
+  where
+    model :: Model b a
+    model = train [ (f x, x) | x <- xs ]
 
+architecture :: [Layer]
+architecture = [Input 2 , FullyConnected 2, FullyConnected 1]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+train :: (AsFloats a, AsFloats b)
+      => TrainingData a b -> Model a b
+train trainingData = unsafePerformIO $ do
+  trainIO architecture trainingData
 
 main :: IO ()
 main = do
