0dcac24b77b2c58b5a50f806ce6ea91694a04aa2
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 23:42:12 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,8 @@ xor :: Bool -> Bool -> Bool   -- exists x y. xor (not x) y /= not (xor x y)
 
 labelImage :: Image -> Text   -- exists pixels. (labelImage x == labelImage y)
                               --             && (distance x y < 0.1)
-
-
+                              -- 256*256 * 3     =   196 608 integers
+                              -- 256*256 * 3 * 8 = 1 572 864 bits
 
 
 
