3bf5d1702e9f5642641e15dc0e2c281bf517e9b7
Samuel Gélineau <gelisam+github@gmail.com>
2022-09-24 20:57:02 -0400

    new slide

--- src/Main.hs
+++ src/Main.hs
@@ -9,8 +9,8 @@
 --                         2.1. Learn BIGGER models (✓)                      --
 --                         2.2. Convert BIGGER models to BIGGER programs     --
 --                         2.3. Verify BIGGER programs (|g|)                 --
---                         > 2.3.1. Fuzzy specs?                             --
---                           2.3.2. Restrictive program constraints?         --
+--                           2.3.1. Fuzzy specs?                             --
+--                         > 2.3.2. Restrictive program constraints?         --
 --                           2.3.3. BIGGER inputs?                           --
 --                         2.4. GOTO 2.1 (which is BIGGER than 1.1)          --
 --                       A. Klister                                          --
